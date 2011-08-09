7f8e76704c9448e5102a450f27c14f5945fb74ae
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-08-09 20:41:41 +0000

    reorganizing test-support

---
+++ model-impl/impl/src/test/java/org/cytoscape/model/NetworkTestSupport.java
@@ -0,0 +1,40 @@
+
+package org.cytoscape.test.support;
+
+import org.cytoscape.equations.Interpreter;
+import org.cytoscape.event.CyEventHelper;
+import org.cytoscape.event.DummyCyEventHelper;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.CyTableFactory;
+
+import org.cytoscape.model.internal.CyNetworkFactoryImpl;
+import org.cytoscape.model.internal.CyTableFactoryImpl;
+import org.cytoscape.model.internal.CyTableManagerImpl;
+
+import static org.mockito.Mockito.*;
+
+public class NetworkTestSupport {
+
+	protected CyNetworkFactory networkFactory;
+	protected CyEventHelper eventHelper;
+	protected CyTableManagerImpl tableMgr;
+
+	public NetworkTestSupport() {
+		eventHelper = new DummyCyEventHelper();
+		tableMgr = mock(CyTableManagerImpl.class); 
+		CyTableFactoryImpl tableFactory = new CyTableFactoryImpl(eventHelper,tableMgr, mock(Interpreter.class));
+
+		networkFactory = new CyNetworkFactoryImpl( eventHelper, tableMgr, tableFactory );
+	}
+
+	public CyNetwork getNetwork() {
+		return networkFactory.getInstance();
+	}
+
+	public CyNetworkFactory getNetworkFactory() {
+		return networkFactory;	
+	}
+}
+
+
