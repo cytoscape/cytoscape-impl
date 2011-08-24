5bd741a69d82828fcc4c52f892b18724e44c36a7
ruschein <ruschein@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-08-24 21:55:01 +0000

    Fixed various spelling errors and general cleanup.

--- core-task-impl/src/main/java/org/cytoscape/task/internal/quickstart/StartTask.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/quickstart/StartTask.java
@@ -1,5 +1,6 @@
 package org.cytoscape.task.internal.quickstart;
 
+
 import org.cytoscape.model.CyNetworkManager;
 import org.cytoscape.task.internal.quickstart.subnetworkbuilder.CreateSubnetworkWithoutGeneListTask;
 import org.cytoscape.task.internal.quickstart.subnetworkbuilder.SearchRelatedGenesTask;
@@ -11,33 +12,36 @@ import org.cytoscape.work.util.ListSingleSelection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class StartTask extends ImportNetworkFromPublicDataSetTask {
-	
 	private static final Logger logger = LoggerFactory.getLogger(StartTask.class);
 	
 	@Tunable(description = "Gene Ontology (Optional)")
 	public String go;
 
-	@Tunable(description = "Disease/Phynotype (Optional)")
-	public String phynotype;
+	@Tunable(description = "Disease/Phenotype (Optional)")
+	public String phenotype;
 	
-	@Tunable(description = "List of genes you are interested in (Optional. Separated by space)")
+	@Tunable(description = "List of genes you are interested in (Optional. Separated by spaces)")
 	public String queryGenes;
 
 	@Tunable(description = "Gene ID Type (Optional)")
-	public ListSingleSelection<IDType> selection = new ListSingleSelection<IDType>(IDType.GENE_SYMBOL, IDType.ENSEMBL,
-			IDType.ENTREZ_GENE, IDType.UNIPROT);
+	public ListSingleSelection<IDType> selection =
+		new ListSingleSelection<IDType>(IDType.GENE_SYMBOL, IDType.ENSEMBL,
+		                                IDType.ENTREZ_GENE, IDType.UNIPROT);
 
 	protected final QuickStartState state;
 	protected final ImportTaskUtil importTaskUtil;
-	
 	protected final CyNetworkManager networkManager;
 	protected final SubnetworkBuilderUtil subnetworkUtil;
 	
 	public StartTask(final QuickStartState state, final ImportTaskUtil importTaskUtil,
-			final CyNetworkManager networkManager, final SubnetworkBuilderUtil subnetworkUtil) {
-		super(importTaskUtil.processors, importTaskUtil.mgr, networkManager, importTaskUtil.networkViewManager,
-				importTaskUtil.props, importTaskUtil.cyNetworkNaming, importTaskUtil.streamUtil);
+	                 final CyNetworkManager networkManager,
+	                 final SubnetworkBuilderUtil subnetworkUtil)
+	{
+		super(importTaskUtil.processors, importTaskUtil.mgr, networkManager,
+		      importTaskUtil.networkViewManager, importTaskUtil.props,
+		      importTaskUtil.cyNetworkNaming, importTaskUtil.streamUtil);
 		this.state = state;
 		this.importTaskUtil = importTaskUtil;
 		this.networkManager = networkManager;
@@ -45,16 +49,24 @@ public class StartTask extends ImportNetworkFromPublicDataSetTask {
 	}
 
 	public void run(TaskMonitor tm) throws Exception {
-		if((go != null && go.trim().length() != 0)|| (phynotype != null && phynotype.trim().length() !=0)) {
+		if (go != null && go.trim().length() != 0
+		    || phenotype != null && phenotype.trim().length() != 0)
+		{
 			logger.info("GO = " + go);
-			logger.info("Phynotype = " + phynotype);
-			insertTasksAfterCurrentTask(new SearchRelatedGenesTask(subnetworkUtil, new SubnetworkBuilderState(), go, phynotype));
+			logger.info("Phenotype = " + phenotype);
+			insertTasksAfterCurrentTask(
+				new SearchRelatedGenesTask(subnetworkUtil,
+				                           new SubnetworkBuilderState(), go,
+				                           phenotype));
 		}
 		
-		if(queryGenes != null && queryGenes.trim().length() != 0) {
+		if (queryGenes != null && queryGenes.trim().length() != 0) {
 			logger.info("Query Genes: " + queryGenes);
 			final IDType selectedType = selection.getSelectedValue();
-			insertTasksAfterCurrentTask(new CreateSubnetworkWithoutGeneListTask(subnetworkUtil, new SubnetworkBuilderState(), queryGenes, selectedType));
+			insertTasksAfterCurrentTask(
+				new CreateSubnetworkWithoutGeneListTask(subnetworkUtil,
+				                                        new SubnetworkBuilderState(),
+				                                        queryGenes, selectedType));
 		}
 		
 		super.run(tm);
