149dd860b7e36e7126961a8b99761797db8e996d
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-09-20 22:47:02 +0000

    adding performance subdir

---
+++ io-impl/performance/src/main/java/org/cytoscape/io/read/sif/PerfTest.java
@@ -0,0 +1,19 @@
+package org.cytoscape.io.read.sif; 
+
+
+
+
+
+public class PerfTest {
+
+
+	public static void main(String[] args) {
+		new PerfTest().runTestLoop();
+	}
+
+	public PerfTest() {
+	}
+
+	public void runTestLoop() {
+	}
+}
