14bdb8f889fbbd4f2a4f16387a697fac15a34aa1
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-09-22 17:46:59 +0000

    added better error checking to plugin loading

--- plugin-impl/src/main/java/org/cytoscape/plugin/internal/PluginLoaderTask.java
+++ plugin-impl/src/main/java/org/cytoscape/plugin/internal/PluginLoaderTask.java
@@ -14,8 +14,12 @@ import java.io.File;
 import java.net.URLClassLoader;
 import java.net.URL;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class PluginLoaderTask extends AbstractTask {
+	private static final Logger logger = LoggerFactory.getLogger( PluginLoaderTask.class );
 	private CyPluginAdapter adapter;
 
 	@Tunable(description="Select plugin JAR to load",params="input=true")
@@ -31,7 +35,11 @@ public class PluginLoaderTask extends AbstractTask {
 		JarFile jar = null; 
 		try {
 			jar = new JarFile(filename);
+			logger.debug("attempting to load simple plugin jar: " + filename);
 			String name = jar.getManifest().getMainAttributes().getValue("Cytoscape-Plugin");
+			logger.debug("attempting to load CyPlugin class: " + name); 	
+			if ( name == null || name.isEmpty() )
+				throw new IllegalArgumentException("This plugin jar is missing the \"Cytoscape-Plugin: your.package.YourCyPlugin\" entry in the META-INF/MANIFEST.MF file. Without that entry we can't start the plugin!");
 			URL jarurl = filename.toURI().toURL(); 
 			URLClassLoader ucl = URLClassLoader.newInstance( new URL[]{jarurl}, 
 			                                      PluginLoaderTask.class.getClassLoader() );
