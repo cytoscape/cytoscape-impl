c149cace9f2d6ed85fe8219c2cb19e0cb9ac1da4
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-09-23 22:01:29 +0000

    added support for root network factory

--- model-impl/impl/src/test/java/org/cytoscape/model/NetworkTestSupport.java
+++ model-impl/impl/src/test/java/org/cytoscape/model/NetworkTestSupport.java
@@ -6,11 +6,13 @@ import org.cytoscape.event.CyEventHelper;
 import org.cytoscape.event.DummyCyEventHelper;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.subnetwork.CyRootNetworkFactory;
 
 import org.cytoscape.model.internal.CyNetworkFactoryImpl;
 import org.cytoscape.model.internal.CyNetworkTableManagerImpl;
 import org.cytoscape.model.internal.CyTableFactoryImpl;
 import org.cytoscape.model.internal.CyTableManagerImpl;
+import org.cytoscape.model.internal.CyRootNetworkFactoryImpl;
 import org.cytoscape.service.util.CyServiceRegistrar;
 
 import static org.mockito.Mockito.*;
@@ -22,17 +24,16 @@ public class NetworkTestSupport {
 	protected CyEventHelper eventHelper;
 	protected CyTableManagerImpl tableMgr;
 	protected CyNetworkTableManagerImpl networkTableMgr;
+	protected CyRootNetworkFactory rootNetworkFactory;
 
 	public NetworkTestSupport() {
 		eventHelper = new DummyCyEventHelper();
 		tableMgr = mock(CyTableManagerImpl.class); 
 		networkTableMgr = mock(CyNetworkTableManagerImpl.class);
 		final CyServiceRegistrar serviceRegistrar = mock(CyServiceRegistrar.class);
-		final CyTableFactoryImpl tableFactory =
-			new CyTableFactoryImpl(eventHelper, mock(Interpreter.class), serviceRegistrar);
-		networkFactory =
-			new CyNetworkFactoryImpl(eventHelper, tableMgr, networkTableMgr, tableFactory,
-			                         serviceRegistrar);
+		final CyTableFactoryImpl tableFactory = new CyTableFactoryImpl(eventHelper, mock(Interpreter.class), serviceRegistrar);
+		networkFactory = new CyNetworkFactoryImpl(eventHelper, tableMgr, networkTableMgr, tableFactory, serviceRegistrar);
+		rootNetworkFactory = new CyRootNetworkFactoryImpl();
 	}
 
 	public CyNetwork getNetwork() {
@@ -41,6 +42,10 @@ public class NetworkTestSupport {
 
 	public CyNetworkFactory getNetworkFactory() {
 		return networkFactory;	
+	}
+
+	public CyRootNetworkFactory getRootNetworkFactory() {
+		return rootNetworkFactory;
 	}
 }
 
