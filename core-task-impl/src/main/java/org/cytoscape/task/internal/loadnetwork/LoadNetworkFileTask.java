166600e10e88c88daf3931a5e884e827276b312e
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-11-16 18:16:02 +0000

    Bug fixed

--- core-task-impl/src/main/java/org/cytoscape/task/internal/loadnetwork/LoadNetworkFileTask.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/loadnetwork/LoadNetworkFileTask.java
@@ -56,7 +56,6 @@ public class LoadNetworkFileTask extends AbstractLoadNetworkTask {
 	 */
 	public void run(TaskMonitor taskMonitor) throws Exception {
 		this.taskMonitor = taskMonitor;
-		taskMonitor.setProgress(-1);
 		
 		if (file == null)
 			throw new NullPointerException("No file specified!");
