4368bc1fd4757505f427dde17ea39bca16ef2f66
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-11-19 00:13:21 +0000

    Original creation

---
+++ plugin-impl/src/main/java/org/cytoscape/plugin/internal/PluginLoaderTaskFactory2.java
@@ -0,0 +1,22 @@
+package org.cytoscape.plugin.internal;
+
+
+import org.cytoscape.work.TaskFactory;
+import org.cytoscape.work.TaskIterator;
+
+
+public class PluginLoaderTaskFactory2 implements TaskFactory {
+	
+	private PluginLoaderTask2 task = null;
+	 
+	PluginLoaderTaskFactory2() {
+	}
+
+	public TaskIterator getTaskIterator() {
+		return new TaskIterator(task);
+	}
+	
+	public void setTask(PluginLoaderTask2 task){
+		this.task = task;
+	}
+}
