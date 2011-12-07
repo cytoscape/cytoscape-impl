a09232332637549992b65f44d58f5565775926a1
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-12-07 01:57:24 +0000

    Welcome Screen is now using global Bookmark CyProperty.

--- core-task-impl/src/main/java/org/cytoscape/task/internal/welcome/CreateNewNetworkPanel.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/welcome/CreateNewNetworkPanel.java
@@ -7,14 +7,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -23,6 +26,9 @@ import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
 import org.cytoscape.application.CyApplicationConfiguration;
+import org.cytoscape.property.bookmark.Bookmarks;
+import org.cytoscape.property.bookmark.BookmarksUtil;
+import org.cytoscape.property.bookmark.DataSource;
 import org.cytoscape.work.TaskFactory;
 import org.cytoscape.work.TaskManager;
 
@@ -44,17 +50,45 @@ public class CreateNewNetworkPanel extends JPanel {
 
 	private DownloadBiogridDataTaskFactory taskFactory;
 	private final TaskFactory loadNetworkFileTF;
+	
+	private final BookmarksUtil bkUtil;
+	private final Bookmarks bookmarks;
+	private final Map<String, String> dataSourceMap;
 
 	CreateNewNetworkPanel(Window parent, final TaskManager guiTaskManager, final LoadMitabFileTaskFactory loadTF,
-			final CyApplicationConfiguration config, final TaskFactory loadNetworkFileTF) {
+			final CyApplicationConfiguration config, final TaskFactory loadNetworkFileTF, BookmarksUtil bkUtil,
+			Bookmarks bookmarks) {
 		this.loadTF = loadTF;
 		this.parent = parent;
 		this.loadNetworkFileTF = loadNetworkFileTF;
 		this.guiTaskManager = guiTaskManager;
+		this.bkUtil = bkUtil;
+		this.bookmarks = bookmarks;
+		this.dataSourceMap = new HashMap<String, String>();
+
 		this.networkList = new JComboBox();
-		taskFactory = new DownloadBiogridDataTaskFactory(networkList, config);
-		guiTaskManager.execute(taskFactory);
+		// taskFactory = new DownloadBiogridDataTaskFactory(networkList,
+		// config);
+		// guiTaskManager.execute(taskFactory);
+		setFromBookmark();
+
 		initComponents();
+	}
+	
+	private void setFromBookmark() {
+		DefaultComboBoxModel theModel = new DefaultComboBoxModel();
+
+		// Extract the URL entries
+		List<DataSource> dataSources = bkUtil.getDataSourceList("network", bookmarks.getCategory());
+		if (dataSources != null) {
+			for(DataSource ds: dataSources) {
+				final String link = ds.getHref();
+				final String sourceName = ds.getName();
+				dataSourceMap.put(sourceName, link);
+				theModel.addElement(sourceName);
+			}
+		}
+		this.networkList.setModel(theModel);
 	}
 
 	private void initComponents() {
@@ -103,7 +137,7 @@ public class CreateNewNetworkPanel extends JPanel {
 			public void actionPerformed(ActionEvent ae) {
 				try {
 					loadNetwork();
-				} catch (URISyntaxException e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -112,7 +146,7 @@ public class CreateNewNetworkPanel extends JPanel {
 
 	private boolean firstSelection = false;
 
-	private void loadNetwork() throws URISyntaxException {
+	private void loadNetwork() throws URISyntaxException, MalformedURLException {
 		
 		Object file = networkList.getSelectedItem();
 		if(file == null || firstSelection == false) {
@@ -120,13 +154,14 @@ public class CreateNewNetworkPanel extends JPanel {
 			return;
 		}
 		
-		final URL url = taskFactory.getMap().get(file);
+		//final URL url = taskFactory.getMap().get(file);
+		final URL url = new URL(dataSourceMap.get(file));
 		if(layout.isSelected())
 			loadTF.setApplyLayout(true);
 		else
 			loadTF.setApplyLayout(false);
 		
-		loadTF.setFile(new File(url.toURI()));
+		loadTF.setURL(url);
 		guiTaskManager.execute(loadTF);
 		parent.dispose();
 	}
