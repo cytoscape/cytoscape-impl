0efedf3710a7f987549ad1a48f0ed449e7fa6f75
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-12-08 21:10:41 +0000

    Minor bugs fixed.

--- psi-mi-impl/src/main/java/org/cytoscape/psi_mi/internal/plugin/PsiMiTabCyFileFilter.java
+++ psi-mi-impl/src/main/java/org/cytoscape/psi_mi/internal/plugin/PsiMiTabCyFileFilter.java
@@ -1,12 +1,17 @@
 package org.cytoscape.psi_mi.internal.plugin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.cytoscape.io.CyFileFilter;
 import org.cytoscape.io.DataCategory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PsiMiTabCyFileFilter implements CyFileFilter {
 
@@ -21,7 +26,7 @@ public class PsiMiTabCyFileFilter implements CyFileFilter {
 		contentTypes = new HashSet<String>();
 		contentTypes.add("text/psi-mi-tab");
 
-		this.description = "PSI-MI TAB file";
+		this.description = "PSI-MI TAB 2.5 file";
 	}
 
 	@Override
@@ -29,7 +34,8 @@ public class PsiMiTabCyFileFilter implements CyFileFilter {
 		if (!category.equals(DataCategory.NETWORK))
 			return false;
 		
-		if(extensions.contains(getExtension(uri.toString())))
+		final String ext = getExtension(uri.toString());
+		if(extensions.contains(ext))
 			return true;
 		else
 			return false;
@@ -38,7 +44,30 @@ public class PsiMiTabCyFileFilter implements CyFileFilter {
 
 	@Override
 	public boolean accepts(InputStream stream, DataCategory category) {
-		// Not supported.
+		if (!category.equals(DataCategory.NETWORK)) {
+			return false;
+		}
+		try {
+			return checkFirstLine(stream);
+		} catch (IOException e) {
+			Logger logger = LoggerFactory.getLogger(getClass());
+			logger.error("Error while checking header", e);
+			return false;
+		}
+	}
+	
+	private boolean checkFirstLine(InputStream stream) throws IOException {
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		final String line = reader.readLine();
+		
+		if (line != null) {
+			String[] parts = line.split("\t");
+			if(parts.length >= 15)
+				return true;
+			else {
+				return false;
+			}
+		}
 		return false;
 	}
 	
