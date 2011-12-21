3a062300e464cef5fedf67aa007abdbb28b7099f
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-12-21 00:56:08 +0000

    Code cleanup only.  No API change.

--- ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/Bend.java
+++ ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/Bend.java
@@ -1,109 +1,116 @@
 package org.cytoscape.ding;
 
 import java.awt.geom.Point2D;
+import java.util.List;
 
 /**
- * A class that encapsulates the representation of the bend used for a 
+ * A class that encapsulates the representation of the bend used for a
  * particular EdgeView.
- *
- * @author Mike Smoot (mes5k) 
+ * 
+ * @author Mike Smoot (mes5k)
  */
-public interface  Bend {
+public interface Bend {
 
-  /**
-   * Given a list of points removes all existing handles/handlePoints 
-	 * and adds new ones for those specified in the List.
-	 * @param bendPoints A list of Point2Ds to create new handles.
-   */
-	public void setHandles( java.util.List bendPoints );
+	/**
+	 * Given a list of points removes all existing handles/handlePoints and adds
+	 * new ones for those specified in the List.
+	 * 
+	 * @param bendPoints
+	 *            A list of Point2Ds to create new handles.
+	 */
+	void setHandles(final List<Point2D> bendPoints);
 
 	/**
 	 * Returns a (new) List of clones of the Point2Ds that locate the handles.
 	 */
-	public java.util.List<Point2D> getHandles();
+	List<Point2D> getHandles();
 
 	/**
-	 *  Moves the handle specified at the given index to the given point.
-	 *  @param i Index of the handle to move.
-	 *  @param pt Point2D to which to move the specified handle.
+	 * Moves the handle specified at the given index to the given point.
+	 * 
+	 * @param handleIndex
+	 *            Index of the handle to move.
+	 * @param handlePosition
+	 *            Point2D to which to move the specified handle.
 	 */
-	public void moveHandle( int i, Point2D pt );
+	void moveHandle(final int handleIndex, final Point2D handlePosition);
 
-  /**
-   * Returns the handle Point2D closest to the source node.
-   */
-  public Point2D getSourceHandlePoint ();
+	/**
+	 * Returns the handle Point2D closest to the source node.
+	 */
+	Point2D getSourceHandlePoint();
 
-  /**
-   * Returns the handle Point2D closest to the target node.
-   */
-  public Point2D getTargetHandlePoint ();
+	/**
+	 * Returns the handle Point2D closest to the target node.
+	 */
+	Point2D getTargetHandlePoint();
 
-  /**
-   * Add a PHandle to the edge at the point specified. Acts as
-   * an interface to actuallyAddHandle() which does the actual adding.
-   *
-   * @param pt The point at which to draw the PHandle and to which the
-   *        PHandle will be attached via the locator.
-   */
-  public void addHandle ( Point2D pt );
+	/**
+	 * Add a PHandle to the edge at the point specified. Acts as an interface to
+	 * actuallyAddHandle() which does the actual adding.
+	 * 
+	 * @param handlePosition
+	 *            The point at which to draw the PHandle and to which the
+	 *            PHandle will be attached via the locator.
+	 */
+	void addHandle(final Point2D handlePosition);
 
-  /**
-   * Add a PHandle to the edge at the point and index specified. Acts as
-   * an interface to actuallyAddHandle() which does the actual adding.
-   *
-   * @param insertIndex The index at which to add the PHandle to the 
-   * list of handles.
-   * @param pt The point at which to draw the PHandle and to which the
-   *        PHandle will be attached via the locator.
-   */
-	public void addHandle ( int insertIndex , Point2D pt );
+	/**
+	 * Add a PHandle to the edge at the point and index specified. Acts as an
+	 * interface to actuallyAddHandle() which does the actual adding.
+	 * 
+	 * @param handleIndex
+	 *            The index at which to add the PHandle to the list of handles.
+	 * @param handlePosition
+	 *            The point at which to draw the PHandle and to which the
+	 *            PHandle will be attached via the locator.
+	 */
+	void addHandle(final int handleIndex, final Point2D handlePosition);
 
+	/**
+	 * Removes the PHandle at the specified point.
+	 * 
+	 * @param handlePosition
+	 *            If this point intersects an existing PHandle, then remove that
+	 *            PHandle.
+	 */
+	void removeHandle(final Point2D handlePosition);
 
-  /**
-   * Removes the PHandle at the specified point.
-   *
-   * @param pt If this point intersects an existing PHandle, then remove that
-   *        PHandle.
-   */
-  public void removeHandle ( Point2D pt );
+	/**
+	 * Removes the PHandle at the given index.
+	 * 
+	 * @param handleIndex
+	 *            The index of the PHandle to remove.
+	 */
+	void removeHandle(final int handleIndex);
 
- 
+	/**
+	 * Removes all handles
+	 */
+	void removeAllHandles();
 
-  /**
-   * Removes the PHandle at the given index. 
-   *
-   * @param i The index of the PHandle to remove. 
-   */
-  public void removeHandle ( int i );
+	/**
+	 * Checks to see if a PHandle already exists for the given point.
+	 * 
+	 * @param pt
+	 *            If this point intersects a currently existing PHandle, then
+	 *            return true, else return false.
+	 */
+	boolean handleAlreadyExists(final Point2D handlePosition);
 
-
-   /**
-   * Removes all handles
-   */
-  public void removeAllHandles () ;
-
-  /**
-   * Checks to see if a PHandle already exists for the given point.
-   *
-   * @param pt If this point intersects a currently existing PHandle, then
-   *        return true, else return false.
-   */
-  public boolean handleAlreadyExists ( Point2D pt );
-	
 	/**
 	 * Draws any handles previously added.
 	 */
-	public void drawSelected();
+	void drawSelected();
 
 	/**
 	 * Removes any handles from the display.
 	 */
-	public void drawUnselected();
+	void drawUnselected();
 
 	/**
-	 * Returns a list of points that define what gets drawn and hence
-	 * what is visible to the user.
+	 * Returns a list of points that define what gets drawn and hence what is
+	 * visible to the user.
 	 */
-	public Point2D[] getDrawPoints();
+	Point2D[] getDrawPoints();
 }
