4dc63f2dfad1aad3692e802371989a0025b8c555
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-01-14 00:23:14 +0000

    Export selected table

--- core-task-impl/src/main/java/org/cytoscape/task/internal/export/table/ExportCurrentTableTaskFactory.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/export/table/ExportCurrentTableTaskFactory.java
@@ -1,19 +1,25 @@
 package org.cytoscape.task.internal.export.table;
 
 import org.cytoscape.io.write.CyTableWriterManager;
+import org.cytoscape.model.CyNetworkManager;
+import org.cytoscape.model.CyTableManager;
 import org.cytoscape.task.AbstractTableTaskFactory;
 import org.cytoscape.work.TaskIterator;
 
 public class ExportCurrentTableTaskFactory extends AbstractTableTaskFactory {
 
 	private final CyTableWriterManager writerManager;
-
-	public ExportCurrentTableTaskFactory(CyTableWriterManager writerManager) {
+	private final CyTableManager cyTableManagerServiceRef;
+	private final CyNetworkManager cyNetworkManagerServiceRef;
+	
+	public ExportCurrentTableTaskFactory(CyTableWriterManager writerManager, CyTableManager cyTableManagerServiceRef, CyNetworkManager cyNetworkManagerServiceRef) {
 		this.writerManager = writerManager;
+		this.cyTableManagerServiceRef = cyTableManagerServiceRef;
+		this.cyNetworkManagerServiceRef = cyNetworkManagerServiceRef;
 	}
 	
 	@Override
 	public TaskIterator createTaskIterator() {
-		return new TaskIterator(2,new CyTableWriter(writerManager, table));
+		return new TaskIterator(new SelectExportTableTask(this.writerManager, this.cyTableManagerServiceRef, this.cyNetworkManagerServiceRef));
 	}
 }
