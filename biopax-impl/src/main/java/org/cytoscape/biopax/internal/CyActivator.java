54df80443db789299a9f65549afb2c61c14f41f9
rodche <rodche@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-03-30 02:05:04 +0000

    Removed unused classes (outdated; export to BioPAX was already disabled, not ported; anyway, it worked in Cy 2.8x only for BioPAX networks and only to save original BioPAX (modifications would be lost); biopax plugin has never been a BioPAX editor, but reader/mapper only)

--- biopax-impl/src/main/java/org/cytoscape/biopax/internal/CyActivator.java
+++ biopax-impl/src/main/java/org/cytoscape/biopax/internal/CyActivator.java
@@ -1,6 +1,5 @@
 package org.cytoscape.biopax.internal;
 
-import org.cytoscape.work.TaskManager;
 import org.cytoscape.util.swing.OpenBrowser;
 import org.cytoscape.view.vizmap.VisualMappingManager;
 import org.cytoscape.view.model.CyNetworkViewManager;
@@ -12,14 +11,12 @@ import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.view.vizmap.VisualMappingFunctionFactory;
 import org.cytoscape.view.vizmap.VisualStyleFactory;
 import org.cytoscape.model.CyNetworkFactory;
-import org.cytoscape.util.swing.FileUtil;
 import org.cytoscape.view.model.CyNetworkViewFactory;
 import org.cytoscape.view.model.events.NetworkViewAboutToBeDestroyedListener;
 import org.cytoscape.view.model.events.NetworkViewAddedListener;
 
 import org.cytoscape.biopax.internal.BioPaxFilter;
 import org.cytoscape.biopax.internal.action.BioPaxViewTracker;
-import org.cytoscape.biopax.internal.action.ExportAsBioPAXAction;
 import org.cytoscape.biopax.internal.BioPaxReaderTaskFactory;
 import org.cytoscape.biopax.internal.util.BioPaxVisualStyleUtil;
 import org.cytoscape.biopax.internal.view.BioPaxContainer;
@@ -49,14 +46,12 @@ public class CyActivator extends AbstractCyActivator {
 	public void start(BundleContext bc) {
 
 		CySwingApplication cySwingApplicationRef = getService(bc,CySwingApplication.class);
-		TaskManager<?,?> taskManagerRef = getService(bc,TaskManager.class);
 		OpenBrowser openBrowserRef = getService(bc,OpenBrowser.class);
 		CyApplicationManager cyApplicationManagerRef = getService(bc,CyApplicationManager.class);
 		CyNetworkViewManager cyNetworkViewManagerRef = getService(bc,CyNetworkViewManager.class);
 		CyNetworkNaming cyNetworkNamingRef = getService(bc,CyNetworkNaming.class);
 		CyNetworkFactory cyNetworkFactoryRef = getService(bc,CyNetworkFactory.class);
 		CyNetworkViewFactory cyNetworkViewFactoryRef = getService(bc,CyNetworkViewFactory.class);
-		FileUtil fileUtilRef = getService(bc,FileUtil.class);
 		StreamUtil streamUtilRef = getService(bc,StreamUtil.class);
 		VisualMappingManager visualMappingManagerRef = getService(bc,VisualMappingManager.class);
 		VisualStyleFactory visualStyleFactoryRef = getService(bc,VisualStyleFactory.class);
@@ -72,7 +67,6 @@ public class CyActivator extends AbstractCyActivator {
 		BioPaxViewTracker bioPaxViewTracker = new BioPaxViewTracker(bioPaxDetailsPanel,bioPaxContainer, cyApplicationManagerRef, visualMappingManagerRef, bioPaxVisualStyleUtil);
 		InputStreamTaskFactory inputStreamTaskFactory = new BioPaxReaderTaskFactory(bioPaxFilter,cyNetworkFactoryRef,cyNetworkViewFactoryRef,cyNetworkNamingRef,visualMappingManagerRef,bioPaxVisualStyleUtil);
 		CytoPanelComponent cytoPanelComponent = new BioPaxCytoPanelComponent(bioPaxContainer);
-//		ExportAsBioPAXAction exportAsBioPAXAction = new ExportAsBioPAXAction(fileUtilRef, cyApplicationManagerRef, bioPaxFilter, taskManagerRef);
 		
 		// register/export core Cytoscape osgi service implementations
 		registerService(bc,inputStreamTaskFactory,InputStreamTaskFactory.class, new Properties());
@@ -82,8 +76,6 @@ public class CyActivator extends AbstractCyActivator {
 		registerService(bc,(NetworkViewAboutToBeDestroyedListener)bioPaxViewTracker,NetworkViewAboutToBeDestroyedListener.class, new Properties());
 		registerService(bc,(SetCurrentNetworkViewListener)bioPaxViewTracker,SetCurrentNetworkViewListener.class, new Properties());
 		
-		// the next CyAction service (export BioPAX) is disabled: TODO needs refactoring; use ExportAsBioPAXTaskFactory as well...
-		//registerService(bc, exportAsBioPAXAction, CyAction.class,  new Properties());
 	}
 }
 
