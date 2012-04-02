9de84087299f59231abb3a77df875b9cf6c5ca45
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-02 16:43:17 +0000

    Fixes #664: Added API for fetching the configuration directory for an app

--- application-impl/src/main/java/org/cytoscape/application/internal/CyApplicationConfigurationImpl.java
+++ application-impl/src/main/java/org/cytoscape/application/internal/CyApplicationConfigurationImpl.java
@@ -1,17 +1,27 @@
 package org.cytoscape.application.internal;
 
 import java.io.File;
+import java.util.Dictionary;
 
 import org.cytoscape.application.CyApplicationConfiguration;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CyApplicationConfigurationImpl implements CyApplicationConfiguration {
 	
+	/** Default configuration directory used for all Cytoscape configuration files */
+	public static final String DEFAULT_CONFIG_DIR = ".cytoscape";
+
 	private static final Logger logger = LoggerFactory.getLogger(CyApplicationConfigurationImpl.class);
 	
 	private static final String DEF_USER_DIR = System.getProperty("user.home");
+
+	private static final String APP_CONFIGURATION_DIR = "app-data";
 	
+	private static final String BUNDLE_SYMBOLIC_NAME = "Bundle-SymbolicName";
+
 	private final File configFileLocation;
 	
 	public CyApplicationConfigurationImpl() {
@@ -31,4 +41,26 @@ public class CyApplicationConfigurationImpl implements CyApplicationConfiguratio
 		return configFileLocation;	
 	}
 
+	@Override
+	public File getAppConfigurationDirectoryLocation(Class<?> appClass) {
+		File configurationDirectory = getConfigurationDirectoryLocation();
+		Bundle bundle = FrameworkUtil.getBundle(appClass);
+		Dictionary<String, String> headers = bundle.getHeaders();
+		String basePath = headers.get(BUNDLE_SYMBOLIC_NAME);
+		String path = join(File.separator, configurationDirectory.getPath(), APP_CONFIGURATION_DIR, basePath);
+		return new File(path);
+	}
+	
+	private static String join(String separator, String... items) {
+		StringBuilder builder = new StringBuilder();
+		boolean first = true;
+		for (String item : items) {
+			if (!first) {
+				builder.append(separator);
+			}
+			builder.append(item);
+			first = false;
+		}
+		return builder.toString();
+	}
 }
