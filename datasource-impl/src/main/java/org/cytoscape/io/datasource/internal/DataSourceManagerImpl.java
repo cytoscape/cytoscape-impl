5c01be412d5102a052c1f825e494b3e3f2b9ec6f
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-20 22:01:16 +0000

    Fixed bug#885

---
+++ datasource-impl/src/main/java/org/cytoscape/io/datasource/internal/DataSourceManagerImpl.java
@@ -0,0 +1,68 @@
+package org.cytoscape.io.datasource.internal;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.cytoscape.io.DataCategory;
+import org.cytoscape.io.datasource.DataSource;
+import org.cytoscape.io.datasource.DataSourceManager;
+
+public final class DataSourceManagerImpl implements DataSourceManager {
+	
+	
+	private final Map<String, DataSource> dataSourceMap;
+	
+	DataSourceManagerImpl() {
+		this.dataSourceMap = new HashMap<String, DataSource>();
+	}
+	
+	/**
+	 * Will be used by OSGi services
+	 * @param datasource
+	 */
+	public void addDataSource(final DataSource datasource, Map metadata) {
+		if(datasource == null)
+			return;
+
+		dataSourceMap.put(datasource.getName(), datasource);
+	}
+	
+	public void removeDataSource(final DataSource datasource, Map metadata) {
+		if(datasource == null)
+			return;
+
+		dataSourceMap.remove(datasource.getName());
+	}
+	
+
+	@Override
+	public Collection<DataSource> getDataSources(DataCategory category) {
+		final Set<DataSource> sources = new HashSet<DataSource>();
+		for(DataSource source: dataSourceMap.values()) {
+			if(source.getDataCategory() == category)
+				sources.add(source);
+		}
+		
+		return sources;
+	}
+
+	@Override
+	public Collection<DataSource> getDataSources(String providerName) {
+		final Set<DataSource> sources = new HashSet<DataSource>();
+		for(DataSource source: dataSourceMap.values()) {
+			if(source.getProvider().equals(providerName))
+				sources.add(source);
+		}
+		
+		return sources;
+	}
+
+	@Override
+	public Collection<DataSource> getAllDataSources() {
+		return dataSourceMap.values();
+	}
+
+}
---
+++ datasource-impl/src/main/java/org/cytoscape/io/datasource/internal/bookmarks/BookmarkDataSourceBuilder.java
@@ -0,0 +1,83 @@
+package org.cytoscape.io.datasource.internal.bookmarks;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.cytoscape.io.DataCategory;
+import org.cytoscape.io.datasource.DefaultDataSource;
+import org.cytoscape.property.CyProperty;
+import org.cytoscape.property.bookmark.Bookmarks;
+import org.cytoscape.property.bookmark.BookmarksUtil;
+import org.cytoscape.property.bookmark.Category;
+import org.cytoscape.property.bookmark.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BookmarkDataSourceBuilder {
+
+	private static final Logger logger = LoggerFactory.getLogger(BookmarkDataSourceBuilder.class);
+
+	private static final Map<String, DataCategory> CONVERSION_MAP;
+
+	static {
+		CONVERSION_MAP = new HashMap<String, DataCategory>();
+		CONVERSION_MAP.put("network", DataCategory.NETWORK);
+		CONVERSION_MAP.put("table", DataCategory.TABLE);
+	}
+
+	private final Bookmarks bookMarks;
+	private final BookmarksUtil bookmarksUtil;
+
+	private final Set<org.cytoscape.io.datasource.DataSource> datasourceSet;
+
+	public BookmarkDataSourceBuilder(final CyProperty<Bookmarks> bookmarkServiceRef, final BookmarksUtil bookmarksUtil) {
+		bookMarks = bookmarkServiceRef.getProperties();
+
+		this.bookmarksUtil = bookmarksUtil;
+		datasourceSet = new HashSet<org.cytoscape.io.datasource.DataSource>();
+		buildDataSource();
+	}
+
+	public Set<org.cytoscape.io.datasource.DataSource> getDataSources() {
+		return this.datasourceSet;
+	}
+
+	private void buildDataSource() {
+
+		final List<Category> categoryList = bookMarks.getCategory();
+
+		for (final Category category : categoryList) {
+			final DataCategory dataType = CONVERSION_MAP.get(category.getName());
+			if (dataType == null)
+				continue;
+
+			final List<DataSource> theDataSourceList = bookmarksUtil
+					.getDataSourceList(category.getName(), categoryList);
+			if (theDataSourceList != null) {
+				for (final DataSource ds : theDataSourceList) {
+					final String location = ds.getHref();
+					final String name = ds.getName();
+					final String description = "From Bookmarks";
+					final String provider = "Legacy";
+					URL url = null;
+					try {
+						url = new URL(location);
+					} catch (MalformedURLException e) {
+						logger.warn("Bookmark file coniatin invalid URL: " + location);
+						continue;
+					}
+
+					final org.cytoscape.io.datasource.DataSource dataSource = new DefaultDataSource(name, provider,
+							description, dataType, url);
+					datasourceSet.add(dataSource);
+				}
+			}
+		}
+	}
+
+}
