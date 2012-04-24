312c883bbd2a5035ddce0a5ec816f0cac615962d
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-24 18:58:28 +0000

    New tests added.

---
+++ webservice-psicquic-client-impl/src/test/java/org/cytoscape/webservice/psicquic/PSICQUICClientTest.java
@@ -0,0 +1,49 @@
+package org.cytoscape.webservice.psicquic;
+
+import static org.junit.Assert.*;
+
+import java.net.URI;
+
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.CyNetworkManager;
+import org.cytoscape.task.NetworkTaskFactory;
+import org.cytoscape.util.swing.OpenBrowser;
+import org.cytoscape.webservice.AbstractWebServiceClientTest;
+import org.cytoscape.work.swing.DialogTaskManager;
+import org.junit.After;
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class PSICQUICClientTest extends AbstractWebServiceClientTest {
+
+	@Mock
+	private OpenBrowser openBrowser;
+	@Mock
+	private DialogTaskManager tm;
+	@Mock
+	CyNetworkFactory cyNetworkFactoryServiceRef;
+	@Mock
+	CyNetworkManager cyNetworkManagerServiceRef;
+	@Mock
+	NetworkTaskFactory createViewTaskFactoryServiceRef;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+
+		final String uriString = "http://www.ebi.ac.uk/Tools/webservices/psicquic/registry/registry";
+		this.locationUri = new URI(uriString);
+		this.description = "PSICUIC Web Service Client";
+		this.displayName = "PSICQUIC";
+		this.queryObject = "brca1 AND brca2";
+		
+		client = new PSICQUICWebServiceClient(uriString, displayName, description, cyNetworkFactoryServiceRef,
+				cyNetworkManagerServiceRef, tm, createViewTaskFactoryServiceRef, openBrowser);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
