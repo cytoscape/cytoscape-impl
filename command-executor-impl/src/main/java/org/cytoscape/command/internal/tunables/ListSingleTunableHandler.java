f88223aa9978d6eee91173ca886e12ad36b9a717
mes <mes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-05-10 23:08:57 +0000

    added better support for different types of objects in list single selection

--- command-executor-impl/src/main/java/org/cytoscape/command/internal/tunables/ListSingleTunableHandler.java
+++ command-executor-impl/src/main/java/org/cytoscape/command/internal/tunables/ListSingleTunableHandler.java
@@ -5,13 +5,28 @@ import org.cytoscape.work.Tunable;
 import org.cytoscape.work.util.ListSingleSelection;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.List;
 
 public class ListSingleTunableHandler extends AbstractStringTunableHandler {
     public ListSingleTunableHandler(Field f, Object o, Tunable t) { super(f,o,t); }
     public ListSingleTunableHandler(Method get, Method set, Object o, Tunable t) { super(get,set,o,t); }
 	public Object processArg(String arg) throws Exception {
 		ListSingleSelection lss = (ListSingleSelection)getValue();
+
+		List<Object> vals = lss.getPossibleValues();
+
+		// hopefully what happens
+		for ( Object o : vals ) {
+			if ( o.toString().equals(arg) ) {
+				lss.setSelectedValue(o);
+				return lss;
+			}
+		}
+
+		// hopefully NOT what happens
+		// only get here if we can't match one of the possible values
 		lss.setSelectedValue((Object)arg);
+
 		return lss;
 	}
 }
