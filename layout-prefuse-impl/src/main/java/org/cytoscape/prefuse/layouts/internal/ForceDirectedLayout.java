9aeb24408498a300c9c25edffb3452100fbca04a
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-06-19 22:52:07 +0000

    Minor source code cleanup only.

--- layout-prefuse-impl/src/main/java/org/cytoscape/prefuse/layouts/internal/ForceDirectedLayout.java
+++ layout-prefuse-impl/src/main/java/org/cytoscape/prefuse/layouts/internal/ForceDirectedLayout.java
@@ -1,9 +1,6 @@
 package org.cytoscape.prefuse.layouts.internal;
 
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.cytoscape.model.CyNode;
@@ -17,18 +14,27 @@ import prefuse.util.force.EulerIntegrator;
 import prefuse.util.force.Integrator;
 import prefuse.util.force.RungeKuttaIntegrator;
 
-
 public class ForceDirectedLayout extends AbstractLayoutAlgorithm {
 
+	private static final String ALGORITHM_ID = "force-directed";
+	private static final String ALGORITHM_DISPLAY_NAME = "Force Directed Layout";
+
 	private Integrators integrator = Integrators.RUNGEKUTTA;
-	
+
 	public enum Integrators {
-		RUNGEKUTTA ("Runge-Kutta"),
-		EULER ("Euler");
+		RUNGEKUTTA("Runge-Kutta"), EULER("Euler");
 
 		private String name;
-		private Integrators(String str) { name=str; }
-		public String toString() { return name; }
+
+		private Integrators(String str) {
+			name = str;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+
 		public Integrator getNewIntegrator() {
 			if (this == EULER)
 				return new EulerIntegrator();
@@ -37,36 +43,34 @@ public class ForceDirectedLayout extends AbstractLayoutAlgorithm {
 		}
 	}
 
-	
-	/**
-	 * Creates a new GridNodeLayout object.
-	 */
 	public ForceDirectedLayout(UndoSupport undo) {
-		super("force-directed", "Force Directed Layout",undo);
+		super(ALGORITHM_ID, ALGORITHM_DISPLAY_NAME, undo);
 	}
 
 	@Override
-	public TaskIterator createTaskIterator(CyNetworkView networkView, Object context, Set<View<CyNode>> nodesToLayOut, String attrName) {
-		return new TaskIterator(new ForceDirectedLayoutTask(getName(), networkView, nodesToLayOut, (ForceDirectedLayoutContext)context, integrator, attrName,undoSupport));
+	public TaskIterator createTaskIterator(CyNetworkView networkView, Object context, Set<View<CyNode>> nodesToLayOut,
+			String attrName) {
+		return new TaskIterator(new ForceDirectedLayoutTask(getName(), networkView, nodesToLayOut,
+				(ForceDirectedLayoutContext) context, integrator, attrName, undoSupport));
 	}
-	
+
 	@Override
 	public Object createLayoutContext() {
 		return new ForceDirectedLayoutContext();
 	}
-	
-	public Set<Class<?>> getSupportedEdgeAttributeTypes() {
-		Set<Class<?>> ret = new HashSet<Class<?>>();
 
-		ret.add( Integer.class );
-		ret.add( Double.class );
+	@Override
+	public Set<Class<?>> getSupportedEdgeAttributeTypes() {
+		final Set<Class<?>> ret = new HashSet<Class<?>>();
+
+		ret.add(Integer.class);
+		ret.add(Double.class);
 
 		return ret;
 	}
-	
+
 	@Override
 	public boolean getSupportsSelectedOnly() {
 		return true;
 	}
-
 }
