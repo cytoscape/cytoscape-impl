db51a7d61e599b0dff3228ea54326bf7f0796884
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-07-05 20:28:38 +0000

    Fixes #1217: Fixed CyAnnotator leaks

--- ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/impl/cyannotator/tasks/RemoveAnnotationTaskFactory.java
+++ ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/impl/cyannotator/tasks/RemoveAnnotationTaskFactory.java
@@ -2,34 +2,28 @@ package org.cytoscape.ding.impl.cyannotator.tasks;
 
 
 import java.awt.geom.Point2D;
-import java.awt.datatransfer.Transferable;
-
-import org.cytoscape.task.NetworkViewLocationTaskFactory;
-import org.cytoscape.view.model.CyNetworkView;
-import org.cytoscape.work.TaskIterator;
 
 import org.cytoscape.ding.impl.DGraphView;
 import org.cytoscape.ding.impl.cyannotator.CyAnnotator;
 import org.cytoscape.ding.impl.cyannotator.api.Annotation;
-import org.cytoscape.ding.impl.cyannotator.api.ImageAnnotation;
-import org.cytoscape.ding.impl.cyannotator.api.ShapeAnnotation;
+import org.cytoscape.task.NetworkViewLocationTaskFactory;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.work.TaskIterator;
 
 public class RemoveAnnotationTaskFactory implements NetworkViewLocationTaskFactory {
-	private CyAnnotator cyAnnotator;
-	private Annotation annotation;
 	
 	@Override
 	public TaskIterator createTaskIterator(CyNetworkView networkView, Point2D javaPt, Point2D xformPt) {
-		this.cyAnnotator = ((DGraphView)networkView).getCyAnnotator();
-		annotation = cyAnnotator.getAnnotationAt(javaPt);
+		CyAnnotator cyAnnotator = ((DGraphView)networkView).getCyAnnotator();
+		Annotation annotation = cyAnnotator.getAnnotationAt(javaPt);
 		return new TaskIterator(new RemoveAnnotationTask(networkView, annotation));
 
 	}
 
 	@Override
 	public boolean isReady(CyNetworkView networkView, Point2D javaPt, Point2D xformPt) {
-		this.cyAnnotator = ((DGraphView)networkView).getCyAnnotator();
-		annotation = cyAnnotator.getAnnotationAt(javaPt);
+		CyAnnotator cyAnnotator = ((DGraphView)networkView).getCyAnnotator();
+		Annotation annotation = cyAnnotator.getAnnotationAt(javaPt);
 		if (annotation != null)
 			return true;
 		return false;
