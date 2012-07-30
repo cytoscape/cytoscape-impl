a9a5ad6feeae5fe7d35b734befab00f3adecc169
scooter <scooter@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-07-30 00:04:39 +0000

    Update menu properties for layouts

--- layout-cytoscape-impl/src/main/java/csapps/layout/CyActivator.java
+++ layout-cytoscape-impl/src/main/java/csapps/layout/CyActivator.java
@@ -38,83 +38,90 @@ public class CyActivator extends AbstractCyActivator {
 		HandleFactory hf = getService(bc,HandleFactory.class);
 		BendFactory bf = getService(bc,BendFactory.class);
 
-		CircularLayoutAlgorithm circularLayoutAlgorithm = new CircularLayoutAlgorithm(undo);
-		HierarchicalLayoutAlgorithm hierarchicalLayoutAlgorithm = new HierarchicalLayoutAlgorithm(undo,hf,bf);
-		AttributeCircleLayout attributeCircleLayout = new AttributeCircleLayout(undo);
-		DegreeSortedCircleLayout degreeSortedCircleLayout = new DegreeSortedCircleLayout(undo);
-		ISOMLayout ISOMLayout = new ISOMLayout(undo);
 		// BioLayoutKKAlgorithm bioLayoutKKAlgorithmFALSE = new BioLayoutKKAlgorithm(false,undo);
-		BioLayoutKKAlgorithm bioLayoutKKAlgorithmTRUE = new BioLayoutKKAlgorithm(true,undo);
-		BioLayoutFRAlgorithm bioLayoutFRAlgorithm = new BioLayoutFRAlgorithm(true,undo);
-		StackedNodeLayout stackedNodeLayout = new StackedNodeLayout(undo);
-		GroupAttributesLayout groupAttributesLayout = new GroupAttributesLayout(undo);
 		
+		HierarchicalLayoutAlgorithm hierarchicalLayoutAlgorithm = new HierarchicalLayoutAlgorithm(undo,hf,bf);
+		Properties hierarchicalLayoutAlgorithmProps = new Properties();
+		hierarchicalLayoutAlgorithmProps.setProperty(PREFERRED_MENU, "Layout.Cytoscape Layouts");
+		hierarchicalLayoutAlgorithmProps.setProperty("preferredTaskManager","menu");
+		hierarchicalLayoutAlgorithmProps.setProperty(TITLE,hierarchicalLayoutAlgorithm.toString());
+		hierarchicalLayoutAlgorithmProps.setProperty(MENU_GRAVITY,"10.1");
+		registerService(bc,hierarchicalLayoutAlgorithm,CyLayoutAlgorithm.class, hierarchicalLayoutAlgorithmProps);
 		
+		CircularLayoutAlgorithm circularLayoutAlgorithm = new CircularLayoutAlgorithm(undo);
 		Properties circularLayoutAlgorithmProps = new Properties();
-		circularLayoutAlgorithmProps.setProperty(PREFERRED_MENU,"Layout.Cytoscape Layouts");
+		circularLayoutAlgorithmProps.setProperty(PREFERRED_MENU, "Layout.Cytoscape Layouts");
 		circularLayoutAlgorithmProps.setProperty("preferredTaskManager","menu");
 		circularLayoutAlgorithmProps.setProperty(TITLE,circularLayoutAlgorithm.toString());
 		circularLayoutAlgorithmProps.setProperty(MENU_GRAVITY,"10.2");
-		
 		registerService(bc,circularLayoutAlgorithm,CyLayoutAlgorithm.class, circularLayoutAlgorithmProps);
-		
-		Properties hierarchicalLayoutAlgorithmProps = new Properties();
-		hierarchicalLayoutAlgorithmProps.setProperty(PREFERRED_MENU,"Layout.Cytoscape Layouts");
-		hierarchicalLayoutAlgorithmProps.setProperty("preferredTaskManager","menu");
-		hierarchicalLayoutAlgorithmProps.setProperty(TITLE,hierarchicalLayoutAlgorithm.toString());
-		hierarchicalLayoutAlgorithmProps.setProperty(MENU_GRAVITY,"10.8");
-		registerService(bc,hierarchicalLayoutAlgorithm,CyLayoutAlgorithm.class, hierarchicalLayoutAlgorithmProps);
 
+		StackedNodeLayout stackedNodeLayout = new StackedNodeLayout(undo);
+		Properties stackedNodeLayoutProps = new Properties();
+		stackedNodeLayoutProps.setProperty(PREFERRED_MENU, "Layout.Cytoscape Layouts");
+		stackedNodeLayoutProps.setProperty("preferredTaskManager","menu");
+		stackedNodeLayoutProps.setProperty(TITLE,stackedNodeLayout.toString());
+		stackedNodeLayoutProps.setProperty(MENU_GRAVITY,"10.3");
+		stackedNodeLayoutProps.setProperty(INSERT_SEPARATOR_AFTER,"true");
+		registerService(bc,stackedNodeLayout,CyLayoutAlgorithm.class, stackedNodeLayoutProps);
+
+		AttributeCircleLayout attributeCircleLayout = new AttributeCircleLayout(undo);
 		Properties attributeCircleLayoutProps = new Properties();
-		attributeCircleLayoutProps.setProperty(PREFERRED_MENU,"Layout.Cytoscape Layouts");
+		attributeCircleLayoutProps.setProperty(PREFERRED_MENU, "Layout.Cytoscape Layouts");
 		attributeCircleLayoutProps.setProperty("preferredTaskManager","menu");
 		attributeCircleLayoutProps.setProperty(TITLE,attributeCircleLayout.toString());
-		attributeCircleLayoutProps.setProperty(MENU_GRAVITY,"10.1");
+		attributeCircleLayoutProps.setProperty(MENU_GRAVITY,"10.4");
 		registerService(bc,attributeCircleLayout,CyLayoutAlgorithm.class, attributeCircleLayoutProps);
 
+		DegreeSortedCircleLayout degreeSortedCircleLayout = new DegreeSortedCircleLayout(undo);
 		Properties degreeSortedCircleLayoutProps = new Properties();
-		degreeSortedCircleLayoutProps.setProperty(PREFERRED_MENU,"Layout.Cytoscape Layouts");
+		degreeSortedCircleLayoutProps.setProperty(PREFERRED_MENU, "Layout.Cytoscape Layouts");
 		degreeSortedCircleLayoutProps.setProperty("preferredTaskManager","menu");
 		degreeSortedCircleLayoutProps.setProperty(TITLE,degreeSortedCircleLayout.toString());
-		degreeSortedCircleLayoutProps.setProperty(MENU_GRAVITY,"10.3");
+		degreeSortedCircleLayoutProps.setProperty(MENU_GRAVITY,"10.5");
 		registerService(bc,degreeSortedCircleLayout,CyLayoutAlgorithm.class, degreeSortedCircleLayoutProps);
 
-		Properties ISOMLayoutProps = new Properties();
-		ISOMLayoutProps.setProperty(PREFERRED_MENU,"Layout.Cytoscape Layouts");
-		ISOMLayoutProps.setProperty("preferredTaskManager","menu");
-		ISOMLayoutProps.setProperty(TITLE,ISOMLayout.toString());
-		ISOMLayoutProps.setProperty(MENU_GRAVITY,"10.9");
-		registerService(bc,ISOMLayout,CyLayoutAlgorithm.class, ISOMLayoutProps);
+		GroupAttributesLayout groupAttributesLayout = new GroupAttributesLayout(undo);
+		Properties groupAttributesLayoutProps = new Properties();
+		groupAttributesLayoutProps.setProperty(PREFERRED_MENU, "Layout.Cytoscape Layouts");
+		groupAttributesLayoutProps.setProperty("preferredTaskManager","menu");
+		groupAttributesLayoutProps.setProperty(TITLE,groupAttributesLayout.toString());
+		groupAttributesLayoutProps.setProperty(MENU_GRAVITY,"10.6");
+		registerService(bc,groupAttributesLayout,CyLayoutAlgorithm.class, groupAttributesLayoutProps);
 
 		/*
 		Properties bioLayoutKKAlgorithmFALSEProps = new Properties();
-		bioLayoutKKAlgorithmFALSEProps.setProperty(PREFERRED_MENU,"Layout.Cytoscape Layouts");
+		bioLayoutKKAlgorithmFALSEProps.setProperty(PREFERRED_MENU, "Layout.Cytoscape Layouts");
 		bioLayoutKKAlgorithmFALSEProps.setProperty("preferredTaskManager","menu");
 		bioLayoutKKAlgorithmFALSEProps.setProperty(TITLE,bioLayoutKKAlgorithmFALSE.toString());
 		bioLayoutKKAlgorithmFALSEProps.setProperty(MENU_GRAVITY,"10.99");
 		registerService(bc,bioLayoutKKAlgorithmFALSE,CyLayoutAlgorithm.class, bioLayoutKKAlgorithmFALSEProps);
 		*/
 		
-		Properties bioLayoutKKAlgorithmTRUEProps = new Properties();
-		bioLayoutKKAlgorithmTRUEProps.setProperty(PREFERRED_MENU,"Layout.Cytoscape Layouts");
-		bioLayoutKKAlgorithmTRUEProps.setProperty("preferredTaskManager","menu");
-		bioLayoutKKAlgorithmTRUEProps.setProperty(TITLE,bioLayoutKKAlgorithmTRUE.toString());
-		bioLayoutKKAlgorithmTRUEProps.setProperty(MENU_GRAVITY,"10.5");
-		registerService(bc,bioLayoutKKAlgorithmTRUE,CyLayoutAlgorithm.class, bioLayoutKKAlgorithmTRUEProps);
-		
+		BioLayoutFRAlgorithm bioLayoutFRAlgorithm = new BioLayoutFRAlgorithm(true,undo);
 		Properties bioLayoutFRAlgorithmProps = new Properties();
-		bioLayoutFRAlgorithmProps.setProperty(PREFERRED_MENU,"Layout.Cytoscape Layouts");
+		bioLayoutFRAlgorithmProps.setProperty(PREFERRED_MENU, "Layout.Cytoscape Layouts");
 		bioLayoutFRAlgorithmProps.setProperty("preferredTaskManager","menu");
 		bioLayoutFRAlgorithmProps.setProperty(TITLE,bioLayoutFRAlgorithm.toString());
-		bioLayoutFRAlgorithmProps.setProperty(MENU_GRAVITY,"10.4");
+		bioLayoutFRAlgorithmProps.setProperty(MENU_GRAVITY,"10.8");
 		registerService(bc,bioLayoutFRAlgorithm,CyLayoutAlgorithm.class, bioLayoutFRAlgorithmProps);
+		
+		BioLayoutKKAlgorithm bioLayoutKKAlgorithmTRUE = new BioLayoutKKAlgorithm(true,undo);
+		Properties bioLayoutKKAlgorithmTRUEProps = new Properties();
+		bioLayoutKKAlgorithmTRUEProps.setProperty(PREFERRED_MENU, "Layout.Cytoscape Layouts");
+		bioLayoutKKAlgorithmTRUEProps.setProperty("preferredTaskManager","menu");
+		bioLayoutKKAlgorithmTRUEProps.setProperty(TITLE,bioLayoutKKAlgorithmTRUE.toString());
+		bioLayoutKKAlgorithmTRUEProps.setProperty(MENU_GRAVITY,"10.9");
+		bioLayoutKKAlgorithmTRUEProps.setProperty(INSERT_SEPARATOR_AFTER,"true");
+		registerService(bc,bioLayoutKKAlgorithmTRUE,CyLayoutAlgorithm.class, bioLayoutKKAlgorithmTRUEProps);
 
-		Properties groupAttributesLayoutProps = new Properties();
-		groupAttributesLayoutProps.setProperty(PREFERRED_MENU,"Layout.Cytoscape Layouts");
-		groupAttributesLayoutProps.setProperty("preferredTaskManager","menu");
-		groupAttributesLayoutProps.setProperty(TITLE,groupAttributesLayout.toString());
-		groupAttributesLayoutProps.setProperty(MENU_GRAVITY,"10.6");
-		registerService(bc,groupAttributesLayout,CyLayoutAlgorithm.class, groupAttributesLayoutProps);
+		ISOMLayout ISOMLayout = new ISOMLayout(undo);
+		Properties ISOMLayoutProps = new Properties();
+		ISOMLayoutProps.setProperty(PREFERRED_MENU, "Layout.Cytoscape Layouts");
+		ISOMLayoutProps.setProperty("preferredTaskManager","menu");
+		ISOMLayoutProps.setProperty(TITLE,ISOMLayout.toString());
+		ISOMLayoutProps.setProperty(MENU_GRAVITY,"10.99");
+		registerService(bc,ISOMLayout,CyLayoutAlgorithm.class, ISOMLayoutProps);
 	}
 }
 
