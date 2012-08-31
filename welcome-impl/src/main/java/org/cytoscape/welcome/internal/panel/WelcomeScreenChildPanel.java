104675952159c8cb0a16affcc6b136748ae20f1b
rozagh <rozagh@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-08-31 22:09:15 +0000

    fixes #1417 changing the font colors.

--- welcome-impl/src/main/java/org/cytoscape/welcome/internal/panel/WelcomeScreenChildPanel.java
+++ welcome-impl/src/main/java/org/cytoscape/welcome/internal/panel/WelcomeScreenChildPanel.java
@@ -11,9 +11,9 @@ public interface WelcomeScreenChildPanel {
 	public static final Font LINK_FONT = new Font(Font.DIALOG, Font.PLAIN, 12);
 	public static final Font TITLE_FONT = new Font("HelveticaNeue-UltraLight", Font.PLAIN, 14);
 	
-	public static final Color LINK_FONT_COLOR = new Color(0x87, 0xCE, 0xFF);
-	public static final Color REGULAR_FONT_COLOR = new Color(0x72, 0x72, 0x72);
-	public static final Color COMMAND_FONT_COLOR = new Color(0x52, 0x52, 0x52);
+	public static final Color LINK_FONT_COLOR = new Color(0x00, 0x00, 0xCD);
+	public static final Color REGULAR_FONT_COLOR = new Color(0x2F, 0x4F, 0x4f);
+	public static final Color COMMAND_FONT_COLOR = new Color(0x2F, 0x4F, 0x4f);
 	public static final Color TITLE_FONT_COLOR = new Color(0xF5, 0xF5, 0xF5);
 	public static final Color TITLE_BG_COLOR = new Color(0xa0, 0xa0, 0xa0, 210);
 
