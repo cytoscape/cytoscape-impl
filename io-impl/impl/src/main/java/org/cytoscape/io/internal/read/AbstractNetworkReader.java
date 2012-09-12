4f03dc4256dc1e0fed3a3790ea44cf07e709734e
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-09-12 23:26:21 +0000

    Add a new method

--- io-impl/impl/src/main/java/org/cytoscape/io/internal/read/AbstractNetworkReader.java
+++ io-impl/impl/src/main/java/org/cytoscape/io/internal/read/AbstractNetworkReader.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.cytoscape.io.internal.util.ReadUtils;
 import org.cytoscape.io.read.CyNetworkReader;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNetworkFactory;
 import org.cytoscape.model.CyNetworkManager;
+import org.cytoscape.model.CyNode;
 import org.cytoscape.model.subnetwork.CyRootNetwork;
 import org.cytoscape.model.subnetwork.CyRootNetworkManager;
 import org.cytoscape.view.model.CyNetworkViewFactory;
@@ -18,6 +20,7 @@ import org.cytoscape.view.vizmap.VisualStyle;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.Tunable;
 import org.cytoscape.work.util.ListSingleSelection;
+import org.cytoscape.model.subnetwork.CySubNetwork;
 
 public abstract class AbstractNetworkReader extends AbstractTask implements CyNetworkReader {
 
@@ -36,6 +39,7 @@ public abstract class AbstractNetworkReader extends AbstractTask implements CyNe
 	public ListSingleSelection<Object> networkCollection;
 
 	protected HashMap<String, CyRootNetwork> name2RootMap;
+	protected Map<String, CyNode> nMap = new HashMap<String, CyNode>(10000);
 
 	public AbstractNetworkReader(InputStream inputStream, final CyNetworkViewFactory cyNetworkViewFactory,
 			final CyNetworkFactory cyNetworkFactory, final CyNetworkManager cyNetworkManager, final CyRootNetworkManager cyRootNetworkManager) {
@@ -68,5 +72,20 @@ public abstract class AbstractNetworkReader extends AbstractTask implements CyNe
 	@Override
 	public CyNetwork[] getNetworks() {
 		return cyNetworks;
+	}
+	
+	protected void initNodeMap(CyNetwork rootNetwork){		
+		Iterator<CySubNetwork> subnetworkIt = cyRootNetworkManager.getRootNetwork(rootNetwork).getSubNetworkList().iterator();
+		
+		while(subnetworkIt.hasNext()){
+			CySubNetwork subnetwork = subnetworkIt.next();
+			
+			Iterator<CyNode> nodeIt = subnetwork.getNodeList().iterator();
+			while(nodeIt.hasNext()){
+				CyNode node = nodeIt.next();
+				String name = subnetwork.getDefaultNodeTable().getRow(node).get(CyNetwork.NAME, String.class);
+				this.nMap.put(name, node);				
+			}
+		}
 	}
 }
