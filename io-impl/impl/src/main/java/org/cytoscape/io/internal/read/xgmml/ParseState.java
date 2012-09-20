510221767a6189958c8942dcdd7c045cca7ca86a
apico <apico@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-09-20 03:03:20 +0000

    #1466

--- io-impl/impl/src/main/java/org/cytoscape/io/internal/read/xgmml/ParseState.java
+++ io-impl/impl/src/main/java/org/cytoscape/io/internal/read/xgmml/ParseState.java
@@ -3,23 +3,23 @@ package org.cytoscape.io.internal.read.xgmml;
 public enum ParseState {
     NONE("none"),
     RDF("RDF"),
-    NET_ATT("Network Attribute"),
-    NODE_ATT("Node Attribute"),
-    EDGE_ATT("Edge Attribute"),
+    NET_ATT("Network Table Column"),
+    NODE_ATT("Node Table Column"),
+    EDGE_ATT("Edge Table Column"),
 
     // Types of attributes that require special handling
-    LIST_ATT("List Attribute"),
+    LIST_ATT("List Column Type"),
     LIST_ELEMENT("List Element"),
     NET_GRAPHICS("Network Graphics"),
     NODE_GRAPH("Node Graph"),
     NODE_GRAPHICS("Node Graphics"),
     EDGE_GRAPHICS("Edge Graphics"),
-    LOCKED_VISUAL_PROP_ATT("Bypass Attribute"),
+    LOCKED_VISUAL_PROP_ATT("Bypass Column"),
 
     // Handle edge handles
     EDGE_BEND("Edge Bend"),
     EDGE_HANDLE("Edge Handle"),
-    EDGE_HANDLE_ATT("Edge Handle Attribute"),
+    EDGE_HANDLE_ATT("Edge Handle Column"),
     NODE("Node Element"),
     EDGE("Edge Element"),
     GRAPH("Graph Element"),
