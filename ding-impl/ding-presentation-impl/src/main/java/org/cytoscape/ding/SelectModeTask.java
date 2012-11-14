e14d70c17767aed9eec0bf883584b242914d6f2a
clopes <clopes@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-11-14 16:54:41 +0000

    SelectModeTask now uses the visual properties NETWORK_NODE_SELECTION and NETWORK_EDGE_SELECTION (as bypass), instead of modifying DGraphView directly.

--- ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/SelectModeTask.java
+++ ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/SelectModeTask.java
@@ -1,43 +1,34 @@
 package org.cytoscape.ding;
 
 import org.cytoscape.application.CyApplicationManager;
+import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.TaskMonitor;
-import org.cytoscape.view.model.CyNetworkView;
-import org.cytoscape.ding.impl.DGraphView;
 
 public class SelectModeTask extends AbstractTask {
 
-	private CyApplicationManager applicationManagerServiceRef;
-	private String actionName;
+	private final CyApplicationManager applicationManagerServiceRef;
+	private final String actionName;
 
-	public SelectModeTask(String actionName, CyApplicationManager applicationManagerServiceRef){
+	public SelectModeTask(final String actionName, final CyApplicationManager applicationManagerServiceRef){
 		this.applicationManagerServiceRef = applicationManagerServiceRef;
 		this.actionName = actionName;
 	}
 	
 	@Override
-	public void run(TaskMonitor taskMonitor) {
-		
-		CyNetworkView view = this.applicationManagerServiceRef.getCurrentNetworkView();
+	public void run(final TaskMonitor taskMonitor) {
+		final CyNetworkView view = this.applicationManagerServiceRef.getCurrentNetworkView();
 
-		if (view == null){
-			return;
-		}
-		
-		if (view instanceof DGraphView){
-			DGraphView dView = (DGraphView)view;			
-			if (this.actionName.equalsIgnoreCase("Nodes only")){
-				dView.enableNodeSelection();
-				dView.disableEdgeSelection();				
-			}
-			else if (this.actionName.equalsIgnoreCase("Edges only")){
-				dView.disableNodeSelection();
-				dView.enableEdgeSelection();					
-			}
-			else if (this.actionName.equalsIgnoreCase("Nodes and Edges")){
-				dView.enableNodeSelection();
-				dView.enableEdgeSelection();	
+		if (view != null) {
+			if (actionName.equalsIgnoreCase("Nodes only")) {
+				view.clearValueLock(DVisualLexicon.NETWORK_NODE_SELECTION);
+				view.setLockedValue(DVisualLexicon.NETWORK_EDGE_SELECTION, Boolean.FALSE);
+			} else if (actionName.equalsIgnoreCase("Edges only")) {
+				view.clearValueLock(DVisualLexicon.NETWORK_EDGE_SELECTION);
+				view.setLockedValue(DVisualLexicon.NETWORK_NODE_SELECTION, Boolean.FALSE);
+			} else if (actionName.equalsIgnoreCase("Nodes and Edges")) {
+				view.clearValueLock(DVisualLexicon.NETWORK_NODE_SELECTION);
+				view.clearValueLock(DVisualLexicon.NETWORK_EDGE_SELECTION);
 			}
 		}
 	}
