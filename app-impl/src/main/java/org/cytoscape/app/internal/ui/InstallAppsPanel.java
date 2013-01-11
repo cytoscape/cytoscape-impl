35038fd3391e419c6a38e56c100e747a2ef76225
thully <thully@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2013-01-11 17:51:08 +0000

    Fix for #1642 - disable multiple selection for now. We may want to come back to it later, though that would require new methods to be implemented.

--- app-impl/src/main/java/org/cytoscape/app/internal/ui/InstallAppsPanel.java
+++ app-impl/src/main/java/org/cytoscape/app/internal/ui/InstallAppsPanel.java
@@ -280,6 +280,7 @@ public class InstallAppsPanel extends javax.swing.JPanel {
         tagsTree.setModel(new javax.swing.tree.DefaultTreeModel(treeNode1));
         tagsTree.setFocusable(false);
         tagsTree.setRootVisible(false);
+        tagsTree.getSelectionModel().setSelectionMode(javax.swing.tree.TreeSelectionModel.SINGLE_TREE_SELECTION);
         tagsScrollPane.setViewportView(tagsTree);
 
         tagsSplitPane.setLeftComponent(tagsScrollPane);
@@ -288,6 +289,7 @@ public class InstallAppsPanel extends javax.swing.JPanel {
         resultsTree.setModel(new javax.swing.tree.DefaultTreeModel(treeNode1));
         resultsTree.setFocusable(false);
         resultsTree.setRootVisible(false);
+        resultsTree.getSelectionModel().setSelectionMode(javax.swing.tree.TreeSelectionModel.SINGLE_TREE_SELECTION);
         resultsScrollPane.setViewportView(resultsTree);
 
         tagsSplitPane.setRightComponent(resultsScrollPane);
