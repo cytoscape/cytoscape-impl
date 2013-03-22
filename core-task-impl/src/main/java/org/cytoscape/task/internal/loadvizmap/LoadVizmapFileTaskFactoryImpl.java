e6329138d033176d564d28cbd9dbdf31a5c6cb62
keiono <keiono@gmail.com>
2013-03-22 13:44:25 -0700

    fixes #1785 Correct extension will be given to the temp VizMap file.
    Change-Id: I2d9371dabcda26eed77b5fedc701e22c57b85343

--- core-task-impl/src/main/java/org/cytoscape/task/internal/loadvizmap/LoadVizmapFileTaskFactoryImpl.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/loadvizmap/LoadVizmapFileTaskFactoryImpl.java
@@ -34,6 +34,7 @@ import java.io.InputStreamReader;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -87,9 +88,19 @@ public class LoadVizmapFileTaskFactoryImpl extends AbstractTaskFactory implement
 		return task.getStyles();
 	}
 
-	public Set<VisualStyle> loadStyles(InputStream is) {
+	public Set<VisualStyle> loadStyles(final InputStream is) {
 		// Save the contents of inputStream in a tmp file
-		File f = this.getFileFromStream(is);
+		File f = null;
+		try {
+			f = this.getFileFromStream(is);
+		} catch (IOException e) {
+			throw new IllegalStateException("Could not create temp file", e);
+		}
+
+		if (f == null)
+			throw new NullPointerException("Could not create temp file.");
+
+		
 		return this.loadStyles(f);
 	}
 
@@ -103,12 +114,12 @@ public class LoadVizmapFileTaskFactoryImpl extends AbstractTaskFactory implement
 	}
 
 	// Read the inputStream and save the content in a tmp file
-	private File getFileFromStream(InputStream is) {
+	private File getFileFromStream(final InputStream is) throws IOException {
 
 		File returnFile = null;
 
 		// Get the contents from inputStream
-		ArrayList<String> list = new ArrayList<String>();
+		final List<String> list = new ArrayList<String>();
 
 		BufferedReader bf = null;
 		String line;
@@ -136,7 +147,7 @@ public class LoadVizmapFileTaskFactoryImpl extends AbstractTaskFactory implement
 		// Save the content to a tmp file
 		Writer output = null;
 		try {
-			returnFile = File.createTempFile("visualStyles", ".props", new File(System.getProperty("java.io.tmpdir")));
+			returnFile = File.createTempFile("visualStyles", "", new File(System.getProperty("java.io.tmpdir")));
 			returnFile.deleteOnExit();
 
 			// use buffering
@@ -159,6 +170,41 @@ public class LoadVizmapFileTaskFactoryImpl extends AbstractTaskFactory implement
 
 			}
 		}
-		return returnFile;
+
+		if (returnFile == null)
+			throw new NullPointerException("Could not create temp VizMap file.");
+
+		final String originalFileName = returnFile.getAbsolutePath();
+		if (isXML(list)) {
+			final File xmlFile = new File(originalFileName + ".xml");
+			final boolean renamed = returnFile.renameTo(xmlFile);
+			if (renamed)
+				return xmlFile;
+			else
+				throw new IOException("Could not create temp vizmap file: " + xmlFile);
+		} else {
+			// Return ad legacy property format
+			final File propFile = new File(originalFileName + ".props");
+			final boolean renamed = returnFile.renameTo(propFile);
+			if (renamed)
+				return propFile;
+			else
+				throw new IOException("Could not create temp vizmap file: " + propFile);
+		}
+	}
+
+	/**
+	 * Perform simple test whether this file is XML or not.
+	 * 
+	 * @return
+	 */
+	private final boolean isXML(final List<String> list) {
+		if(list == null || list.isEmpty())
+			return false;
+		
+		if(list.get(0).contains("<?xml"))
+			return true;
+		else
+			return false;
 	}
 }
