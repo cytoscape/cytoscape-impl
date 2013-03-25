d2814ab121dbfc912ebed66571ce463aaf2a3718
keiono <keiono@gmail.com>
2013-03-25 14:20:26 -0700

    fixes #1853 SIF Writer uses UTF-8 by default.
    Change-Id: I45309bc813c1e01dca886df98df1a840b2f1f456

--- io-impl/impl/src/main/java/org/cytoscape/io/internal/write/sif/SifWriter.java
+++ io-impl/impl/src/main/java/org/cytoscape/io/internal/write/sif/SifWriter.java
@@ -24,85 +24,115 @@ package org.cytoscape.io.internal.write.sif;
  * #L%
  */
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
 import java.util.List;
+import java.util.SortedMap;
 
 import org.cytoscape.io.write.CyWriter;
 import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNode;
 import org.cytoscape.work.TaskMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SifWriter implements CyWriter {
 
-	// TODO this should come from model-api
-	private static final String NODE_NAME_ATTR_LABEL = "name";
-	private static final String INTERACTION_ATTR_LABEL = "interaction";
-	
-	private OutputStream outputStream;
-	private CyNetwork network;
+	private static final Logger logger = LoggerFactory.getLogger(SifWriter.class);
 
-	public SifWriter(OutputStream outputStream, CyNetwork network) {
+	private static final String DEFAULT_INTERACTION = "-";
+	private static final String ENCODING = "UTF-8";
+
+	private final OutputStream outputStream;
+	private final CyNetwork network;
+
+	private final CharsetEncoder encoder;
+
+	public SifWriter(final OutputStream outputStream, final CyNetwork network) {
 		this.outputStream = outputStream;
-		this.network = network; 
+		this.network = network;
+
+		if(Charset.isSupported(ENCODING)) {
+			// UTF-8 is supported by system
+			this.encoder = Charset.forName(ENCODING).newEncoder();
+		} else {
+			// Use default.
+			logger.warn("UTF-8 is not supported by this system.  This can be a problem for non-English annotations.");
+			this.encoder = Charset.defaultCharset().newEncoder();
+		}
 	}
 
 	@Override
 	public void run(TaskMonitor taskMonitor) throws Exception {
-		taskMonitor.setProgress(0.0);
-
-		final Writer writer = new OutputStreamWriter(outputStream);
-		try {
-			final String lineSep = System.getProperty("line.separator");
-			final List<CyNode> nodeList = network.getNodeList();
-	
-			int i = 0;
-			for ( CyNode node : nodeList ) {
-				if (taskMonitor != null) {
-					//  Report on Progress
-					double percent = ((double) i++ / nodeList.size());
-					taskMonitor.setProgress(percent);
-				}
-	
-				String canonicalName = network.getRow(node).get(NODE_NAME_ATTR_LABEL, String.class);
-				List<CyEdge> edges = network.getAdjacentEdgeList(node, CyEdge.Type.ANY);
-	
-				if (edges.size() == 0) {
-					writer.write(canonicalName + lineSep);
-				} else {
-					for ( CyEdge edge : edges ) {
-	
-						if (node == edge.getSource()) { //do only for outgoing edges
-							CyNode target = edge.getTarget();
-							String canonicalTargetName = network.getRow(target).get(NODE_NAME_ATTR_LABEL,String.class);
-							String interactionName = network.getRow(edge).get(INTERACTION_ATTR_LABEL,String.class);
-	
-							if (interactionName == null) {
-								interactionName = "xx";
-							}
-	
-							writer.write(canonicalName);
-							writer.write("\t");
-							writer.write(interactionName);
-							writer.write("\t");
-							writer.write(canonicalTargetName);
-							writer.write(lineSep);
-						}
-					} 
-				} 
-			} 
-		} finally {
-			writer.close();
+		if (taskMonitor != null) {
+			taskMonitor.setProgress(0.0);
+			taskMonitor.setTitle("Exporting to SIF");
+			taskMonitor.setStatusMessage("Exporting current network as SIF...");
 		}
-		taskMonitor.setProgress(1.0);
+
+		System.out.println("Encoding = " + encoder.charset());
+		final OutputStreamWriter writer = new OutputStreamWriter(outputStream, encoder);
+
+		final String lineSep = System.getProperty("line.separator");
+		final List<CyNode> nodeList = network.getNodeList();
+
+		int i = 0;
+		for (CyNode node : nodeList) {
+			if (taskMonitor != null) {
+				final double percent = ((double) i++ / nodeList.size());
+				taskMonitor.setProgress(percent);
+			}
+			
+			final String sourceName = network.getRow(node).get(CyNetwork.NAME, String.class);
+			final List<CyEdge> edges = network.getAdjacentEdgeList(node, CyEdge.Type.ANY);
+			if(sourceName == null || sourceName.length() == 0)
+				throw new IllegalStateException("This network contains null or empty node name.");
+			
+
+			if (edges.size() == 0) {
+				writer.write(sourceName + lineSep);
+			} else {
+				for (final CyEdge edge : edges) {
+
+					if (node == edge.getSource()) { 
+						// Do only for outgoing edges
+						final CyNode target = edge.getTarget();
+						final String targetName = network.getRow(target).get(CyNetwork.NAME, String.class);
+						if(targetName == null || targetName.length() == 0)
+							throw new IllegalStateException("This network contains null or empty node name.");
+						
+						String interactionName = network.getRow(edge).get(CyEdge.INTERACTION, String.class);
+						if (interactionName == null)
+							interactionName = DEFAULT_INTERACTION;
+
+						writer.write(sourceName);
+						writer.write("\t");
+						writer.write(interactionName);
+						writer.write("\t");
+						writer.write(targetName);
+						writer.write(lineSep);
+					}
+				}
+			}
+		}
+
+		writer.close();
+		outputStream.close();
 	}
-	
+
 	@Override
 	public void cancel() {
-		// TODO Auto-generated method stub
+		if (outputStream == null)
+			return;
 
+		try {
+			outputStream.close();
+		} catch (IOException e) {
+			logger.error("Could not close Outputstream for SifWriter.", e);
+		}
 	}
-
 }
