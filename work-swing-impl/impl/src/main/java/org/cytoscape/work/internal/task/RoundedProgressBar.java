d255773da498d45560bf3eb881aa50f3ac68478c
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2013-07-11 14:24:20 +0000

    Slightly increased background color of rounded progress bar to improve appearance on Linux L&F

--- work-swing-impl/impl/src/main/java/org/cytoscape/work/internal/task/RoundedProgressBar.java
+++ work-swing-impl/impl/src/main/java/org/cytoscape/work/internal/task/RoundedProgressBar.java
@@ -16,7 +16,7 @@ public class RoundedProgressBar extends JComponent {
 	protected static final float CORNER_RADIUS = 5.2f;
 	protected static final float HEIGHT = 6.0f;
 	protected static final Color FG_COLOR = new Color(0x527057);
-	protected static final Color BK_COLOR = new Color(0xd3d3d3);
+	protected static final Color BK_COLOR = new Color(0xc7c7c7);
 	protected static final Color INDET_FG_COLOR = FG_COLOR;
 	protected static final float INDET_BAR_WIDTH = 100.0f;
 	protected static final int INDET_UPDATE_MS = 50;
