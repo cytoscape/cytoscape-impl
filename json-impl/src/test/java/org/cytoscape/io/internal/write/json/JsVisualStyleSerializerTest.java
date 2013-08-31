c286f574806806d6698841ff8fd32b01476169d1
Keiichiro Ono <keiono@gmail.com>
2013-08-30 17:43:57 -0700

    Just started to add test cases for Visual Style Serializer.  Still needs a lot of actual test cases.

--- json-impl/src/test/java/org/cytoscape/io/internal/write/json/JsVisualStyleSerializerTest.java
+++ json-impl/src/test/java/org/cytoscape/io/internal/write/json/JsVisualStyleSerializerTest.java
@@ -1,35 +1,161 @@
 package org.cytoscape.io.internal.write.json;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.cytoscape.event.CyEventHelper;
+import org.cytoscape.io.internal.write.json.serializer.CytoscapejsModule;
 import org.cytoscape.io.internal.write.json.serializer.JsVisualStyleSerializer;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.service.util.CyServiceRegistrar;
+import org.cytoscape.view.model.VisualLexicon;
+import org.cytoscape.view.model.VisualProperty;
+import org.cytoscape.view.presentation.property.ArrowShapeVisualProperty;
+import org.cytoscape.view.presentation.property.BasicVisualLexicon;
+import org.cytoscape.view.presentation.property.NodeShapeVisualProperty;
+import org.cytoscape.view.presentation.property.NullVisualProperty;
+import org.cytoscape.view.presentation.property.values.NodeShape;
+import org.cytoscape.view.vizmap.VisualMappingFunctionFactory;
+import org.cytoscape.view.vizmap.VisualStyle;
+import org.cytoscape.view.vizmap.internal.VisualLexiconManager;
+import org.cytoscape.view.vizmap.internal.VisualStyleFactoryImpl;
+import org.cytoscape.work.TaskMonitor;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsVisualStyleSerializerTest {
 
 	private JsVisualStyleSerializer serializer;
-	
-	
+
+	private VisualStyle style;
+
 	@Before
 	public void setUp() throws Exception {
-		
+
 		serializer = new JsVisualStyleSerializer();
+		style = generateVisualStyle();
+		addMappings();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	
-	private void generateVisualStyle() {
-		
+	private final VisualStyle generateVisualStyle() {
+
+		// Create root node.
+		final VisualLexiconManager lexManager = mock(VisualLexiconManager.class);
+
+		// Create root node.
+		final NullVisualProperty minimalRoot = new NullVisualProperty(
+				"MINIMAL_ROOT", "Minimal Root Visual Property");
+		final BasicVisualLexicon minimalLex = new BasicVisualLexicon(
+				minimalRoot);
+		final Set<VisualLexicon> lexSet = new HashSet<VisualLexicon>();
+		lexSet.add(minimalLex);
+		final Collection<VisualProperty<?>> nodeVP = minimalLex
+				.getAllDescendants(BasicVisualLexicon.NODE);
+		final Collection<VisualProperty<?>> edgeVP = minimalLex
+				.getAllDescendants(BasicVisualLexicon.EDGE);
+		when(lexManager.getNodeVisualProperties()).thenReturn(nodeVP);
+		when(lexManager.getEdgeVisualProperties()).thenReturn(edgeVP);
+
+		when(lexManager.getAllVisualLexicon()).thenReturn(lexSet);
+
+		final CyServiceRegistrar serviceRegistrar = mock(CyServiceRegistrar.class);
+		final VisualMappingFunctionFactory ptFactory = mock(VisualMappingFunctionFactory.class);
+		final CyEventHelper eventHelper = mock(CyEventHelper.class);
+		final VisualStyleFactoryImpl visualStyleFactory = new VisualStyleFactoryImpl(
+				lexManager, serviceRegistrar, ptFactory, eventHelper);
+
+		return visualStyleFactory.createVisualStyle("vs1");
+
 	}
-	
+
+	private final void addMappings() {
+
+		// Node default values
+		style.setDefaultValue(BasicVisualLexicon.NODE_FILL_COLOR, Color.WHITE);
+		style.setDefaultValue(BasicVisualLexicon.NODE_WIDTH, 40d);
+		style.setDefaultValue(BasicVisualLexicon.NODE_HEIGHT, 30d);
+		style.setDefaultValue(BasicVisualLexicon.NODE_SHAPE,
+				NodeShapeVisualProperty.ROUND_RECTANGLE);
+		style.setDefaultValue(BasicVisualLexicon.NODE_BORDER_PAINT, Color.BLUE);
+		style.setDefaultValue(BasicVisualLexicon.NODE_BORDER_WIDTH, 3d);
+		style.setDefaultValue(BasicVisualLexicon.NODE_TRANSPARENCY, 200);
+
+		// Edge default values
+		style.setDefaultValue(BasicVisualLexicon.EDGE_UNSELECTED_PAINT,
+				Color.GREEN);
+		style.setDefaultValue(BasicVisualLexicon.EDGE_WIDTH, 5d);
+		style.setDefaultValue(BasicVisualLexicon.EDGE_TARGET_ARROW_SHAPE,
+				ArrowShapeVisualProperty.DIAMOND);
+		style.setDefaultValue(BasicVisualLexicon.EDGE_TRANSPARENCY, 100);
+	}
+
 	@Test
-	public void test() {
+	public void testSerialize() throws Exception {
+		assertNotNull(serializer);
+		assertNotNull(style);
+
+		TaskMonitor tm = mock(TaskMonitor.class);
+
+		Set<VisualStyle> styles = new HashSet<VisualStyle>();
+
+		final ObjectMapper jsMapper = new ObjectMapper();
+		jsMapper.registerModule(new CytoscapejsModule());
+
+		File temp = File.createTempFile("jsStyle", ".json");
+		temp.deleteOnExit();
+
+		OutputStream os = new FileOutputStream(temp);
+		JSONVisualStyleWriter writer = new JSONVisualStyleWriter(os, jsMapper,
+				styles);
+		writer.run(tm);
+
+		testGeneratedJSONFile(temp);
+	}
+
+	private final void testGeneratedJSONFile(File temp) throws Exception {
+		// Read contents
+		System.out.println("Temp = " + temp.getAbsolutePath());
+
+		final FileInputStream fileInputStream = new FileInputStream(temp);
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(
+				fileInputStream, EncodingUtil.getDecoder()));
+
+		JsonFactory factory = new JsonFactory();
+		JsonParser jp = factory.createParser(reader);
+
+		final ObjectMapper mapper = new ObjectMapper();
+		final JsonNode rootNode = mapper.readValue(reader, JsonNode.class);
+
 		
+		// Perform actual tests
+		assertNotNull(rootNode);
+
+		reader.close();
 	}
 
 }
--- json-impl/src/test/resources/site/app/scripts/main.js
+++ json-impl/src/test/resources/site/app/scripts/main.js
@@ -1,132 +1,57 @@
-console.log('\'Allo \'Allo!');
-
+/**
+ *
+ * Template code for Cytoscpae.js export function.
+ *
+ */
 $(loadCy = function () {
 
-    options = {
+
+    // Basic settings for the Cytoscape window
+    var options = {
+
         showOverlay: false,
-        minZoom: 0.5,
-        maxZoom: 2,
+        minZoom: 0.1,
+        maxZoom: 20,
 
         style: cytoscape.stylesheet().selector('node').css({
             'content': 'data(name)',
-            'font-family': 'helvetica',
-            'font-size': 14,
-            'text-outline-width': 3,
-            'text-outline-color': '#888',
+            'font-family': 'helvetica neu',
+            'font-size': 11,
             'text-valign': 'center',
-            'color': '#fff',
-            'width': 'mapData(weight, 30, 80, 20, 50)',
-            'height': 'mapData(height, 0, 200, 10, 45)',
-            'border-color': '#fff'
+            'color': 'white',
+            'width': 40,
+            'height': 40,
+            'background-color': 'white',
+            'background-opacity': 0.6
         }).selector(':selected').css({
-                'background-color': '#000',
-                'line-color': '#000',
-                'target-arrow-color': '#000',
-                'text-outline-color': '#000'
+                'background-color': 'red',
+                'line-color': 'red',
+                'color': 'white'
             }).selector('edge').css({
-                'width': 2,
-                'target-arrow-shape': 'triangle'
+                'width': 4,
+                'target-arrow-shape': 'triangle',
+                'line-color': 'green'
             }),
 
-        elements: {
-            nodes: [
-                {
-                    data: {
-                        id: 'j',
-                        name: 'Jerry',
-                        weight: 65,
-                        height: 174
-                    }
-                },
-                {
-                    data: {
-                        id: 'e',
-                        name: 'Elaine',
-                        weight: 48,
-                        height: 160
-                    }
-                },
-                {
-                    data: {
-                        id: 'k',
-                        name: 'Kramer',
-                        weight: 75,
-                        height: 185
-                    }
-                },
-                {
-                    data: {
-                        id: 'g',
-                        name: 'George',
-                        weight: 70,
-                        height: 150
-                    }
-                }
-            ],
+        layout: {
+            name: 'circle'
 
-            edges: [
-                {
-                    data: {
-                        source: 'j',
-                        target: 'e'
-                    }
-                },
-                {
-                    data: {
-                        source: 'j',
-                        target: 'k'
-                    }
-                },
-                {
-                    data: {
-                        source: 'j',
-                        target: 'g'
-                    }
-                },
-                {
-                    data: {
-                        source: 'e',
-                        target: 'j'
-                    }
-                },
-                {
-                    data: {
-                        source: 'e',
-                        target: 'k'
-                    }
-                },
-                {
-                    data: {
-                        source: 'k',
-                        target: 'j'
-                    }
-                },
-                {
-                    data: {
-                        source: 'k',
-                        target: 'e'
-                    }
-                },
-                {
-                    data: {
-                        source: 'k',
-                        target: 'g'
-                    }
-                },
-                {
-                    data: {
-                        source: 'g',
-                        target: 'j'
-                    }
-                }
-            ]
         },
 
         ready: function () {
             cy = this;
+            cy.load(networkData.elements);
         }
     };
 
-    $('#network').cytoscape(options);
+
+    var networkData = {};
+
+    // Load JSON file
+    $.getJSON('data/cyjs.json', function(network) {
+        console.log(network);
+        networkData = network;
+        $('.network').cytoscape(options);
+    });
 
 }); 
