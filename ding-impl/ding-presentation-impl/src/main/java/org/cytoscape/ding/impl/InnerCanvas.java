3ef81ee4c2bfd14067eb66c5b20b3c4566440a59
David <david.v.welker@gmail.com>
2014-01-07 10:52:36 -0800

    Remove line of nonsense code. (cherry picked from commit ed06cf53d266a186452b23ac6b1857cbb50aed3d)

--- ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/impl/InnerCanvas.java
+++ ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/impl/InnerCanvas.java
@@ -1446,7 +1446,6 @@ public class InnerCanvas extends DingCanvas implements MouseListener, MouseMotio
 							Bend defaultBend = ev.getDefaultValue(BasicVisualLexicon.EDGE_BEND);
 							if( ev.getVisualProperty(BasicVisualLexicon.EDGE_BEND) == defaultBend )
 							{
-								int x = 5;
 								if( defaultBend instanceof BendImpl )
 									ev.setLockedValue(BasicVisualLexicon.EDGE_BEND, new BendImpl( (BendImpl)defaultBend) );
 								else
