737667e1d9d33781d0e016100f78f90e6d0a8d18
Christian Lopes <chrtannus@gmail.com>
2014-01-08 18:48:18 -0200

    Refs #1969: Replaced GenerateValuesTask by a CyAction, in order to solve the issue where input dialogs created by number mapping generators are blocked by the task monitor’s dialog, locking the UI. Also the input dialogs are now created in the EDT.
    (cherry picked from commit 04c88c93678888735129331779cbecf4221b97c1)

--- vizmap-gui-impl/src/main/java/org/cytoscape/view/vizmap/gui/internal/action/GenerateDiscreteValuesAction.java
+++ vizmap-gui-impl/src/main/java/org/cytoscape/view/vizmap/gui/internal/action/GenerateDiscreteValuesAction.java
@@ -1,4 +1,4 @@
-package org.cytoscape.view.vizmap.gui.internal.task;
+package org.cytoscape.view.vizmap.gui.internal.action;
 
 /*
  * #%L
@@ -24,30 +24,30 @@ package org.cytoscape.view.vizmap.gui.internal.task;
  * #L%
  */
 
+import java.awt.event.ActionEvent;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.Map.Entry;
 
 import org.cytoscape.view.model.VisualProperty;
+import org.cytoscape.view.presentation.property.BasicVisualLexicon;
 import org.cytoscape.view.vizmap.VisualMappingFunction;
 import org.cytoscape.view.vizmap.VisualMappingManager;
 import org.cytoscape.view.vizmap.VisualStyle;
 import org.cytoscape.view.vizmap.gui.internal.VizMapperProperty;
 import org.cytoscape.view.vizmap.gui.internal.event.CellType;
 import org.cytoscape.view.vizmap.gui.internal.util.ServicesUtil;
+import org.cytoscape.view.vizmap.gui.internal.util.mapgenerator.FitLabelMappingGenerator;
 import org.cytoscape.view.vizmap.gui.internal.util.mapgenerator.NumberSeriesMappingGenerator;
 import org.cytoscape.view.vizmap.gui.internal.util.mapgenerator.RandomNumberMappingGenerator;
 import org.cytoscape.view.vizmap.gui.internal.view.VisualPropertySheet;
 import org.cytoscape.view.vizmap.gui.internal.view.VisualPropertySheetItem;
 import org.cytoscape.view.vizmap.gui.internal.view.VisualPropertySheetItemModel;
-import org.cytoscape.view.vizmap.gui.internal.view.VizMapperMainPanel;
 import org.cytoscape.view.vizmap.gui.util.DiscreteMappingGenerator;
 import org.cytoscape.view.vizmap.mappings.DiscreteMapping;
-import org.cytoscape.work.AbstractTask;
-import org.cytoscape.work.TaskMonitor;
 import org.cytoscape.work.undo.AbstractCyEdit;
 import org.cytoscape.work.undo.UndoSupport;
 
@@ -55,70 +55,111 @@ import com.l2fprod.common.propertysheet.Property;
 import com.l2fprod.common.propertysheet.PropertySheetPanel;
 import com.l2fprod.common.propertysheet.PropertySheetTableModel.Item;
 
-public class GenerateValuesTask extends AbstractTask {
+public class GenerateDiscreteValuesAction extends AbstractVizMapperAction {
 
-	private final Map<DiscreteMapping<?, ?>, Map<Object, Object>> previousMappingValues;
-	private final Map<DiscreteMapping<?, ?>, Map<Object, ?>> newMappingValues;
+	private static final long serialVersionUID = 3227895615738968589L;
+	
 	private final DiscreteMappingGenerator<?> generator;
-	private final VizMapperMainPanel vizMapperPanel;
-	private final ServicesUtil servicesUtil;
 
 	// ==[ CONSTRUCTORS ]===============================================================================================
 	
-	public GenerateValuesTask(final DiscreteMappingGenerator<?> generator,
-							  final VizMapperMainPanel vizMapperPanel,
-							  final ServicesUtil servicesUtil) {
+	public GenerateDiscreteValuesAction(final String name, final DiscreteMappingGenerator<?> generator,
+			final ServicesUtil servicesUtil) {
+		super(name, servicesUtil);
 		this.generator = generator;
-		this.vizMapperPanel = vizMapperPanel;
-		this.servicesUtil = servicesUtil;
-		previousMappingValues = new HashMap<DiscreteMapping<?,?>, Map<Object, Object>>();
-		newMappingValues = new HashMap<DiscreteMapping<?,?>, Map<Object, ?>>();
 	}
 
 	// ==[ PUBLIC METHODS ]=============================================================================================
 	
 	@Override
-	public void run(final TaskMonitor monitor) throws Exception {
-		final VisualPropertySheet selVpSheet = vizMapperPanel.getSelectedVisualPropertySheet();
+	public void actionPerformed(final ActionEvent e) {
+		final VisualPropertySheet selVpSheet = getVizMapperMainPanel().getSelectedVisualPropertySheet();
 		
 		if (selVpSheet == null)
 			return;
 		
 		final Set<VisualPropertySheetItem<?>> vpSheetItems = selVpSheet.getSelectedItems();
 		
-		for (final VisualPropertySheetItem<?> vpsItem : vpSheetItems) {
-			final VisualPropertySheetItemModel<?> model = vpsItem.getModel();
+		new Thread() {
+			@Override
+			public void run() {
+				final Map<DiscreteMapping<?, ?>, Map<Object, Object>> previousMappingValues = 
+						new HashMap<DiscreteMapping<?,?>, Map<Object, Object>>();
+				final Map<DiscreteMapping<?, ?>, Map<Object, ?>> newMappingValues = 
+						new HashMap<DiscreteMapping<?,?>, Map<Object, ?>>();
+				
+				for (final VisualPropertySheetItem<?> vpsItem : vpSheetItems) {
+					final VisualPropertySheetItemModel<?> model = vpsItem.getModel();
+			
+					final VisualProperty<?> vp = (VisualProperty<?>) model.getVisualProperty();
+					final Class<?> vpValueType = vp.getRange().getType();
+					final Class<?> generatorType = generator.getDataType();
+		
+					final PropertySheetPanel propSheetPnl = vpsItem.getPropSheetPnl();
+					final Item value = (Item) propSheetPnl.getTable().getValueAt(0, 0);
+					
+					if (value.isProperty()) {
+						final VizMapperProperty<?, ?, ?> prop = (VizMapperProperty<?, ?, ?>) value.getProperty();
+					
+						if ( vpValueType.isAssignableFrom(generatorType)
+								|| ((generator instanceof NumberSeriesMappingGenerator 
+										|| generator instanceof RandomNumberMappingGenerator)
+										&& Number.class.isAssignableFrom(vpValueType)) )
+							generateValues(vpsItem, prop.getValue().toString(), vp, previousMappingValues,
+									newMappingValues);
+					}
+				}
+				
+				// Undo support
+				if (!previousMappingValues.isEmpty()) {
+					final UndoSupport undo = servicesUtil.get(UndoSupport.class);
+					undo.postEdit(new GenerateValuesEdit(previousMappingValues, newMappingValues));
+				}
+			}
+		}.start();
+	}
 	
-			final VisualProperty<?> vp = (VisualProperty<?>) model.getVisualProperty();
-			final Class<?> vpValueType = vp.getRange().getType();
-			final Class<?> generatorType = generator.getDataType();
-
-			final PropertySheetPanel propSheetPnl = vpsItem.getPropSheetPnl();
-			final Item value = (Item) propSheetPnl.getTable().getValueAt(0, 0);
-			
-			if (value.isProperty()) {
-				final VizMapperProperty<?, ?, ?> prop = (VizMapperProperty<?, ?, ?>) value.getProperty();
-			
-				if ( vpValueType.isAssignableFrom(generatorType)
-						|| ((generator instanceof NumberSeriesMappingGenerator 
-								|| generator instanceof RandomNumberMappingGenerator)
-								&& Number.class.isAssignableFrom(vpValueType)) )
-					generateValues(vpsItem, prop.getValue().toString(), vp);
+	@Override
+	public void updateEnableState() {
+		boolean enabled = false;
+		final VisualPropertySheet vpSheet = getVizMapperMainPanel().getSelectedVisualPropertySheet();
+		
+		if (vpSheet != null) {
+			for (final VisualPropertySheetItem<?> item : vpSheet.getSelectedItems()) {
+				final VisualMappingFunction<?, ?> mapping = item.getModel().getVisualMappingFunction();
+				
+				if (mapping != null && mapping instanceof DiscreteMapping) {
+					final VisualProperty<?> vp = item.getModel().getVisualProperty();
+					final Class<?> vpValueType = vp.getRange().getType();
+					final Class<?> generatorType = generator.getDataType();
+					
+					if (generator instanceof FitLabelMappingGenerator) {
+						enabled = vp == BasicVisualLexicon.NODE_SIZE || vp == BasicVisualLexicon.NODE_WIDTH;
+						break;
+					}
+					
+					if ( vpValueType.isAssignableFrom(generatorType)
+							|| ((generator instanceof NumberSeriesMappingGenerator 
+									|| generator instanceof RandomNumberMappingGenerator)
+									&& Number.class.isAssignableFrom(vpValueType)) ) {
+						enabled = true;
+						break;
+					}
+				}
 			}
 		}
 		
-		// Undo support
-		if (!previousMappingValues.isEmpty()) {
-			final UndoSupport undo = servicesUtil.get(UndoSupport.class);
-			undo.postEdit(new GenerateValuesEdit());
-		}
+		setEnabled(enabled);
 	}
-
+	
 	// ==[ PRIVATE METHODS ]============================================================================================
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private void generateValues(final VisualPropertySheetItem<?> vpsItem, final String attrName,
-			final VisualProperty<?> vp) {
+	private void generateValues(final VisualPropertySheetItem<?> vpsItem,
+								final String attrName,
+								final VisualProperty<?> vp,
+								final Map<DiscreteMapping<?, ?>, Map<Object, Object>> previousMappingValues,
+								final Map<DiscreteMapping<?, ?>, Map<Object, ?>> newMappingValues) {
 		final VisualStyle style = servicesUtil.get(VisualMappingManager.class).getCurrentVisualStyle();
 		final VisualMappingFunction<?, ?> mapping = style.getVisualMappingFunction(vp);
 
@@ -155,13 +196,19 @@ public class GenerateValuesTask extends AbstractTask {
 			dm.putAll(newValues);
 		}
 	}
-	
+		
 	// ==[ CLASSES ]====================================================================================================
-	
+
 	private class GenerateValuesEdit extends AbstractCyEdit {
 
-		public GenerateValuesEdit() {
+		private final Map<DiscreteMapping<?, ?>, Map<Object, Object>> previousMappingValues;
+		private final Map<DiscreteMapping<?, ?>, Map<Object, ?>> newMappingValues;
+		
+		public GenerateValuesEdit(final Map<DiscreteMapping<?, ?>, Map<Object, Object>> previousMappingValues,
+								  final Map<DiscreteMapping<?, ?>, Map<Object, ?>> newMappingValues) {
 			super("Mapping Value Generators");
+			this.previousMappingValues = previousMappingValues;
+			this.newMappingValues = newMappingValues;
 		}
 
 		@Override
