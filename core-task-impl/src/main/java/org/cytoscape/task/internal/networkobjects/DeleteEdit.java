dd7375c0fcffd6e4aa9ac5799407c2124ceb3cc0
Christian Lopes <chrtannus@gmail.com>
2014-01-31 12:13:14 -0500

    Refs #2481: Bypass values are now restored on Undo Delete Nodes/Edges (patch).

--- core-task-impl/src/main/java/org/cytoscape/task/internal/networkobjects/DeleteEdit.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/networkobjects/DeleteEdit.java
@@ -29,16 +29,23 @@ import static org.cytoscape.view.presentation.property.BasicVisualLexicon.NODE_X
 import static org.cytoscape.view.presentation.property.BasicVisualLexicon.NODE_Y_LOCATION;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.cytoscape.event.CyEventHelper;
 import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyIdentifiable;
 import org.cytoscape.model.CyNode;
 import org.cytoscape.model.subnetwork.CySubNetwork;
-import org.cytoscape.view.model.CyNetworkViewManager;
 import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.CyNetworkViewManager;
 import org.cytoscape.view.model.View;
+import org.cytoscape.view.model.VisualLexicon;
+import org.cytoscape.view.model.VisualProperty;
+import org.cytoscape.view.presentation.property.BasicVisualLexicon;
 import org.cytoscape.view.vizmap.VisualMappingManager;
 import org.cytoscape.work.undo.AbstractCyEdit;
 
@@ -48,10 +55,12 @@ import org.cytoscape.work.undo.AbstractCyEdit;
  * An undoable edit that will undo and redo deletion of nodes and edges.
  */ 
 final class DeleteEdit extends AbstractCyEdit {
+	
 	private final List<CyNode> nodes;
 	private final Set<CyEdge> edges;
 	private final double[] xPos;
 	private final double[] yPos;
+	private final Map<CyIdentifiable, Map<VisualProperty<?>, Object>> bypassMap;
 	private final CySubNetwork net;
 	private final CyNetworkViewManager netViewMgr;
 	private final VisualMappingManager visualMappingManager;
@@ -88,25 +97,39 @@ final class DeleteEdit extends AbstractCyEdit {
 			throw new NullPointerException("eventHelper is null");
 		this.eventHelper = eventHelper;
 
-		// save the positions of the nodes
+		// save bypass values and the positions of the nodes
+		bypassMap = new HashMap<CyIdentifiable, Map<VisualProperty<?>,Object>>();
 		xPos = new double[nodes.size()]; 
 		yPos = new double[nodes.size()];
 		final Collection<CyNetworkView> views = netViewMgr.getNetworkViews(net);
 		CyNetworkView netView = null;
+		
 		if(views.size() != 0)
 			netView = views.iterator().next();
 
 		if (netView != null) {
+			final VisualLexicon lexicon = visualMappingManager.getAllVisualLexicon().iterator().next();
+			final Collection<VisualProperty<?>> nodeProps = lexicon.getAllDescendants(BasicVisualLexicon.NODE);
+			final Collection<VisualProperty<?>> edgeProps = lexicon.getAllDescendants(BasicVisualLexicon.EDGE);
+			
 			int i = 0;
+			
 			for (CyNode n : nodes) {
 				View<CyNode> nv = netView.getNodeView(n);
+				saveLockedValues(nv, nodeProps);
 				xPos[i] = nv.getVisualProperty(NODE_X_LOCATION);
 				yPos[i] = nv.getVisualProperty(NODE_Y_LOCATION);
 				i++;
 			}
+			
+			for (CyEdge e : edges) {
+				View<CyEdge> ev = netView.getEdgeView(e);
+				saveLockedValues(ev, edgeProps);
+			}
 		}
 	}
 
+	@Override
 	public void redo() {
 		net.removeNodes(nodes);
 		net.removeEdges(edges);
@@ -120,6 +143,7 @@ final class DeleteEdit extends AbstractCyEdit {
 		netView.updateView();
 	}
 
+	@Override
 	public void undo() {
 		for (CyNode n : nodes)
 			net.addNode(n);
@@ -137,13 +161,43 @@ final class DeleteEdit extends AbstractCyEdit {
 			int i = 0;
 			for (final CyNode node : nodes) {
 				View<CyNode> nodeView = netView.getNodeView(node);
+				if (nodeView == null) continue;
 				nodeView.setVisualProperty(NODE_X_LOCATION, xPos[i]);
 				nodeView.setVisualProperty(NODE_Y_LOCATION, yPos[i] );
+				setLockedValues(nodeView);
 				i++;
 			}
+			for (final CyEdge edge: edges) {
+				View<CyEdge> edgeView = netView.getEdgeView(edge);
+				if (edgeView == null) continue;
+				setLockedValues(edgeView);
+			}
 		}
+		
 		visualMappingManager.getVisualStyle(netView).apply(netView);
-
 		netView.updateView();
+	}
+	
+	private void saveLockedValues(final View<? extends CyIdentifiable> view,
+			final Collection<VisualProperty<?>> visualProps) {
+		for (final VisualProperty<?> vp : visualProps) {
+			if (view.isValueLocked(vp)) {
+				Map<VisualProperty<?>, Object> vpMap = bypassMap.get(view.getModel());
+				
+				if (vpMap == null)
+					bypassMap.put(view.getModel(), vpMap = new HashMap<VisualProperty<?>, Object>());
+				
+				vpMap.put(vp, view.getVisualProperty(vp));
+			}
+		}
+	}
+	
+	private void setLockedValues(final View<? extends CyIdentifiable> view) {
+		final Map<VisualProperty<?>, Object> vpMap = bypassMap.get(view.getModel());
+		
+		if (vpMap != null) {
+			for (final Entry<VisualProperty<?>, Object> entry : vpMap.entrySet())
+				view.setLockedValue(entry.getKey(), entry.getValue());
+		}
 	}
 }
