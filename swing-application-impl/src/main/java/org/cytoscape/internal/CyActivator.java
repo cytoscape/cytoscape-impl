c83593371f907528ea40fae5c21b5daed5239da0
Tim Hull <tmhull@ucsd.edu>
2014-04-09 13:58:58 -0700

    Refs #2055 - Fixes JVM SIGSEGV crash that occurs on Cytoscape startup or shutdown on some Linux/JVM combinations

--- swing-application-impl/src/main/java/org/cytoscape/internal/CyActivator.java
+++ swing-application-impl/src/main/java/org/cytoscape/internal/CyActivator.java
@@ -24,12 +24,26 @@ package org.cytoscape.internal;
  * #L%
  */
 
-import static org.cytoscape.application.swing.CytoPanelName.*;
-import static org.cytoscape.application.swing.CyNetworkViewDesktopMgr.ArrangeType.*;
-import static org.cytoscape.work.ServiceProperties.*;
+import static org.cytoscape.application.swing.CyNetworkViewDesktopMgr.ArrangeType.CASCADE;
+import static org.cytoscape.application.swing.CyNetworkViewDesktopMgr.ArrangeType.GRID;
+import static org.cytoscape.application.swing.CyNetworkViewDesktopMgr.ArrangeType.HORIZONTAL;
+import static org.cytoscape.application.swing.CyNetworkViewDesktopMgr.ArrangeType.VERTICAL;
+import static org.cytoscape.application.swing.CytoPanelName.EAST;
+import static org.cytoscape.application.swing.CytoPanelName.SOUTH;
+import static org.cytoscape.application.swing.CytoPanelName.SOUTH_WEST;
+import static org.cytoscape.application.swing.CytoPanelName.WEST;
+import static org.cytoscape.work.ServiceProperties.ACCELERATOR;
+import static org.cytoscape.work.ServiceProperties.IN_TOOL_BAR;
+import static org.cytoscape.work.ServiceProperties.LARGE_ICON_URL;
+import static org.cytoscape.work.ServiceProperties.MENU_GRAVITY;
+import static org.cytoscape.work.ServiceProperties.PREFERRED_MENU;
+import static org.cytoscape.work.ServiceProperties.TITLE;
+import static org.cytoscape.work.ServiceProperties.TOOLTIP;
+import static org.cytoscape.work.ServiceProperties.TOOL_BAR_GRAVITY;
 
 import java.util.Properties;
 
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -40,8 +54,8 @@ import org.cytoscape.application.CyVersion;
 import org.cytoscape.application.events.CyShutdownListener;
 import org.cytoscape.application.events.SetCurrentNetworkViewListener;
 import org.cytoscape.application.swing.CyAction;
-import org.cytoscape.application.swing.CyNetworkViewDesktopMgr;
 import org.cytoscape.application.swing.CyHelpBroker;
+import org.cytoscape.application.swing.CyNetworkViewDesktopMgr;
 import org.cytoscape.application.swing.CytoPanelComponent;
 import org.cytoscape.application.swing.ToolBarComponent;
 import org.cytoscape.event.CyEventHelper;
@@ -68,9 +82,9 @@ import org.cytoscape.internal.undo.RedoAction;
 import org.cytoscape.internal.undo.UndoAction;
 import org.cytoscape.internal.util.undo.UndoMonitor;
 import org.cytoscape.internal.view.BirdsEyeViewHandler;
+import org.cytoscape.internal.view.CyDesktopManager;
 import org.cytoscape.internal.view.CyHelpBrokerImpl;
 import org.cytoscape.internal.view.CytoscapeDesktop;
-import org.cytoscape.internal.view.CyDesktopManager;
 import org.cytoscape.internal.view.CytoscapeMenuBar;
 import org.cytoscape.internal.view.CytoscapeMenuPopulator;
 import org.cytoscape.internal.view.CytoscapeMenus;
@@ -477,6 +491,15 @@ public class CyActivator extends AbstractCyActivator {
 	}
 
 	private void setLookAndFeel() {
+		if (!SwingUtilities.isEventDispatchThread()) {
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					setLookAndFeel();
+				}
+			});
+			return;
+		}
 		Logger logger = LoggerFactory.getLogger(getClass());
 		String lookAndFeel;
 		// update look and feel
