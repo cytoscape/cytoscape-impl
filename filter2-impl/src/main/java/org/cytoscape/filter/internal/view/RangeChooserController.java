003bb58a05120f9e7ddf30a811fce80fe491f7fa
Jason Montojo <jrrmzz@gmail.com>
2014-04-24 16:37:55 -0400

    References #2617: Text fields are now always shown with sliders.  Fixed bug where slider maximum drifts when the minimum is dragged around because of a rounding error.
    (cherry picked from commit d656600020439044d23dfc582668f31f6404cb10)

--- filter2-impl/src/main/java/org/cytoscape/filter/internal/view/RangeChooserController.java
+++ filter2-impl/src/main/java/org/cytoscape/filter/internal/view/RangeChooserController.java
@@ -62,11 +62,19 @@ public abstract class RangeChooserController {
 	
 	public void minimumChanged(RangeChooser chooser) {
 		low = (Number) chooser.getMinimumField().getValue();
+		if (low == null) {
+			low = minimum;
+		}
+		sliderModel.setValueRange(low, high, minimum, maximum);
 		handleRangeChanged(low, high);
 	}
 
 	public void maximumChanged(RangeChooser chooser) {
 		high = (Number) chooser.getMaximumField().getValue();
+		if (high == null) {
+			high = maximum;
+		}
+		sliderModel.setValueRange(low, high, minimum, maximum);
 		handleRangeChanged(low, high);
 	}
 
@@ -97,6 +105,14 @@ public abstract class RangeChooserController {
 			high = maximum;
 		}
 		setRange(low, high, minimum, maximum);
+	}
+	
+	public Number getMinimum() {
+		return minimum;
+	}
+
+	public Number getMaximum() {
+		return maximum;
 	}
 	
 	protected abstract void handleRangeChanged(Number low, Number high);
