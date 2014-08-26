233a68e6c1eba9406598a90ee77fceeb4fbbf1a2
Christian Lopes <chrtannus@gmail.com>
2014-08-26 12:07:47 -0400

    Refs #2626: Fixes PointPicker's target position and makes it a simple cross (Radial Gradient editor). Removed unused code from RadialGradientEditor.

--- ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/internal/gradients/radial/RadialGradientEditor.java
+++ ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/internal/gradients/radial/RadialGradientEditor.java
@@ -2,19 +2,13 @@ package org.cytoscape.ding.internal.gradients.radial;
 
 import static org.cytoscape.ding.internal.gradients.radial.RadialGradient.CENTER;
 
-import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.Point2D;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-import javax.swing.DefaultListCellRenderer;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 
 import org.cytoscape.ding.internal.gradients.AbstractGradientEditor;
@@ -24,70 +18,13 @@ public class RadialGradientEditor extends AbstractGradientEditor<RadialGradient>
 	
 	private static final long serialVersionUID = 5997072753907737888L;
 	
-	private static enum Direction {
-		TOP("Top", new Point2D.Double(0.5, 0)),
-		TOP_LEFT("Top Left", new Point2D.Double(0, 0)),
-		TOP_RIGHT("Top Right", new Point2D.Double(1, 0)),
-		RIGHT("Right", new Point2D.Double(1, 0.5)),
-		CENTER("Center", new Point2D.Double(0.5, 0.5)),
-		LEFT("Left", new Point2D.Double(0, 0.5)),
-		BOTTOM_LEFT("Bottom Left", new Point2D.Double(0, 1)),
-		BOTTOM_RIGHT("Bottom Right", new Point2D.Double(1, 1)),
-		BOTTOM("Bottom", new Point2D.Double(0.5, 1));
-		
-		private final String label;
-		private final Point2D center;
-
-		private Direction(final String label, final Point2D center) {
-			this.label = label;
-			this.center = center;
-		}
-		
-		public String getLabel() {
-			return label;
-		}
-		
-		public Point2D getCenter() {
-			return center;
-		}
-
-		public static Direction getValue(final Point2D center) {
-			double x = snap(center.getX());
-			double y = snap(center.getY());
-			final Point2D newCenter = new Point2D.Double(x, y);
-			
-			Direction value = CENTER; // Default
-			
-			for (final Direction dir : Direction.values()) {
-				if (dir.getCenter().equals(newCenter)) {
-					value = dir;
-					break;
-				}
-			}
-			
-			return value;
-		}
-		
-		private static double snap(double d) {
-			d = d >= 0.75 ? 1.0 : d;
-			d = d <  0.25 ? 0.0 : d;
-			d = (d == 0.0 || d == 1.0) ? d : 0.5;
-			
-			return d;
-		}
-	}
-	
 	private JLabel centerLbl;
-	private JComboBox directionCmb;
 	private PointPicker pointPicker;
 	
 	// ==[ CONSTRUCTORS ]===============================================================================================
 	
 	public RadialGradientEditor(final RadialGradient gradient) {
 		super(gradient);
-		
-		// TODO
-//		final float radius = gradient.get(RADIUS, Float.class, 1.0f);
 	}
 	
 	// ==[ PRIVATE METHODS ]============================================================================================
@@ -120,30 +57,10 @@ public class RadialGradientEditor extends AbstractGradientEditor<RadialGradient>
 		return p;
 	}
 	
-	private JComboBox getDirectionCmb() {
-		if (directionCmb == null) {
-			directionCmb = new JComboBox(Direction.values());
-			directionCmb.setRenderer(new DirectionComboBoxRenderer());
-			final Point2D center = gradient.get(CENTER, Point2D.class, Direction.CENTER.getCenter());
-			final Direction direction = Direction.getValue(center);
-			directionCmb.setSelectedItem(direction);
-			
-			directionCmb.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					final Direction value = (Direction) directionCmb.getSelectedItem();
-		            gradient.set(CENTER, value != null ? value.getCenter() : Direction.CENTER.getCenter());
-				}
-			});
-		}
-		
-		return directionCmb;
-	}
-	
 	private PointPicker getPointPicker() {
 		if (pointPicker == null) {
-			final Point2D center = gradient.get(CENTER, Point2D.class, Direction.CENTER.getCenter());
-			pointPicker = new PointPicker(100, 16, center);
+			final Point2D center = gradient.get(CENTER, Point2D.class, new Point2D.Double(0.5, 0.5));
+			pointPicker = new PointPicker(100, 12, center);
 			
 			pointPicker.addPropertyChangeListener("value", new PropertyChangeListener() {
 				@Override
@@ -155,28 +72,5 @@ public class RadialGradientEditor extends AbstractGradientEditor<RadialGradient>
 		}
 		
 		return pointPicker;
-	}
-	
-	// ==[ CLASSES ]====================================================================================================
-	
-	private static class DirectionComboBoxRenderer extends DefaultListCellRenderer {
-
-		private static final long serialVersionUID = 3041717525540256178L;
-
-		@Override
-		public Component getListCellRendererComponent(final JList list, final Object value, final int index,
-				final boolean isSelected, final boolean cellHasFocus) {
-			final DefaultListCellRenderer c = (DefaultListCellRenderer) super.getListCellRendererComponent(
-					list, value, index, isSelected, cellHasFocus);
-			
-			if (value == null)
-				c.setText("-- none --");
-			else if (value instanceof Direction)
-				c.setText(((Direction)value).getLabel());
-			else
-				c.setText("[ invalid value ]"); // Should never happen
-				
-			return c;
-		}
 	}
 }
--- ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/internal/util/PointPicker.java
+++ ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/internal/util/PointPicker.java
@@ -2,7 +2,6 @@ package org.cytoscape.ding.internal.util;
 
 
 import java.awt.BasicStroke;
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -15,17 +14,13 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 
 import javax.swing.GroupLayout;
-import javax.swing.JDialog;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-import javax.swing.WindowConstants;
-import javax.swing.GroupLayout.Alignment;
 
 import org.cytoscape.ding.internal.charts.AbstractChartEditor.DoubleInputVerifier;
 
@@ -36,7 +31,6 @@ public class PointPicker extends JPanel {
     
 	private static final long serialVersionUID = 1152583166571339876L;
 
-	private static final float EXTRA_PADDING = 4.0f;
 	private static final Point2D DEFAULT_VALUE = new Point2D.Double(0.5, 0.5);
     
 	private int size;
@@ -64,13 +58,15 @@ public class PointPicker extends JPanel {
     	if (value == null)
     		value = DEFAULT_VALUE;
     	
+    	final float EXTRA_PADDING = 2.0f;
+    	
     	this.value = value;
     	this.size = size;
         this.targetSize = targetSize;
-        fieldHeight = fieldWidth = size - targetSize - EXTRA_PADDING;
+        fieldHeight = fieldWidth = (size - targetSize - 2 * EXTRA_PADDING);
         
-        fieldX = 0 + targetSize/2 + EXTRA_PADDING/2;
-        fieldY = 0 + targetSize/2 + EXTRA_PADDING/2;
+        fieldX = targetSize/2 + EXTRA_PADDING;
+        fieldY = targetSize/2 + EXTRA_PADDING;
         fieldCenterX = fieldX + fieldWidth / 2;
         fieldCenterY = fieldY + fieldHeight / 2;
         
@@ -110,6 +106,7 @@ public class PointPicker extends JPanel {
 		layout.setHorizontalGroup(layout.createSequentialGroup()
 				.addComponent(getCanvas(), GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
 						GroupLayout.PREFERRED_SIZE)
+				.addGap(4)
 				.addGroup(layout.createParallelGroup(Alignment.LEADING, true)
 						.addGroup(layout.createSequentialGroup()
 							.addComponent(xLbl)
@@ -150,9 +147,8 @@ public class PointPicker extends JPanel {
     				paintCanvas(g);
     		    }
     		};
-    		canvas.setOpaque(false);
     		
-    		// TODO
+    		canvas.setOpaque(false);
     		canvas.setMinimumSize(new Dimension((int) size, (int) size));
     		canvas.setPreferredSize(new Dimension((int) size, (int) size));
     		
@@ -266,13 +262,11 @@ public class PointPicker extends JPanel {
 	}
     
     private Point2D convertToValue(final Point2D position) {
-    	double f = fieldX + fieldWidth;
-    	return new Point2D.Double(position.getX() / f, position.getY() / f);
+    	return new Point2D.Double((position.getX() - fieldX) / fieldWidth, (position.getY() - fieldY) / fieldHeight);
 	}
     
     private Point2D convertToPosition(final Point2D value) {
-    	double f = fieldX + fieldWidth;
-    	return new Point2D.Double(value.getX() * f, value.getY() * f);
+    	return new Point2D.Double(value.getX() * fieldWidth + fieldX, value.getY() * fieldHeight + fieldY);
     }
     
 	protected void paintCanvas(final Graphics g) {
@@ -295,42 +289,26 @@ public class PointPicker extends JPanel {
 	}
     
     protected void drawTarget(final Graphics2D g2, final float strokeWidth, final Color strokeColor) {
-        final double cx = position.getX(); // value x
-        final double cy = position.getY(); // value y
-        final float d1 = targetSize;
-        final float d2 = targetSize * 0.8f;
-        final float cg = targetSize * 0.2f; // value gap
+        final double cx = position.getX();
+        final double cy = position.getY();
+        final float d = targetSize;
         
         g2.setStroke(new BasicStroke(strokeWidth, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
         g2.setColor(strokeColor);
-        // circle
-        g2.drawOval((int)Math.round(cx - d2/2), (int)Math.round(cy - d2/2), (int)d2, (int)d2);
-        // vertical lines
-        g2.drawLine((int)cx, (int)(cy - d1/2), (int)cx, (int)(cy - cg));
-        g2.drawLine((int)cx, (int)(cy + cg), (int)cx, (int)(cy + d1/2));
-        // horizontal lines
-        g2.drawLine((int)(cx - d1/2), (int)cy, (int)(cx - cg), (int)cy);
-        g2.drawLine((int)(cx + cg), (int)cy, (int)(cx + d1/2), (int)cy);
+        // vertical line
+        g2.drawLine((int)cx, (int)(cy - d/2), (int)cx, (int)(cy + d/2));
+        // horizontal line
+        g2.drawLine((int)(cx - d/2), (int)cy, (int)(cx + d/2), (int)cy);
     }
     
-    public static void main(String[] args) {
-		final PointPicker pp = new PointPicker(100, 16, new Point2D.Double(0.5, 0.5));
-		final JLabel lb = new JLabel("Value: ");
-		
-		pp.addPropertyChangeListener("value", new PropertyChangeListener() {
-			@Override
-			public void propertyChange(PropertyChangeEvent e) {
-				Point2D p = (Point2D)e.getNewValue();
-				lb.setText("Value: " + p.getX() + ", " + p.getY());
-			}
-		});
-		
-		JDialog d = new JDialog();
-		d.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		d.getContentPane().add(pp, BorderLayout.CENTER);
-		d.getContentPane().add(lb, BorderLayout.SOUTH);
-		d.pack();
-		d.setVisible(true);
-	}
+//    public static void main(String[] args) {
+//		final PointPicker pp = new PointPicker(100, 12, new Point2D.Double(0.5, 0.5));
+//		
+//		javax.swing.JDialog d = new javax.swing.JDialog();
+//		d.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+//		d.getContentPane().add(pp, java.awt.BorderLayout.CENTER);
+//		d.pack();
+//		d.setVisible(true);
+//	}
 }
 
