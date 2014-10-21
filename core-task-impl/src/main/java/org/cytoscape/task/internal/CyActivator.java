5a19c021e8f783a296465c3f1b2a0f5e3b34320b
Keiichiro Ono <keiono@gmail.com>
2014-10-21 13:43:14 -0700

    refs #2415 Log level and menu title has been changed for HTML5 export.
    (cherry picked from commit 00de3da5b1e565648bec5c9c8c20d9c03437b9c4)

--- core-task-impl/src/main/java/org/cytoscape/task/internal/CyActivator.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/CyActivator.java
@@ -1773,7 +1773,7 @@ public class CyActivator extends AbstractCyActivator {
 		Properties exportAsWebArchiveTaskFactoryProps = new Properties();
 		exportAsWebArchiveTaskFactoryProps.setProperty(PREFERRED_MENU,"File.Export");
 		exportAsWebArchiveTaskFactoryProps.setProperty(MENU_GRAVITY,"3.1");
-		exportAsWebArchiveTaskFactoryProps.setProperty(TITLE,"Everything as Interactive Web Visualization...");
+		exportAsWebArchiveTaskFactoryProps.setProperty(TITLE,"Network Views as Web Page...");
 		registerAllServices(bc, exportAsWebArchiveTaskFactory, exportAsWebArchiveTaskFactoryProps);
 		registerServiceListener(bc, exportAsWebArchiveTaskFactory, "registerFactory", "unregisterFactory", CySessionWriterFactory.class);
 	}
