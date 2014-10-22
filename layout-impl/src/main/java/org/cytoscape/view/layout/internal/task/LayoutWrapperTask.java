bc404b65b92cd931e3ae272e7f56c54d0470fda6
Scooter Morris <scooter@cgl.ucsf.edu>
2014-10-22 10:53:39 -0700

    Add support for weighted layouts to command line.  Refs #2889
    Change-Id: I4fcee3a4ce1e281855e232cd1f3578138b666da1
    (cherry picked from commit 5da21f92e6dd682ef94cc9f0cdd46ad9cf7a94a9)

--- layout-impl/src/main/java/org/cytoscape/view/layout/internal/task/LayoutWrapperTask.java
+++ layout-impl/src/main/java/org/cytoscape/view/layout/internal/task/LayoutWrapperTask.java
@@ -32,8 +32,10 @@ import java.util.Set;
 
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.command.util.NodeList;
+import org.cytoscape.model.CyColumn;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyTable;
 import org.cytoscape.view.layout.CyLayoutAlgorithm;
 import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.view.model.CyNetworkViewManager;
@@ -42,13 +44,15 @@ import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.ContainsTunables;
 import org.cytoscape.work.TaskMonitor;
 import org.cytoscape.work.Tunable;
+import org.cytoscape.work.util.ListSingleSelection;
 
 public class LayoutWrapperTask extends AbstractTask {
 	private final CyApplicationManager appMgr;
 	private final CyNetworkViewManager viewMgr;
 	List<CyNode> nodes = null;
 	private final CyLayoutAlgorithm algorithm;
-	
+	private static final String UNWEIGHTED = "(none)";
+
 	@Tunable(description="Network to layout", context="nogui")
 	public CyNetwork network = null;
 
@@ -61,6 +65,42 @@ public class LayoutWrapperTask extends AbstractTask {
 		return nodeList;
 	}
 	public void setnodeList(NodeList setValue) {}
+
+	// If this layout algorithm supports edge attributes, pick that
+	// up here
+	ListSingleSelection<String> possibleEdgeAttributes = null;
+	@Tunable(description="Edge attribute to use to weight layout", context="nogui")
+	public ListSingleSelection<String> getEdgeAttribute() {
+		// Make sure we know the network
+		if (network == null)
+			network = appMgr.getCurrentNetwork();
+		List<String> attrs = getSupportedEdgeAttributes();
+		if (attrs == null || attrs.size() == 0) {
+			possibleEdgeAttributes = null;
+		} else {
+			possibleEdgeAttributes = new ListSingleSelection<String>(attrs);
+		}
+		return possibleEdgeAttributes;
+	}
+	public void setEdgeAttribute(ListSingleSelection<String> setValue) {}
+
+	// If this layout algorithm supports node attributes, pick that
+	// up here
+	ListSingleSelection<String> possibleNodeAttributes = null;
+	@Tunable(description="Node attribute to use to weight layout", context="nogui")
+	public ListSingleSelection<String> getNodeAttribute() {
+		// Make sure we know the network
+		if (network == null)
+			network = appMgr.getCurrentNetwork();
+		List<String> attrs = getSupportedNodeAttributes();
+		if (attrs == null || attrs.size() == 0) {
+			possibleNodeAttributes = null;
+		} else {
+			possibleNodeAttributes = new ListSingleSelection<String>(attrs);
+		}
+		return possibleNodeAttributes;
+	}
+	public void setNodeAttribute(ListSingleSelection<String> setValue) {}
 
 	@ContainsTunables
 	public Object layoutContext;
@@ -89,9 +129,45 @@ public class LayoutWrapperTask extends AbstractTask {
 					nodeViews.add(view.getNodeView(node));
 			} 
 	
-			insertTasksAfterCurrentTask(algorithm.createTaskIterator(view, layoutContext, nodeViews,""));
+			insertTasksAfterCurrentTask(algorithm.createTaskIterator(view, layoutContext, nodeViews, getLayoutAttribute()));
 		}
 
+	}
+
+	private List<String> getSupportedEdgeAttributes() {
+		Set<Class<?>> supportedEdgeTypes = algorithm.getSupportedEdgeAttributeTypes();
+		if (supportedEdgeTypes == null || supportedEdgeTypes.size() == 0) return null;
+		return getSupportedAttributes(supportedEdgeTypes, network.getDefaultEdgeTable());
+	}
+
+	private List<String> getSupportedNodeAttributes() {
+		Set<Class<?>> supportedNodeTypes = algorithm.getSupportedNodeAttributeTypes();
+		if (supportedNodeTypes == null || supportedNodeTypes.size() == 0) return null;
+		return getSupportedAttributes(supportedNodeTypes, network.getDefaultNodeTable());
+	}
+
+	private List<String> getSupportedAttributes(Set<Class<?>> types, CyTable table) {
+		List<String> attributes = new ArrayList<String>();
+		attributes.add(UNWEIGHTED);
+		for (CyColumn column: table.getColumns()) {
+			for (Class<?> type: types) {
+				if (column.getType().equals(type)) {
+					attributes.add(column.getName());
+					break;
+				}
+			}
+		}
+		if (attributes.size() > 1)
+			return attributes;
+		return null;
+	}
+
+	private String getLayoutAttribute() {
+		if (possibleEdgeAttributes != null && !possibleEdgeAttributes.getSelectedValue().equals(UNWEIGHTED))
+			return possibleEdgeAttributes.getSelectedValue();
+		if (possibleNodeAttributes != null && !possibleNodeAttributes.getSelectedValue().equals(UNWEIGHTED))
+			return possibleNodeAttributes.getSelectedValue();
+		return null;
 	}
 
 }
