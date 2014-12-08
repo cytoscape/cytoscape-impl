ccaf71831b03652abb68ff0ca42dc5710b7bf59a
Christian Lopes <chrtannus@gmail.com>
2014-12-08 17:02:32 -0500

    Refs #2939: Getting titled borders from swing-util-api instead.

--- work-swing-impl/impl/src/main/java/org/cytoscape/work/internal/tunables/utils/TitledPanel.java
+++ work-swing-impl/impl/src/main/java/org/cytoscape/work/internal/tunables/utils/TitledPanel.java
@@ -1,15 +1,13 @@
 package org.cytoscape.work.internal.tunables.utils;
 
+import static org.cytoscape.util.swing.LookAndFeelUtil.createTitledBorder;
 import static org.cytoscape.util.swing.LookAndFeelUtil.isAquaLAF;
 
 import java.awt.Component;
 
-import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
-import javax.swing.UIManager;
 import javax.swing.border.Border;
-import javax.swing.border.TitledBorder;
 
 @SuppressWarnings("serial")
 public class TitledPanel extends SimplePanel {
@@ -39,21 +37,5 @@ public class TitledPanel extends SimplePanel {
 		
 		if (isAquaLAF() && c instanceof JPanel)
 			((JPanel) c).setOpaque(false);
-	}
-	
-	private static Border createTitledBorder(final String title) {
-		final Border border;
-		
-		if (title == null || title.trim().isEmpty()) {
-			final Border aquaBorder = isAquaLAF() ? UIManager.getBorder("InsetBorder.aquaVariant") : null;
-			border = aquaBorder != null ? aquaBorder : BorderFactory.createTitledBorder("SAMPLE").getBorder();
-		} else {
-			final Border aquaBorder = isAquaLAF() ? UIManager.getBorder("TitledBorder.aquaVariant") : null;
-			final TitledBorder tb = aquaBorder != null ?
-					BorderFactory.createTitledBorder(aquaBorder, title) : BorderFactory.createTitledBorder(title);
-			border = tb;
-		}
-		
-		return border;
 	}
 }
