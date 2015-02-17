9457273323e23210b8b0f7663d6b8244652ea31e
Christian Lopes <chrtannus@gmail.com>
2015-02-17 13:41:14 -0500

    Refs #2939: Minor UI improvements to "Add New Ontology Data Source" dialog (Ontology Import) and some refactoring.

--- table-import-impl/src/main/java/org/cytoscape/tableimport/internal/ui/DataSourceSelectDialog.java
+++ table-import-impl/src/main/java/org/cytoscape/tableimport/internal/ui/DataSourceSelectDialog.java
@@ -24,18 +24,32 @@ package org.cytoscape.tableimport.internal.ui;
  * #L%
  */
 
+import static javax.swing.GroupLayout.DEFAULT_SIZE;
+import static javax.swing.GroupLayout.PREFERRED_SIZE;
+import static javax.swing.GroupLayout.Alignment.CENTER;
+import static javax.swing.GroupLayout.Alignment.LEADING;
+import static javax.swing.GroupLayout.Alignment.TRAILING;
+
 import java.awt.Dialog;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
 import org.cytoscape.util.swing.FileChooserFilter;
 import org.cytoscape.util.swing.FileUtil;
+import org.cytoscape.util.swing.LookAndFeelUtil;
 
 /**
  *
@@ -44,6 +58,16 @@ public class DataSourceSelectDialog extends JDialog {
 
 	private static final long serialVersionUID = -2750086123978509461L;
 
+	private JPanel mainPanel;
+	private JPanel buttonPanel;
+	private JButton addButton;
+	private JButton browseButton;
+	private JButton cancelButton;
+	private JTextField dataSourceTextField;
+	private JLabel nameLabel;
+	private JTextField ontologyNameTextField;
+	private JLabel sourceLabel;
+	
 	private int sourceType;
 	private String sourceName;
 	private String sourceFileName;
@@ -65,134 +89,112 @@ public class DataSourceSelectDialog extends JDialog {
 	}
 
 	private void initComponents() {
-		titleLabel = new javax.swing.JLabel();
-		jSeparator1 = new javax.swing.JSeparator();
-		ontologyNameTextField = new javax.swing.JTextField();
-		dataSourceTextField = new javax.swing.JTextField();
-		cancelButton = new javax.swing.JButton();
-		addButton = new javax.swing.JButton();
-		browseButton = new javax.swing.JButton();
-		nameLabel = new javax.swing.JLabel();
-		sourceLabel = new javax.swing.JLabel();
+		ontologyNameTextField = new JTextField();
+		dataSourceTextField = new JTextField();
+		cancelButton = new JButton("Cancel");
+		addButton = new JButton("Add");
+		browseButton = new JButton("Browse Local Files");
+		nameLabel = new JLabel("Data Source Name:");
+		sourceLabel = new JLabel("Data Source URL:");
 
-		setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-		setTitle("Add new ontology data source");
-		titleLabel.setFont(new java.awt.Font("SansSerif", 1, 14));
-		titleLabel.setText("Add New Data Source");
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		setTitle("Add New Ontology Data Source");
 
 		ontologyNameTextField.setToolTipText("Add data source from local file system...");
-		ontologyNameTextField.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				ontologyNameTextFieldActionPerformed(evt);
-			}
-		});
 
 		dataSourceTextField.setText("http://");
 		dataSourceTextField.setToolTipText("http, ftp, and file are supported.");
 
-		cancelButton.setText("Cancel");
-		cancelButton.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
+		cancelButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
 				cancelButtonActionPerformed(evt);
 			}
 		});
 
-		addButton.setText("Add");
-		addButton.setMaximumSize(new java.awt.Dimension(75, 25));
-		addButton.setMinimumSize(new java.awt.Dimension(75, 25));
-		addButton.setPreferredSize(new java.awt.Dimension(75, 25));
-		addButton.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
+		addButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
 				addButtonActionPerformed(evt);
 			}
 		});
 
-		browseButton.setText("Browse Local Files");
 		browseButton.setToolTipText("Add data source from local file system...");
-		browseButton.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
+		browseButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
 				browseButtonActionPerformed(evt);
 			}
 		});
 
-		nameLabel.setText("Data Source Name:");
-
-		sourceLabel.setText("Data Source URL:");
-
-		org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
+		final GroupLayout layout = new GroupLayout(getContentPane());
 		getContentPane().setLayout(layout);
-		layout.setHorizontalGroup(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING).add(
-				layout.createSequentialGroup()
-						.addContainerGap()
-						.add(layout
-								.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-								.add(jSeparator1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 681, Short.MAX_VALUE)
-								.add(titleLabel)
-								.add(org.jdesktop.layout.GroupLayout.TRAILING,
-										layout.createSequentialGroup()
-												.add(addButton, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE,
-														org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-														org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-												.addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-												.add(cancelButton)
-												.addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED))
-								.add(layout
-										.createSequentialGroup()
-										.add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-												.add(sourceLabel).add(nameLabel))
-										.addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-										.add(layout
-												.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
-												.add(layout
-														.createSequentialGroup()
-														.add(dataSourceTextField,
-																org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 378,
-																Short.MAX_VALUE)
-														.addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-														.add(browseButton,
-																org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 169,
-																Short.MAX_VALUE))
-												.add(ontologyNameTextField,
-														org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 553,
-														Short.MAX_VALUE)))).addContainerGap()));
-		layout.setVerticalGroup(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING).add(
-				layout.createSequentialGroup()
-						.addContainerGap()
-						.add(titleLabel)
-						.add(8, 8, 8)
-						.add(jSeparator1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE,
-								org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-								org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-						.addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-						.add(layout
-								.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-								.add(nameLabel)
-								.add(ontologyNameTextField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE,
-										org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-										org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-						.addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-						.add(layout
-								.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-								.add(sourceLabel)
-								.add(dataSourceTextField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE,
-										org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-										org.jdesktop.layout.GroupLayout.PREFERRED_SIZE).add(browseButton))
-						.addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED,
-								org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.add(layout
-								.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-								.add(addButton, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE,
-										org.jdesktop.layout.GroupLayout.DEFAULT_SIZE,
-										org.jdesktop.layout.GroupLayout.PREFERRED_SIZE).add(cancelButton))
-						.addContainerGap()));
+		layout.setAutoCreateContainerGaps(true);
+		layout.setAutoCreateGaps(true);
+		
+		layout.setHorizontalGroup(layout.createParallelGroup(LEADING, true)
+				.addComponent(getMainPanel(), DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+				.addComponent(getButtonPanel(), DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+		);
+		layout.setVerticalGroup(layout.createSequentialGroup()
+				.addComponent(getMainPanel(), PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+				.addComponent(getButtonPanel(), PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+		);
+		
 		pack();
-	} // </editor-fold>
+	}
+	
+	private JPanel getMainPanel() {
+		if (mainPanel == null) {
+			mainPanel = new JPanel();
+			
+			final GroupLayout layout = new GroupLayout(mainPanel);
+			mainPanel.setLayout(layout);
+			layout.setAutoCreateContainerGaps(true);
+			layout.setAutoCreateGaps(true);
+			
+			layout.setHorizontalGroup(layout.createSequentialGroup()
+					.addGroup(layout.createParallelGroup(TRAILING)
+							.addComponent(nameLabel)
+							.addComponent(sourceLabel)
+					)
+					.addGroup(layout.createParallelGroup(LEADING)
+							.addComponent(ontologyNameTextField, DEFAULT_SIZE, 540, Short.MAX_VALUE)
+							.addGroup(layout.createSequentialGroup()
+									.addComponent(dataSourceTextField, DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(browseButton, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+							)
+					)
+			);
+			layout.setVerticalGroup(layout.createSequentialGroup()
+					.addGroup(layout.createParallelGroup(CENTER)
+							.addComponent(nameLabel)
+							.addComponent(ontologyNameTextField, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+					)
+					.addGroup(layout.createParallelGroup(CENTER)
+							.addComponent(sourceLabel)
+							.addComponent(dataSourceTextField, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+							.addComponent(browseButton, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+					)
+			);
+		}
+		
+		return mainPanel;
+	}
+	
+	private JPanel getButtonPanel() {
+		if (buttonPanel == null) {
+			buttonPanel = LookAndFeelUtil.createOkCancelPanel(addButton, cancelButton);
+		}
+		
+		return buttonPanel;
+	}
 
-	private void cancelButtonActionPerformed(java.awt.event.ActionEvent evt) {
+	private void cancelButtonActionPerformed(ActionEvent evt) {
 		dispose();
 	}
 
-	private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {
+	private void addButtonActionPerformed(ActionEvent evt) {
 		final String name = ontologyNameTextField.getText();
 		final String source = this.dataSourceTextField.getText();
 
@@ -207,7 +209,6 @@ public class DataSourceSelectDialog extends JDialog {
 	}
 
 	private void browseButtonActionPerformed(ActionEvent evt) {
-
 		File file = null;
 
 		if (sourceType == ONTOLOGY_TYPE) {
@@ -243,47 +244,15 @@ public class DataSourceSelectDialog extends JDialog {
 		this.toFront();
 	}
 
-	private void ontologyNameTextFieldActionPerformed(java.awt.event.ActionEvent evt) {
-		// TODO add your handling code here:
-	}
-
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @return DOCUMENT ME!
-	 */
 	public String getSourceUrlString() {
 		return sourceUrlString;
 	}
 
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @return DOCUMENT ME!
-	 */
 	public String getSourceName() {
 		return sourceName;
 	}
 
-	/**
-	 * DOCUMENT ME!
-	 * 
-	 * @return DOCUMENT ME!
-	 */
 	public String getSourceFileName() {
 		return sourceFileName;
 	}
-
-	// Variables declaration - do not modify
-	private javax.swing.JButton addButton;
-	private javax.swing.JButton browseButton;
-	private javax.swing.JButton cancelButton;
-	private javax.swing.JTextField dataSourceTextField;
-	private javax.swing.JSeparator jSeparator1;
-	private javax.swing.JLabel nameLabel;
-	private javax.swing.JTextField ontologyNameTextField;
-	private javax.swing.JLabel sourceLabel;
-	private javax.swing.JLabel titleLabel;
-
-	// End of variables declaration
 }
