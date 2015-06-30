a64c203889d46aa51df0ab565c410883bbaeafd3
Christian Lopes <chrtannus@gmail.com>
2015-06-30 18:25:17 -0400

    Refs #3187: Fixes NPE caused by unit tests.

--- core-task-impl/src/main/java/org/cytoscape/task/internal/table/MergeTablesTask.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/table/MergeTablesTask.java
@@ -651,6 +651,9 @@ public class MergeTablesTask extends AbstractTask implements TunableValidator {
 			Collections.sort(names, new Comparator<String>() {
 				@Override
 				public int compare(String s1, String s2) {
+					if (s1 == null && s2 == null) return 0;
+					if (s1 == null) return -1;
+					if (s2 == null) return 1;
 					return collator.compare(s1, s2);
 				}
 			});
