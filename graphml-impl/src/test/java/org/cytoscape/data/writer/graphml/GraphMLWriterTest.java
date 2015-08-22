6be3a94e634484f9f64c6aed3627f903d07f7754
keiono <keiono@gmail.com>
2015-08-21 22:21:40 -0700

    refs #2907 Tests for GraphMLWriter added.

--- graphml-impl/src/test/java/org/cytoscape/data/writer/graphml/GraphMLWriterTest.java
+++ graphml-impl/src/test/java/org/cytoscape/data/writer/graphml/GraphMLWriterTest.java
@@ -1,5 +1,23 @@
 package org.cytoscape.data.writer.graphml;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.cytoscape.ding.NetworkViewTestSupport;
+import org.cytoscape.io.internal.write.graphml.GraphMLWriter;
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
+import org.junit.After;
+import org.junit.Before;
+
 /*
  * #%L
  * Cytoscape GraphML Impl (graphml-impl)
@@ -25,13 +43,86 @@ package org.cytoscape.data.writer.graphml;
  */
 
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class GraphMLWriterTest {
-	
-	@Test
-	public void testWrite1() {
+
+	private final NetworkViewTestSupport support = new NetworkViewTestSupport();
+
+	private CyNetwork network1;
+
+	@Before
+	public void setUp() throws Exception {
+		network1 = support.getNetwork();
+		network1.getRow(network1).set(CyNetwork.NAME, "SampleNet1");
 		
-		//GraphMLWriter writer = new GraphMLWriter();
+		CyNode n1 = network1.addNode();
+		CyNode n2 = network1.addNode();
+		CyNode n3 = network1.addNode();
+
+		// Not connected
+		CyNode n4 = network1.addNode();
+
+		CyEdge e1 = network1.addEdge(n1, n2, true);
+		CyEdge e2 = network1.addEdge(n2, n3, true);
+		CyEdge e3 = network1.addEdge(n1, n3, true);
+		CyEdge e1self = network1.addEdge(n1, n1, true);
+
+		network1.getRow(n1).set(CyNetwork.NAME, "n1");
+		network1.getRow(n2).set(CyNetwork.NAME, "n2 日本語");
+		network1.getRow(n3).set(CyNetwork.NAME, "n3");
+		network1.getRow(n4).set(CyNetwork.NAME, "Alone");
+
+		network1.getRow(e1).set(CyNetwork.NAME, "e1");
+		network1.getRow(e2).set(CyNetwork.NAME, "e2");
+		network1.getRow(e3).set(CyNetwork.NAME, "e3");
+		network1.getRow(e1self).set(CyNetwork.NAME, "e1self");
 	}
 
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGraphMLWriter() throws Exception {
+		assertNotNull(network1);
+
+		final File temp = File.createTempFile("network1", ".graphml");
+		temp.deleteOnExit();
+
+		OutputStream os = new FileOutputStream(temp);
+		GraphMLWriter writer = new GraphMLWriter(os, network1);
+		writer.run(null);
+		os.close();
+
+		// Read contents
+		System.out.println("Temp GraphML file = " + temp.getAbsolutePath());
+
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(temp);
+		doc.getDocumentElement().normalize();
+
+		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+		NodeList graphList = doc.getElementsByTagName("graph");
+
+		assertNotNull(graphList);
+		assertEquals(1, graphList.getLength());
+		Node graphNode = graphList.item(0);
+		Node networkName = graphNode.getAttributes().getNamedItem("id");
+		System.out.println("Network Name :" + networkName.getNodeValue());
+		assertEquals("SampleNet1", networkName.getNodeValue());
+		
+		NodeList nList = doc.getElementsByTagName("node");
+		assertEquals(4, nList.getLength());
+
+		NodeList eList = doc.getElementsByTagName("edge");
+		assertEquals(4, eList.getLength());
+		
+		NodeList typeList = doc.getElementsByTagName("key");
+		assertEquals(14, typeList.getLength());
+	}
 }
