1dc8caddc890a04c90f44a30dabe215e5087fbe5
Christian Lopes <chrtannus@gmail.com>
2016-02-26 14:24:32 -0500

    Refs #3454: Updating Network List's filler panel height (it allows the user to click bellow the items to deselect all networks) and vertical scrollbar correctly now.

--- swing-application-impl/src/main/java/org/cytoscape/internal/view/NetworkMainPanel.java
+++ swing-application-impl/src/main/java/org/cytoscape/internal/view/NetworkMainPanel.java
@@ -17,6 +17,8 @@ import java.awt.Rectangle;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -236,6 +238,12 @@ public class NetworkMainPanel extends JPanel implements CytoPanelComponent2, Net
 	private JScrollPane getRootNetworkScroll() {
 		if (rootNetworkScroll == null) {
 			rootNetworkScroll = new JScrollPane(getRootNetworkListPanel());
+			rootNetworkScroll.addComponentListener(new ComponentAdapter() {
+				@Override
+				public void componentResized(ComponentEvent e) {
+					getRootNetworkListPanel().updateScrollableTracksViewportHeight();
+				}
+			});
 		}
 		
 		return rootNetworkScroll;
@@ -1273,6 +1281,7 @@ public class NetworkMainPanel extends JPanel implements CytoPanelComponent2, Net
 	private class RootNetworkListPanel extends JPanel implements Scrollable {
 		
 		private final JPanel filler = new JPanel();
+		private boolean scrollableTracksViewportHeight;
 		
 		private final Map<CyRootNetwork, RootNetworkPanel> items = new LinkedHashMap<>();
 		
@@ -1292,11 +1301,33 @@ public class NetworkMainPanel extends JPanel implements CytoPanelComponent2, Net
 			
 			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 			add(filler);
+			
+			updateScrollableTracksViewportHeight();
 		}
 		
 		void update() {
 			for (final RootNetworkPanel item : getAllItems())
 				item.update();
+			
+			updateScrollableTracksViewportHeight();
+		}
+		
+		void updateScrollableTracksViewportHeight() {
+			final boolean oldValue = scrollableTracksViewportHeight;
+			
+			if (items == null || items.isEmpty()) {
+				scrollableTracksViewportHeight = true;
+			} else {
+				int ih = 0; // Total items height
+				
+				for (RootNetworkPanel rnp : items.values())
+					ih += rnp.getHeight();
+				
+				scrollableTracksViewportHeight = ih <= getRootNetworkScroll().getViewport().getHeight();
+			}
+			
+			if (oldValue != scrollableTracksViewportHeight)
+				updateUI();
 		}
 
 		Collection<RootNetworkPanel> getAllItems() {
@@ -1308,6 +1339,13 @@ public class NetworkMainPanel extends JPanel implements CytoPanelComponent2, Net
 				final RootNetworkPanelModel model = new RootNetworkPanelModel(rootNetwork, serviceRegistrar);
 				final RootNetworkPanel rootNetworkPanel = new RootNetworkPanel(model, showNodeEdgeCount, serviceRegistrar);
 				rootNetworkPanel.setAlignmentX(LEFT_ALIGNMENT);
+				
+				rootNetworkPanel.addComponentListener(new ComponentAdapter() {
+					@Override
+					public void componentResized(ComponentEvent e) {
+						updateScrollableTracksViewportHeight();
+					}
+				});
 				
 				items.put(rootNetwork, rootNetworkPanel);
 				add(rootNetworkPanel, getComponentCount() - 1);
@@ -1361,7 +1399,7 @@ public class NetworkMainPanel extends JPanel implements CytoPanelComponent2, Net
 
 		@Override
 		public boolean getScrollableTracksViewportHeight() {
-			return items == null || items.isEmpty();
+			return scrollableTracksViewportHeight;
 		}
 	}
 	
