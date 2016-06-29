05504de0a2100225a095e9c0572d5e2906ea7f77
Adam Treister <adamtreister@gmail.com>
2016-06-29 14:00:18 -0700

    fix minor copy error:  call getLogger on this class

--- table-import-impl/src/main/java/org/cytoscape/tableimport/internal/task/SelectURLTableTask.java
+++ table-import-impl/src/main/java/org/cytoscape/tableimport/internal/task/SelectURLTableTask.java
@@ -51,7 +51,7 @@ public class SelectURLTableTask extends AbstractTask {
 	private InputStream stream;
 	private final CyServiceRegistrar serviceRegistrar;
 	
-	private static final Logger logger = LoggerFactory.getLogger( SelectFileTableTask.class ); 
+	private static final Logger logger = LoggerFactory.getLogger( SelectURLTableTask.class ); 
 
 	public SelectURLTableTask(final Task readerTask, final CyServiceRegistrar serviceRegistrar) {
 		if (readerTask instanceof LoadTableReaderTask) {
