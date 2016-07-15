b3e24be02bf6aca7094199f7c4970b8329f79046
Christian Lopes <chrtannus@gmail.com>
2016-07-15 15:06:31 -0400

    Refs #3584: Fixes importing multiple network files by drag-and-drop (files loaded in sequence now). Disables drag-and-drop of image and text for now. Code reformat and cleanup.

--- swing-application-impl/src/main/java/org/cytoscape/internal/view/CyDropListener.java
+++ swing-application-impl/src/main/java/org/cytoscape/internal/view/CyDropListener.java
@@ -8,7 +8,7 @@ import java.awt.dnd.DropTargetDropEvent;
 import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
 import java.io.File;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.BorderFactory;
@@ -19,153 +19,157 @@ import javax.swing.border.Border;
 
 import org.cytoscape.service.util.CyServiceRegistrar;
 import org.cytoscape.task.read.LoadNetworkFileTaskFactory;
+import org.cytoscape.work.FinishStatus;
+import org.cytoscape.work.ObservableTask;
+import org.cytoscape.work.TaskObserver;
 import org.cytoscape.work.swing.DialogTaskManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
+ * #%L
+ * Cytoscape Swing Application Impl (swing-application-impl)
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2006 - 2016 The Cytoscape Consortium
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 @SuppressWarnings("serial")
 public class CyDropListener implements DropTargetListener {
 
-	private JComponent panel; // the zone that accepts the drop
-	private CyServiceRegistrar registrar; // how to pass the drop to a service
-	private Border borderSave; // we show that a drag is ongoing by changing
+	private final JComponent parentPanel; // the zone that accepts the drop
+	private Border originalBorder;
+	
+	private final CyServiceRegistrar serviceRegistrar;
 	
 	private static final Logger logger = LoggerFactory.getLogger(CyDropListener.class);
 
-	public static boolean isAcceptable(DropTargetDropEvent ev) {
-		if (ev.isDataFlavorSupported(DataFlavor.imageFlavor))			return true;
-		if (ev.isDataFlavorSupported(DataFlavor.stringFlavor))			return true;
-		if (ev.isDataFlavorSupported(DataFlavor.javaFileListFlavor))	return true;		
-		return false;
-	}
-	public static boolean isAcceptable(TransferHandler.TransferSupport info) {
-		if (info.isDataFlavorSupported(DataFlavor.imageFlavor))			return true;
-		if (info.isDataFlavorSupported(DataFlavor.stringFlavor))		return true;
-		if (info.isDataFlavorSupported(DataFlavor.javaFileListFlavor))	return true;		
-		return false;
-	}
-
-	public CyDropListener(JComponent parentPanel, CyServiceRegistrar reg)
-	{
-// 		System.out.println("CyDropListener1");	
-		panel = parentPanel;
-		registrar = reg;
+	public CyDropListener(final JComponent parentPanel, final CyServiceRegistrar serviceRegistrar) {
+		this.parentPanel = parentPanel;
+		this.serviceRegistrar = serviceRegistrar;
+		
 		parentPanel.setTransferHandler(new TransferHandler() {
-	
-		        @Override    public boolean canImport(TransferHandler.TransferSupport info) {
-		        	return	isAcceptable(info);
-		        }
-	
-		        @Override      public boolean importData(TransferHandler.TransferSupport info) {
-		    		System.out.println("importData");
-		            if (!info.isDrop())                 return false;
-		            if (!isAcceptable(info))            return false;
-	
-		            Transferable t = info.getTransferable();
-	            if (info.isDataFlavorSupported(DataFlavor.stringFlavor)) 
-	            	{        
-			    		System.out.println("annotate with Text: ");			//+ info.getTransferable().getTransferData()
-			            try {
-			            		System.out.println("" + t.getTransferData(DataFlavor.stringFlavor));
-			            }
-			            catch (Exception e) {  System.out.println("Exception"); }
-
-	            	}
-		            if (info.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) 
-		            	{        
-	
-		            // Get the fileList that is being dropped.
-		            List<File> data;
-		            try {
-		                data = (List<File>)t.getTransferData(DataFlavor.javaFileListFlavor);
-		            } 
-		            catch (Exception e) { return false; }
-	//		            DefaultListModel model = (DefaultListModel) fileDropper.getModel();
-		            for (File file : data) {
-	//		                model.addElement(file);
-		            }
-		            return true;
-		        }
-		        return false;
+	        @Override
+	        public boolean canImport(TransferHandler.TransferSupport info) {
+	        	return isAcceptable(info);
+	        }
+	        @Override
+	        public boolean importData(TransferHandler.TransferSupport info) {
+	            return info.isDrop() && !isAcceptable(info);
 	        }
 	    });
+		
 		new DropTarget(parentPanel,this);
 	}
 
 	@Override
-	public void dragEnter(DropTargetDragEvent dtde) {
-		borderSave = panel.getBorder();
-		panel.setBorder(BorderFactory.createLineBorder(UIManager.getColor("Focus.color"), 2));
+	public void dragEnter(DropTargetDragEvent evt) {
+		originalBorder = parentPanel.getBorder();
+		parentPanel.setBorder(BorderFactory.createLineBorder(UIManager.getColor("Focus.color"), 2));
 	}
 
 	@Override
-	public void dragExit(DropTargetEvent dtde) {
-		panel.setBorder(borderSave);
+	public void dragExit(DropTargetEvent evt) {
+		parentPanel.setBorder(originalBorder);
 	}
 
 	@Override
-	public void dragOver(DropTargetDragEvent dtde) {
-// 		System.out.println("move");
-
+	public void dragOver(DropTargetDragEvent evt) {
+	}
+	
+	@Override
+	public void dropActionChanged(DropTargetDragEvent evt) {
 	}
 
 	@Override
-	public void drop(DropTargetDropEvent dtde) {
-// 		System.out.println("Drop it here!");
-		panel.setBorder(borderSave);
-		dtde.acceptDrop(dtde.getDropAction());
-//		panel.getTransferHandler().importData(panel, dtde.getTransferable());
-        if (!isAcceptable(dtde))            return;
+	@SuppressWarnings("unchecked")
+	public void drop(DropTargetDropEvent evt) {
+		parentPanel.setBorder(originalBorder);
+		evt.acceptDrop(evt.getDropAction());
+        
+		if (!isAcceptable(evt))
+        	return;
 
-        Transferable t = dtde.getTransferable();
-        if (dtde.isDataFlavorSupported(DataFlavor.stringFlavor)) 
-    	{        
-    		System.out.println("annotate with Text: ");			//+ info.getTransferable().getTransferData()
-            try {
-            		String txt = "" + t.getTransferData(DataFlavor.stringFlavor);
-            		System.out.println("" + txt);
-            }
-            catch (Exception e) {  System.out.println("Text Exception: " + e.getMessage()); }
-            return;
-
-    	}
-        if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) 
-        {        
-    		System.out.println("Drop file handler");
+		Transferable t = evt.getTransferable();
+		
+		if (evt.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+			// TODO: Add Text annotation to network view (probably better to do it from Ding)
+			try {
+				String txt = "" + t.getTransferData(DataFlavor.stringFlavor);
+			} catch (Exception e) {
+				logger.error("Cannot add text to network by Drag-and-Drop.", e);
+			}
+		} else if (evt.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {       
             // Get the fileList that is being dropped.
 	        List<File> data;
+	        
 	        try {
-	            data = (List<File>)t.getTransferData(DataFlavor.javaFileListFlavor);
-	        } 
-	        catch (Exception e) { 
-	        	System.out.println("Exception: " + e.getMessage()); 
+	            data = (List<File>) t.getTransferData(DataFlavor.javaFileListFlavor);
+	        } catch (Exception e) { 
+	        	logger.error("Cannot load network files by Drag-and-Drop.", e);
 	        	return; 
 	        }
+	        
 	        loadFiles(data);
-	        return;
         }
 	}
-
+	
 	private void loadFiles(final List<File> data) {
-		final DialogTaskManager taskManager = registrar.getService(DialogTaskManager.class);
-		final LoadNetworkFileTaskFactory factory = registrar.getService(LoadNetworkFileTaskFactory.class,
+		final DialogTaskManager taskManager = serviceRegistrar.getService(DialogTaskManager.class);
+		final LoadNetworkFileTaskFactory factory = serviceRegistrar.getService(LoadNetworkFileTaskFactory.class,
 				"(id=loadNetworkFileTaskFactory)");
 
-		for (File file : data) {
-			if (file.isDirectory()) {
-				loadFiles(Arrays.asList(file.listFiles()));
-			} else if (factory != null) {
+		if (factory != null)
+			loadFiles(data.iterator(), taskManager, factory);
+	}
+	
+	private void loadFiles(final Iterator<File> iterator, final DialogTaskManager taskManager,
+			final LoadNetworkFileTaskFactory factory) {
+		while (iterator.hasNext()) {
+			final File file = iterator.next();
+			
+			if (!file.isDirectory()) {
 				try {
-					taskManager.execute(factory.createTaskIterator(file));
+					taskManager.execute(factory.createTaskIterator(file), new TaskObserver() {
+						@Override
+						public void taskFinished(ObservableTask task) {
+						}
+						@Override
+						public void allFinished(FinishStatus finishStatus) {
+							// Load the other files recursively
+							loadFiles(iterator, taskManager, factory);
+						}
+					});
 				} catch (Exception e) {
-					logger.error("Cannot load network file.", e);
+					logger.error("Cannot load network file by Drag-and-Drop.", e);
 				}
+				
+				return;
 			}
 		}
 	}
 
-	@Override
-	public void dropActionChanged(DropTargetDragEvent dtde) {
-		System.out.println("dropActionChanged");
+	private static boolean isAcceptable(DropTargetDropEvent evt) {
+		return evt.isDataFlavorSupported(DataFlavor.javaFileListFlavor);
+	}
+
+	private static boolean isAcceptable(TransferHandler.TransferSupport info) {
+		return info.isDataFlavorSupported(DataFlavor.javaFileListFlavor);
 	}
 }
