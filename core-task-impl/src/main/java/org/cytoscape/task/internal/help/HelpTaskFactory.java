5094061ddd5828c0524767ce36c1f881b48eacff
Adam Treister <adamtreister@gmail.com>
2017-04-05 13:08:57 -0700

    updating HelpTaskFactory to current version

--- core-task-impl/src/main/java/org/cytoscape/task/internal/help/HelpTaskFactory.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/help/HelpTaskFactory.java
@@ -39,6 +39,6 @@ public class HelpTaskFactory extends AbstractTaskFactory {
 	}
 
 	public TaskIterator createTaskIterator() {
-		return new TaskIterator(new HelpTask(registrar,"http://manual.cytoscape.org/en/3.4.0/Quick_Tour_of_Cytoscape.html"));
+		return new TaskIterator(new HelpTask(registrar,"http://manual.cytoscape.org/en/3.5.0/Quick_Tour_of_Cytoscape.html"));
 	} 
 }
