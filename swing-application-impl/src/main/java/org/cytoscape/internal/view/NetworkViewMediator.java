35e1129b3f5d8afd48d2705165b90218fe571373
Christian Lopes <chrtannus@gmail.com>
2017-07-24 16:43:32 -0400

    Refs #3832: NetView Mediator now uses a simple Timer with a small delay to apply styles and update views, in order to avoid unnecessary updates when it catches events that happen too close to each other.

--- swing-application-impl/src/main/java/org/cytoscape/internal/view/NetworkViewMediator.java
+++ swing-application-impl/src/main/java/org/cytoscape/internal/view/NetworkViewMediator.java
@@ -22,6 +22,7 @@ import java.util.WeakHashMap;
 
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.Timer;
 
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.application.NetworkViewRenderer;
@@ -129,12 +130,14 @@ public class NetworkViewMediator
 	private final Map<CyNetworkView, RenderingEngine<CyNetwork>> presentationMap;
 	private final Set<CyNetworkView> viewUpdateRequired;
 
-	/** columnIdentifier -> { valueInfo -> [views] }*/
+	/** columnIdentifier -> { valueInfo -> [views] } */
 	private final Map<CyColumnIdentifier, Map<MappedVisualPropertyValueInfo, Set<View<?>>>> mappedValuesMap;
 	
+	private final Map<CyNetworkView, Timer> viewUpdateTimers;
 	private volatile boolean loadingSession;
 
 	private final CyServiceRegistrar serviceRegistrar;
+	private final Object lock = new Object();
 	
 	public NetworkViewMediator(
 			final NetworkViewMainPanel networkViewMainPanel,
@@ -150,6 +153,7 @@ public class NetworkViewMediator
 		presentationMap = new WeakHashMap<>();
 		viewUpdateRequired = new HashSet<>();
 		mappedValuesMap = new HashMap<>();
+		viewUpdateTimers = new WeakHashMap<>();
 		
 		initComponents();
 	}
@@ -218,6 +222,15 @@ public class NetworkViewMediator
 	@Override
 	public void handleEvent(NetworkViewAboutToBeDestroyedEvent nvde) {
 		final CyNetworkView view = nvde.getNetworkView();
+		Timer timer = null;
+		
+		synchronized (lock) {
+			timer = viewUpdateTimers.remove(view);
+		}
+		
+		if (timer != null)
+			timer.stop();
+		
 		removeView(view);
 	}
 	
@@ -739,19 +752,37 @@ public class NetworkViewMediator
 		return result;
 	}
 	
-	private void updateView(final CyNetworkView view, VisualStyle vs) {
+	private void updateView(final CyNetworkView view, final VisualStyle style) {
 		if (view == null)
 			return;
 		
 		if (getNetworkViewMainPanel().isGridVisible()
 				|| view.equals(getNetworkViewMainPanel().getCurrentNetworkView())) {
-			if (vs == null) {
-				final VisualMappingManager vmm = serviceRegistrar.getService(VisualMappingManager.class);
-				vs = vmm.getVisualStyle(view);
+			Timer timer = null;
+			
+			synchronized (lock) {
+				timer = viewUpdateTimers.get(view);
 			}
 			
-			vs.apply(view);
-			view.updateView();
+			if (timer == null) {
+				timer = new Timer(0, evt -> {
+					VisualStyle vs = style != null ?
+							style : serviceRegistrar.getService(VisualMappingManager.class).getVisualStyle(view);
+					vs.apply(view);
+					view.updateView();
+				});
+				timer.setRepeats(false);
+				timer.setCoalesce(true);
+				
+				synchronized (lock) {
+					viewUpdateTimers.put(view, timer);
+				}
+			} else {
+				timer.stop();
+			}
+			
+			timer.setInitialDelay(120);
+			timer.start();
 		} else {
 			viewUpdateRequired.add(view);
 		}
