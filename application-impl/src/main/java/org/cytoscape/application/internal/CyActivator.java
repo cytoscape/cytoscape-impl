54c8f84c4b675da2036e864bb79861ca7a1aea70
Keiichiro Ono <keiono@gmail.com>
2017-10-06 15:00:11 -0700

    installoptions handler added.

--- application-impl/src/main/java/org/cytoscape/application/internal/CyActivator.java
+++ application-impl/src/main/java/org/cytoscape/application/internal/CyActivator.java
@@ -3,9 +3,12 @@ package org.cytoscape.application.internal;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 import org.cytoscape.application.CyApplicationConfiguration;
+import org.cytoscape.application.CyUserLog;
 import org.cytoscape.application.NetworkViewRenderer;
 import org.cytoscape.property.CyProperty;
 import org.cytoscape.service.util.AbstractCyActivator;
@@ -14,6 +17,8 @@ import org.cytoscape.view.model.CyNetworkViewFactory;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /*
  * #%L
@@ -41,7 +46,12 @@ import org.osgi.framework.Constants;
 
 public class CyActivator extends AbstractCyActivator {
 
+	private static final String PROP_PREFIX = "installoptions";
+	private static final String SHARE_STATISTICS_TAG = "shareStatistics";
+	private static final String SHARE_STATISTICS_DATE_TAG = "shareStatisticsDate";
 	private static final String INSTALL_OPTIONS_FILE_NAME = "cytoscape.installoptions";
+
+	private static final Logger logger = LoggerFactory.getLogger(CyUserLog.NAME);
 
 	@Override
 	public void start(BundleContext bc) {
@@ -69,21 +79,57 @@ public class CyActivator extends AbstractCyActivator {
 		final CyProperty<Properties> cyPropertyServiceRef = getService(bc, CyProperty.class,
 				"(cyPropertyName=cytoscape3.props)");
 
+		checkIsntallOptions(cyApplicationConfiguration, cyPropertyServiceRef);
 	}
 
-	private void checkIsntallOptions(CyApplicationConfiguration appConfig) {
+	private void checkIsntallOptions(CyApplicationConfiguration appConfig,
+									 CyProperty<Properties> cyPropertyServiceRef) {
 		final File installationLocation = appConfig.getInstallationDirectoryLocation();
 		final File optionFile = new File(
 				installationLocation.getAbsolutePath(), INSTALL_OPTIONS_FILE_NAME);
 
+
 		if(optionFile.exists()) {
+			logger.info("Install option file found: " + optionFile.toString());
+
 			// Extract
 			final Properties prop = new Properties();
 			try {
 				final FileInputStream is = new FileInputStream(optionFile);
 				prop.load(is);
 			} catch(IOException e) {
+				logger.warn("Could not read the install option property file.", e);
+			}
 
+			final String shareStatistics = prop.getProperty(SHARE_STATISTICS_TAG);
+			final String shareStatisticsDate = prop.getProperty(SHARE_STATISTICS_DATE_TAG);
+
+			final String currentDate = cyPropertyServiceRef.getProperties().getProperty(PROP_PREFIX + "." + SHARE_STATISTICS_DATE_TAG);
+
+			if(shareStatistics == null || shareStatisticsDate == null) {
+				return;
+			}
+
+			if(currentDate == null) {
+				cyPropertyServiceRef.getProperties().setProperty(PROP_PREFIX + "." + SHARE_STATISTICS_TAG, shareStatistics);
+				cyPropertyServiceRef.getProperties().setProperty(PROP_PREFIX + "." + SHARE_STATISTICS_DATE_TAG, shareStatisticsDate);
+			} else {
+			    // Define date format.
+				final SimpleDateFormat parser = new SimpleDateFormat("yyyy/mm/dd hh:mm:ss");
+				try {
+					final Date oldDate = parser.parse(currentDate);
+					final Date newDate = parser.parse(shareStatisticsDate);
+					final int comparison = oldDate.compareTo(newDate);
+
+					if(comparison < 0) {
+						cyPropertyServiceRef.getProperties().setProperty(PROP_PREFIX + "." + SHARE_STATISTICS_TAG, shareStatistics);
+						cyPropertyServiceRef.getProperties().setProperty(PROP_PREFIX + "." + SHARE_STATISTICS_DATE_TAG,
+								shareStatisticsDate);
+					}
+				} catch(Exception e) {
+					// Could not parse
+					logger.warn("Failed to parse Share Statistics Date.", e);
+				}
 			}
 		}
 	}
