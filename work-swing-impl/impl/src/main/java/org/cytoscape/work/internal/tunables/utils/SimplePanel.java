910ab6172495fd57064268eba941673482530b40
Christian Lopes <chrtannus@gmail.com>
2018-01-08 16:57:14 -0500

    Refs #4025: Do not hide a disabled tunable field that depends on another tunable's value, because it may be enabled later.

--- work-swing-impl/impl/src/main/java/org/cytoscape/work/internal/tunables/utils/SimplePanel.java
+++ work-swing-impl/impl/src/main/java/org/cytoscape/work/internal/tunables/utils/SimplePanel.java
@@ -12,6 +12,33 @@ import javax.swing.Box.Filler;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
+import org.cytoscape.work.swing.AbstractGUITunableHandler.TunableFieldPanel;
+import org.cytoscape.work.swing.GUITunableHandler;
+
+/*
+ * #%L
+ * Cytoscape Work Swing Impl (work-swing-impl)
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2006 - 2018 The Cytoscape Consortium
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 @SuppressWarnings("serial")
 public class SimplePanel extends JPanel implements HierarchyListener {
 
@@ -106,28 +133,55 @@ public class SimplePanel extends JPanel implements HierarchyListener {
 
 	@Override
     public void hierarchyChanged(HierarchyEvent e) {
-        if (isDisplayed() && !hasEnabledControls(this))
-        	setVisible(false);
+		// Hide this panel if all of its fields are disabled, so the UI is not cluttered with
+		// fields that cannot be interacted with. However, if any of its children is a tunable that
+		// depends on another tunable, we cannot hide it even when everything is disabled,
+		// since it can become enabled again at any time the field it depends on has its value changed.
+        if (isDisplayed() && !hasDependentFields(this) && !hasEnabledControls(this))
+        		setVisible(false);
+    }
+	
+	public boolean hasDependentFields(final JPanel panel) {
+		final int total = panel.getComponentCount();
+
+		for (int i = 0; i < total; i++) {
+			final Component c = panel.getComponent(i);
+
+			if (c.isVisible()) {
+				if (c instanceof TunableFieldPanel) {
+					GUITunableHandler gh = ((TunableFieldPanel) c).getTunableHandler();
+					
+					// Does this tunable field depend on another tunable field's value
+					if (gh != null && gh.dependsOn() != null && !gh.dependsOn().trim().isEmpty())
+						return true;
+				} else if (c instanceof JPanel) {
+					if (hasDependentFields((JPanel) c))
+						return true;
+				}
+			}
+		}
+
+		return false;
     }
 	
 	public boolean hasEnabledControls(final JPanel panel) {
 		final int total = panel.getComponentCount();
-        
-        for (int i = 0; i < total; i++) {
-        	final Component c = panel.getComponent(i);
-    		
-        	if (c.isVisible() && c.isEnabled()) {
-	    		if (c instanceof JPanel) {
-	    			if (hasEnabledControls((JPanel) c))
-	    				return true;
-	    		} else if (c instanceof Filler == false) {
-	    			return true;
-	    		}
-	    	}
-    	}
-        
-        return false;
-    }
+		
+		for (int i = 0; i < total; i++) {
+			final Component c = panel.getComponent(i);
+			
+			if (c.isVisible() && c.isEnabled()) {
+				if (c instanceof JPanel) {
+					if (hasEnabledControls((JPanel) c))
+						return true;
+				} else if (c instanceof Filler == false) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
 	
 	private boolean isDisplayed() {
         Container c = getParent();
