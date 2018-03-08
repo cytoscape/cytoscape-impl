a82673187bf4d22ba56ff6776846950ea5b26a03
Scooter Morris <scooter@cgl.ucsf.edu>
2018-03-08 13:54:14 -0800

    Refs #4133  Adds support for the new "lookup" parameter in ListSingleSelection toolables

---
+++ work-swing-impl/impl/src/main/java/org/cytoscape/work/internal/tunables/JComboBoxDecorator.java
@@ -0,0 +1,96 @@
+package org.cytoscape.work.internal.tunables;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ * Makes the species combo box searchable.
+ */
+public class JComboBoxDecorator {
+
+	static List<?> previousEntries = new ArrayList<>();
+
+	public static void decorate(final JComboBox<?> jcb, final List<?> entries, boolean begins) {
+		Object selectedItem = jcb.getSelectedItem();
+		jcb.setEditable(true);
+		// jcb.setModel(new DefaultComboBoxModel(entries.toArray()));
+
+		final JTextField textField = (JTextField)jcb.getEditor().getEditorComponent();
+		jcb.setSelectedItem(selectedItem);
+
+		textField.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e) {
+				SwingUtilities.invokeLater(new Runnable() {
+				 	public void run() {
+						int currentCaretPosition=textField.getCaretPosition();
+						comboFilter(textField.getText(), jcb, entries, begins);
+						textField.setCaretPosition(currentCaretPosition);
+				 	}
+				});
+			}
+		});
+	}
+
+	/**
+	 * Create a list of entries that match the user's entered text.
+	 */
+	private static void comboFilter(String enteredText, JComboBox<?> jcb, List<?> entries, boolean begins) {
+		List<Object> entriesFiltered = new ArrayList<>();
+		boolean changed = true;
+		DefaultComboBoxModel<?> jcbModel = (DefaultComboBoxModel<?>) jcb.getModel();
+
+		if (enteredText == null) {
+			return;
+		}
+
+		for (Object entry : entries) {
+			if (begins) {
+				if (entry.toString().toLowerCase().startsWith(enteredText.toLowerCase())) {
+					entriesFiltered.add(entry);
+					// System.out.println(jcbModel.getIndexOf(entry));
+				}
+			} else {
+				if (entry.toString().toLowerCase().contains(enteredText.toLowerCase())) {
+					entriesFiltered.add(entry);
+					// System.out.println(jcbModel.getIndexOf(entry));
+				}
+			}
+		}
+
+		if (previousEntries.size() == entriesFiltered.size()
+				&& previousEntries.containsAll(entriesFiltered)) {
+			changed = false;
+		}
+
+		if (changed && entriesFiltered.size() > 1) {
+			previousEntries = entriesFiltered;
+			jcb.setModel(new DefaultComboBoxModel(entriesFiltered.toArray()));
+			jcb.setSelectedItem(enteredText);
+			jcb.showPopup();
+		} else if (entriesFiltered.size() == 1) {
+			if (entriesFiltered.get(0).toString().equalsIgnoreCase(enteredText)) {
+				previousEntries = new ArrayList<>();
+				jcb.setSelectedItem(entriesFiltered.get(0));
+				jcb.hidePopup();
+			} else {
+				previousEntries = entriesFiltered;
+				jcb.setModel(new DefaultComboBoxModel(entriesFiltered.toArray()));
+				jcb.setSelectedItem(enteredText);
+				jcb.showPopup();
+			}
+		} else if (entriesFiltered.size() == 0) {
+			previousEntries = new ArrayList<>();
+			jcb.hidePopup();
+		}
+	}
+
+}
