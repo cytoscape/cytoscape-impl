8cf78b55169de2a8ab61265acb1c432931ab5308
Mike Kucera <mikekucera@gmail.com>
2018-05-17 15:17:24 -0400

    Use TextIcon for default namespace icon. Refs #4174.

--- swing-application-impl/src/main/java/org/cytoscape/internal/view/CyColumnPresentationManagerImpl.java
+++ swing-application-impl/src/main/java/org/cytoscape/internal/view/CyColumnPresentationManagerImpl.java
@@ -1,5 +1,11 @@
 package org.cytoscape.internal.view;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,14 +15,28 @@ import javax.swing.ImageIcon;
 import org.cytoscape.application.swing.CyColumnPresentation;
 import org.cytoscape.application.swing.CyColumnPresentationManager;
 import org.cytoscape.internal.util.RandomImage;
+import org.cytoscape.util.swing.TextIcon;
 
 public class CyColumnPresentationManagerImpl implements CyColumnPresentationManager {
 	
-	private static final String ICON_PATH = "/images/logo-light-96.png";
+	private static final String FONT_PATH = "/fonts/cytoscape-3.ttf";
 
-	private final CyColumnPresentation CYTOSCAPE_PRESENTATION = new DefaultPresentation("Cytoscape", ICON_PATH); 
-
+	private final CyColumnPresentation cytoscapePresentation;
 	private final Map<String,CyColumnPresentation> presentations = new HashMap<>();
+	
+	public CyColumnPresentationManagerImpl() {
+		CyColumnPresentation defaultPresentation;
+		URL url = getClass().getResource(FONT_PATH);
+		try(InputStream in = url.openStream()) {
+			Font font = Font.createFont(Font.TRUETYPE_FONT, in).deriveFont(15f);
+			Color color = new Color(254, 193, 125);
+			Icon icon = new TextIcon("b", font, color, 16, 16);
+			defaultPresentation = new DefaultPresentation("Cytoscape", icon);
+		} catch (IOException | FontFormatException e) {
+			defaultPresentation = new DefaultPresentation("Cytoscape");
+		}
+		this.cytoscapePresentation = defaultPresentation;
+	}
 	
 
 	public void addPresentation(CyColumnPresentation presentation, Map<String,String> props) {
@@ -33,7 +53,7 @@ public class CyColumnPresentationManagerImpl implements CyColumnPresentationMana
 	@Override
 	public CyColumnPresentation getColumnPresentation(String namespace) {
 		if(namespace == null)
-			return CYTOSCAPE_PRESENTATION;
+			return cytoscapePresentation;
 		return presentations.computeIfAbsent(namespace.toLowerCase(), DefaultPresentation::new);
 	}
 	
@@ -43,14 +63,13 @@ public class CyColumnPresentationManagerImpl implements CyColumnPresentationMana
 		private final String description;
 		private final Icon icon;
 		
-		public DefaultPresentation(String description, String iconPath) {
-			this.description = description;
-			this.icon = new ImageIcon(getClass().getResource(iconPath));
+		public DefaultPresentation(String desc, Icon icon) {
+			this.description = desc;
+			this.icon = (icon == null) ? new ImageIcon(new RandomImage(16,16,desc.hashCode())) : icon;
 		}
 		
 		public DefaultPresentation(String namespace) {
-			this.description = namespace;
-			this.icon = new ImageIcon(new RandomImage(16, 16, namespace.hashCode())); // assume namespace.toLowerCase() has already been done
+			this(namespace, null);
 		}
 		
 		@Override
