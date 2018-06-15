a24303414cae84e82693364af15db48bcc8d3869
Christian Lopes <chrtannus@gmail.com>
2018-06-15 12:57:32 -0400

    Reverts changes from commit 17c08bc67a866cea33396913b38aa7398ef427ff because it prevents the selection info panel from being updated when deselecting nodes by clicking the view background.

--- swing-application-impl/src/main/java/org/cytoscape/internal/view/NetworkViewMediator.java
+++ swing-application-impl/src/main/java/org/cytoscape/internal/view/NetworkViewMediator.java
@@ -411,6 +411,14 @@ public class NetworkViewMediator
 			
 			if (tbl.equals(net.getDefaultNodeTable()) || tbl.equals(net.getDefaultEdgeTable())) {
 				final Collection<CyNetworkView> networkViews = netViewMgr.getNetworkViews(net);
+
+				// Update node/edge selection info
+				final Collection<RowSetRecord> selectedRecords = e.getColumnRecords(CyNetwork.SELECTED);
+				
+				if (!selectedRecords.isEmpty()) {
+					for (final CyNetworkView view : networkViews)
+						getNetworkViewMainPanel().updateSelectionInfo(view);
+				}
 				
 				// Update views with styles affected by this RowsSetEvent
 				final Set<CyNetworkView> viewsToUpdate = new HashSet<>();
