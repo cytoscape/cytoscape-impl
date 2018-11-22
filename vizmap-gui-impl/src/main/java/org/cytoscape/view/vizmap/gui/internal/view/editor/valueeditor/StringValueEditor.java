dc93880d355ff2b5c43ec86377b9ad6edf0844f1
Christian Lopes <chrtannus@gmail.com>
2018-11-22 16:19:08 -0500

    CYTOSCAPE-11897 #comment Default/Bypass editor for Node/Edge Labels and Tooltips now provides a text area that allows users to enter multi-line text.

--- vizmap-gui-impl/src/main/java/org/cytoscape/view/vizmap/gui/internal/view/editor/valueeditor/StringValueEditor.java
+++ vizmap-gui-impl/src/main/java/org/cytoscape/view/vizmap/gui/internal/view/editor/valueeditor/StringValueEditor.java
@@ -1,12 +1,30 @@
 package org.cytoscape.view.vizmap.gui.internal.view.editor.valueeditor;
 
+import static javax.swing.GroupLayout.DEFAULT_SIZE;
+import static javax.swing.GroupLayout.PREFERRED_SIZE;
+
+import java.awt.Component;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import org.cytoscape.util.swing.LookAndFeelUtil;
+import org.cytoscape.view.model.VisualProperty;
+import org.cytoscape.view.presentation.property.BasicVisualLexicon;
+import org.cytoscape.view.vizmap.gui.editor.VisualPropertyValueEditor;
+
 /*
  * #%L
  * Cytoscape VizMap GUI Impl (vizmap-gui-impl)
  * $Id:$
  * $HeadURL:$
  * %%
- * Copyright (C) 2006 - 2013 The Cytoscape Consortium
+ * Copyright (C) 2006 - 2018 The Cytoscape Consortium
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as 
@@ -24,22 +42,62 @@ package org.cytoscape.view.vizmap.gui.internal.view.editor.valueeditor;
  * #L%
  */
 
-import java.awt.Component;
-
-import javax.swing.JOptionPane;
-
-public class StringValueEditor extends AbstractValueEditor<String> {
+public class StringValueEditor implements VisualPropertyValueEditor<String> {
 
 	private static final String MESSAGE = "Enter a new text value:";
-	private static final String TITLE = "Text Input";
 
-	public StringValueEditor() {
-		super(String.class);
+	@Override
+	@SuppressWarnings("unchecked")
+	public <S extends String> String showEditor(Component parent, S initialValue, VisualProperty<S> vp) {
+		if (vp == BasicVisualLexicon.NETWORK_TITLE) // Network Title should not have line breaks
+			return showSimpleTextDialog(parent, (String) initialValue, (VisualProperty<String>) vp);
+		
+		// Labels and Tooltips can have multiple lines
+		return showMultiLineDialog(parent, (String) initialValue, (VisualProperty<String>) vp);
 	}
 
 	@Override
-	public String showEditor(Component parent, String initialValue) {
-		return (String) JOptionPane.showInputDialog(parent, MESSAGE, TITLE, JOptionPane.PLAIN_MESSAGE, null,
-				null, initialValue);
+	public Class<String> getValueType() {
+		return String.class;
+	}
+
+	private String showSimpleTextDialog(Component parent, String initialValue, VisualProperty<String> vp) {
+		return (String) JOptionPane.showInputDialog(parent, MESSAGE, vp.getDisplayName(), JOptionPane.PLAIN_MESSAGE,
+				null, null, initialValue);
+	}
+	
+	private String showMultiLineDialog(Component parent, String initialValue, VisualProperty<String> vp) {
+		JLabel label = new JLabel(MESSAGE);
+		JTextArea ta = new JTextArea(initialValue, 5, 30);
+		JScrollPane scrollPane = new JScrollPane(ta);
+		JPanel panel = new JPanel();
+		
+		if (LookAndFeelUtil.isAquaLAF())
+			panel.setOpaque(false);
+		
+		GroupLayout layout = new GroupLayout(panel);
+		panel.setLayout(layout);
+		layout.setAutoCreateContainerGaps(false);
+		layout.setAutoCreateGaps(true);
+        
+        layout.setHorizontalGroup(layout.createParallelGroup(Alignment.LEADING, true)
+                .addComponent(label, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+                .addComponent(scrollPane, DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(layout.createSequentialGroup()
+	            .addComponent(label, PREFERRED_SIZE, DEFAULT_SIZE, PREFERRED_SIZE)
+	            .addComponent(scrollPane, DEFAULT_SIZE, DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+		
+		if (JOptionPane.OK_OPTION == JOptionPane.showConfirmDialog(
+				parent,
+				panel,
+				vp.getDisplayName(),
+				JOptionPane.OK_CANCEL_OPTION,
+				JOptionPane.PLAIN_MESSAGE,
+				null))
+			return ta.getText();
+		
+		return initialValue;
 	}
 }
