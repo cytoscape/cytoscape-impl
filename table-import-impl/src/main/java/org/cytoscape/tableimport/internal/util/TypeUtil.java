06a9af7ca5fee97a8eca9c018c28b41dd4188c62
Christian Lopes <chrtannus@gmail.com>
2019-09-18 14:38:30 -0400

    Minor code reformatting only.

--- table-import-impl/src/main/java/org/cytoscape/tableimport/internal/util/TypeUtil.java
+++ table-import-impl/src/main/java/org/cytoscape/tableimport/internal/util/TypeUtil.java
@@ -1,29 +1,5 @@
 package org.cytoscape.tableimport.internal.util;
 
-/*
- * #%L
- * Cytoscape Table Import Impl (table-import-impl)
- * $Id:$
- * $HeadURL:$
- * %%
- * Copyright (C) 2006 - 2013 The Cytoscape Consortium
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 2.1 of the 
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-2.1.html>.
- * #L%
- */
-
 import static org.cytoscape.tableimport.internal.util.AttributeDataType.TYPE_BOOLEAN;
 import static org.cytoscape.tableimport.internal.util.AttributeDataType.TYPE_FLOATING;
 import static org.cytoscape.tableimport.internal.util.AttributeDataType.TYPE_INTEGER;
@@ -59,9 +35,30 @@ import javax.swing.table.TableModel;
 import org.cytoscape.model.CyIdentifiable;
 import org.cytoscape.model.CyNetwork;
 
-/**
- *
+/*
+ * #%L
+ * Cytoscape Table Import Impl (table-import-impl)
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2006 - 2019 The Cytoscape Consortium
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
  */
+
 public final class TypeUtil {
 
 	/**
@@ -253,6 +250,7 @@ public final class TypeUtil {
 		COLUMN_LOOP:
 		for (int col = 0; col < model.getColumnCount(); col++) {
 			AttributeDataType dt = dataTypes[col];
+			
 			for (int row = 0; row < rowCount; row++) {
 				final String val = (String) model.getValueAt(row, col);
 				
@@ -326,6 +324,7 @@ public final class TypeUtil {
 	
 	public static SourceColumnSemantic[] parseColumnTypeList(final String strList) {
 		final List<SourceColumnSemantic> typeList = new ArrayList<>();
+		
 		if (strList != null) {
 			final String[] tokens = getCSV(strList);
 			
@@ -372,6 +371,7 @@ public final class TypeUtil {
 				typeList.add(dataType);
 			}
 		}
+		
 		return typeList.toArray(new SourceColumnSemantic[typeList.size()]);
 	}
 
@@ -511,9 +511,9 @@ public final class TypeUtil {
 	}
 	
 	private static boolean isNaN(final String val){
-		if (val != null){
+		if (val != null)
 			return val.equals("NA") || val.equals("#NUM!") || val.equals("NaN");
-		}
+		
 		return false;
 	}
 
@@ -521,6 +521,7 @@ public final class TypeUtil {
 		if (val != null) {
 			if (isNaN(val))
 				return true;
+			
 			try {
 				final long n = Long.parseLong(val.trim());
 				return n <= Integer.MAX_VALUE && n >= Integer.MIN_VALUE;
@@ -535,6 +536,7 @@ public final class TypeUtil {
 		if (val != null) {
 			if (isNaN(val))
 				return true;
+			
 			try {
 				Long.parseLong(val.trim());
 				return true;
@@ -548,8 +550,10 @@ public final class TypeUtil {
 	private static boolean isDouble(String val) {
 		if (val != null) {
 			val = val.trim();
+			
 			if (isNaN(val))
 				return true;
+			
 			try {
 				Double.parseDouble(val);
 			} catch (NumberFormatException e) {
