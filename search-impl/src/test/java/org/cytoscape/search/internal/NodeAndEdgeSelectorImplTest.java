01c115e8e21303c1262ccea4dd270e70a35f09e3
Chris Churas <churas.camera@gmail.com>
2019-09-27 17:29:33 -0700

    initial revision

---
+++ search-impl/src/test/java/org/cytoscape/search/internal/NodeAndEdgeSelectorImplTest.java
@@ -0,0 +1,39 @@
+package org.cytoscape.search.internal;
+
+/*
+ * #%L
+ * Cytoscape Search Impl (search-impl)
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2006 - 2019 The Cytoscape Consortium
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class NodeAndEdgeSelectorImplTest {
+
+	@Test
+	public void testNullNetwork() {
+		
+		NodeAndEdgeSelectorImpl selector = new NodeAndEdgeSelectorImpl();
+		selector.selectNodesAndEdges(null, SearchResults.results(null, null), null, null);
+	}
+}
