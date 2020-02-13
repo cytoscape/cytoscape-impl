2d5ef7169582f706496b63d2685e3c15d8c45e76
Christian Lopes <chrtannus@gmail.com>
2020-02-13 17:13:15 -0500

    CYTOSCAPE-12653 #comment The HelpTask will always open the manual URL for the current version from now on (version 3.9+).

--- core-task-impl/src/main/java/org/cytoscape/task/internal/help/HelpTask.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/help/HelpTask.java
@@ -1,5 +1,6 @@
 package org.cytoscape.task.internal.help;
 
+import org.cytoscape.application.CyVersion;
 import org.cytoscape.service.util.CyServiceRegistrar;
 import org.cytoscape.util.swing.OpenBrowser;
 import org.cytoscape.work.AbstractTask;
@@ -7,19 +8,23 @@ import org.cytoscape.work.TaskMonitor;
 
 public class HelpTask extends AbstractTask {
 	
-	private final String url;
-	private final CyServiceRegistrar registrar;
+	private final String URL_TEMPLATE = "https://manual.cytoscape.org/en/%s.%s.%s/Quick_Tour_of_Cytoscape.html";
+	private final CyServiceRegistrar serviceRegistrar;
 
-	public HelpTask(CyServiceRegistrar serviceRegistrar, String link) {
-		super();
-		registrar = serviceRegistrar;
-		url = link;
+	public HelpTask(CyServiceRegistrar serviceRegistrar) {
+		this.serviceRegistrar = serviceRegistrar;
 	}
 
 	@Override
 	public void run(TaskMonitor tm) {
+		var cyVersion = serviceRegistrar.getService(CyVersion.class);
+		var major = cyVersion.getMajorVersion();
+		var minor = cyVersion.getMinorVersion();
+		var fix = cyVersion.getBugFixVersion();
+		var url = String.format(URL_TEMPLATE, major, minor, fix);
+		
 		try {
-			registrar.getService(OpenBrowser.class).openURL(url);
+			serviceRegistrar.getService(OpenBrowser.class).openURL(url);
 		} catch (Exception err) {
 			System.out.println("Unable to open browser for " + url.toString());
 		}
