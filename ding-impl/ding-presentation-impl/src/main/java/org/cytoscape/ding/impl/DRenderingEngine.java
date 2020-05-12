cytoscape/cytoscape-impl

commit 62b11a314072af5c46fc8ca048e35c0c0594cf8e

CYTOSCAPE-12705 Avoid deadlock when an App creates a renderer in a Task

ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/impl/DRenderingEngine.java
@@ -281,11 +281,6 @@ public class DRenderingEngine implements RenderingEngine<CyNetwork>, Printable, 
 		contentChanged = false;
 	}
 	
-	private void updateModelAndView() {
-		updateModel();
-		updateView(UpdateType.ALL_FULL);
-	}
-	
 	
 	public void updateView(UpdateType updateType) {
 		renderComponent.updateView(updateType);
@@ -481,16 +476,7 @@ public class DRenderingEngine implements RenderingEngine<CyNetwork>, Printable, 
 				}
 			});
 		}
-		
-		// MKTODO is this necessary, the timer will check the dirty flag
-		updateModelAndView();
 	}
-	
-	@Override
-	public void handleUpdateView() {
-		updateModelAndView();
-	}
-	
 	
 	public void zoom(int ticks) {
 		if(getViewModelSnapshot().isValueLocked(BasicVisualLexicon.NETWORK_SCALE_FACTOR))
@@ -600,8 +586,6 @@ public class DRenderingEngine implements RenderingEngine<CyNetwork>, Printable, 
 				netView.setVisualProperty(BasicVisualLexicon.NETWORK_CENTER_Y_LOCATION, yCenter);
 			}
 		});
-			
-		updateModelAndView();
 	}
 
 	

ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/impl/canvas/RenderComponent.java
@@ -8,6 +8,7 @@ import java.awt.Image;
 import java.awt.Paint;
 
 import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
 
 import org.cytoscape.ding.debug.DebugFrameType;
 import org.cytoscape.ding.debug.DebugProgressMonitorFactory;
@@ -73,7 +74,10 @@ public abstract class RenderComponent extends JComponent {
 			}
 		}
 		
-		updateView(UpdateType.ALL_FULL);
+		// Avoid deadlock if an App calls pack() or something similar from a Task.
+		if(SwingUtilities.isEventDispatchThread()) {
+			updateView(UpdateType.ALL_FULL);
+		}
 	}
 	
 	public boolean isInitialized() {

viewmodel-impl/impl/src/main/java/org/cytoscape/view/model/internal/model/CyNetworkViewImpl.java
@@ -401,6 +401,7 @@ public class CyNetworkViewImpl extends CyViewBase<CyNetwork> implements CyNetwor
 		for(CyNetworkViewListener listener : listeners) {
 			listener.handleFitContent();
 		}
+		setDirty();
 	}
 
 	@Override
@@ -408,6 +409,7 @@ public class CyNetworkViewImpl extends CyViewBase<CyNetwork> implements CyNetwor
 		for(CyNetworkViewListener listener : listeners) {
 			listener.handleFitSelected();
 		}
+		setDirty();
 	}
 
 	@Override
@@ -420,6 +422,7 @@ public class CyNetworkViewImpl extends CyViewBase<CyNetwork> implements CyNetwor
 		for(CyNetworkViewListener listener : listeners) {
 			listener.handleUpdateView();
 		}
+		setDirty();
 	}
 	
 	@Override

commit 3eaf027e38df03f8814472ecf1817c34cc4e615d

Fix style panel issue caused by commit 62b11a3

ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/impl/DRenderingEngine.java
@@ -478,6 +478,13 @@ public class DRenderingEngine implements RenderingEngine<CyNetwork>, Printable, 
 		}
 	}
 	
+	@Override
+	public void handleUpdateView() {
+		updateModel();
+		updateView(UpdateType.ALL_FULL);
+	}
+	
+	
 	public void zoom(int ticks) {
 		if(getViewModelSnapshot().isValueLocked(BasicVisualLexicon.NETWORK_SCALE_FACTOR))
 			return;