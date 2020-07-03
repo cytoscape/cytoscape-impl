c64de7a8eb95946a37f4217bd41b884385d44f07
Mike Kucera <mikekucera@gmail.com>
2020-07-03 12:21:52 -0400

    CYTOSCAPE-12730 Use custom dialog for 'command pause'

--- swing-application-impl/src/main/java/org/cytoscape/internal/command/PauseCommandTask.java
+++ swing-application-impl/src/main/java/org/cytoscape/internal/command/PauseCommandTask.java
@@ -1,9 +1,17 @@
 package org.cytoscape.internal.command;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.util.swing.LookAndFeelUtil;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.ProvidesTitle;
 import org.cytoscape.work.TaskMonitor;
@@ -30,7 +38,32 @@ public class PauseCommandTask extends AbstractTask {
 		
 		JFrame parent = swingApplication.getJFrame();
 		taskMonitor.showMessage(TaskMonitor.Level.INFO, "Paused...");
-		JOptionPane.showMessageDialog(parent, message, "Paused", JOptionPane.PLAIN_MESSAGE);
+		showDialog(parent, message);
 		taskMonitor.showMessage(TaskMonitor.Level.INFO, "continuing");
+	}
+	
+	
+	private static void showDialog(JFrame parent, String message) {
+		// Can't use JOptionPane because it doesn't work when run from automation script (CYTOSCAPE-12730).
+		JLabel label = new JLabel(message);
+		JButton okButton = new JButton("OK");
+		JPanel buttonPanel = LookAndFeelUtil.createOkCancelPanel(okButton, null);
+		
+		JPanel bodyPanel = new JPanel(new BorderLayout());
+		bodyPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		bodyPanel.add(label, BorderLayout.CENTER);
+		bodyPanel.add(buttonPanel, BorderLayout.SOUTH);
+		
+		JDialog dialog = new JDialog(parent);
+		dialog.getContentPane().add(bodyPanel);
+		
+		okButton.addActionListener(e -> dialog.dispose());
+		
+		dialog.setTitle("Paused");
+		dialog.setMinimumSize(new Dimension(200, 100));
+		dialog.setLocationRelativeTo(parent);
+		dialog.setModal(true);
+		dialog.pack();
+		dialog.setVisible(true);
 	}
 }
