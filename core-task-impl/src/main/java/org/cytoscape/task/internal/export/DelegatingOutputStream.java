a6980a6384ff4cd39ea1c1a6e42892d655a16e4e
Mike Kucera <mikekucera@gmail.com>
2020-09-26 17:52:34 -0400

    Fix performance issue when exporting

--- core-task-impl/src/main/java/org/cytoscape/task/internal/export/DelegatingOutputStream.java
+++ core-task-impl/src/main/java/org/cytoscape/task/internal/export/DelegatingOutputStream.java
@@ -17,6 +17,16 @@ public class DelegatingOutputStream extends OutputStream {
 	}
 	
 	@Override
+	public void write(byte[] b) throws IOException {
+		delegate.write(b);
+	}
+	
+	@Override
+	public void write(byte[] b, int off, int len) throws IOException {
+		delegate.write(b, off, len);
+	}
+	
+	@Override
 	public void flush() throws IOException {
 		delegate.flush();
 	}
