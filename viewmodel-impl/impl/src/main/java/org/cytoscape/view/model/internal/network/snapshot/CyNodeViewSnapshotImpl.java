8f440ca6c393d6c8f15afceba141f5eb2c5f88fc
Mike Kucera <mikekucera@gmail.com>
2021-03-03 14:17:59 -0500

    CYTOSCAPE-12790 Handle exceptions when applying a style.

--- viewmodel-impl/impl/src/main/java/org/cytoscape/view/model/internal/network/snapshot/CyNodeViewSnapshotImpl.java
+++ viewmodel-impl/impl/src/main/java/org/cytoscape/view/model/internal/network/snapshot/CyNodeViewSnapshotImpl.java
@@ -2,6 +2,7 @@ package org.cytoscape.view.model.internal.network.snapshot;
 
 import org.cytoscape.model.CyNode;
 import org.cytoscape.view.model.SnapshotNodeInfo;
+import org.cytoscape.view.model.VisualProperty;
 import org.cytoscape.view.model.internal.base.VPStore;
 import org.cytoscape.view.model.internal.network.CyNodeViewImpl;
 import org.cytoscape.view.presentation.property.BasicVisualLexicon;
@@ -23,15 +24,22 @@ public class CyNodeViewSnapshotImpl extends CyViewSnapshotBase<CyNode> implement
 		
 		// cache these values, they get looked up a lot
 		if(parent.isBVL()) {
-			x = ((Number)view.getVisualProperty(BasicVisualLexicon.NODE_X_LOCATION)).doubleValue();
-			y = ((Number)view.getVisualProperty(BasicVisualLexicon.NODE_Y_LOCATION)).doubleValue();
-			w = ((Number)view.getVisualProperty(BasicVisualLexicon.NODE_WIDTH)).doubleValue();
-			h = ((Number)view.getVisualProperty(BasicVisualLexicon.NODE_HEIGHT)).doubleValue();
+			x = getDoubleVP(view, BasicVisualLexicon.NODE_X_LOCATION);
+			y = getDoubleVP(view, BasicVisualLexicon.NODE_Y_LOCATION);
+			w = getDoubleVP(view, BasicVisualLexicon.NODE_WIDTH);
+			h = getDoubleVP(view, BasicVisualLexicon.NODE_HEIGHT);
 		} else {
 			x = y = w = h = 0;
 		}
 	}
 	
+	private static double getDoubleVP(CyNodeViewImpl view, VisualProperty<Double> vp) {
+		Object val = view.getVisualProperty(vp);
+		if(val instanceof Number) {
+			return ((Number)val).doubleValue();
+		}
+		return vp.getDefault();
+	}
 	
 	
 	@Override
