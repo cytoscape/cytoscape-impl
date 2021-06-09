23e0bacc64289c0edf9eb1b9ad8ca3686d1d985b
Christian Lopes <chrtannus@gmail.com>
2021-06-09 12:17:58 -0400

    Minor code refactoring/reformatting.

--- vizmap-gui-impl/src/main/java/org/cytoscape/view/vizmap/gui/internal/view/DropDownMenuButton.java
+++ vizmap-gui-impl/src/main/java/org/cytoscape/view/vizmap/gui/internal/view/DropDownMenuButton.java
@@ -4,7 +4,6 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Insets;
 import java.awt.RenderingHints;
 import java.awt.event.ActionListener;
 
@@ -40,9 +39,8 @@ import javax.swing.UIManager;
 /**
  * Button with drop down menu.
  */
+@SuppressWarnings("serial")
 public class DropDownMenuButton extends JButton {
-	
-	private final static long serialVersionUID = 1202339868695691L;
 	
 	private final static int TEXT_ICON_GAP = 6;
 	
@@ -59,20 +57,20 @@ public class DropDownMenuButton extends JButton {
 		this(null, true);
 	}
 	
-	public DropDownMenuButton(final boolean showMenuArrowIcon) {
+	public DropDownMenuButton(boolean showMenuArrowIcon) {
 		this(null, showMenuArrowIcon);
 	}
 	
-	public DropDownMenuButton(final JPopupMenu popupMenu) {
+	public DropDownMenuButton(JPopupMenu popupMenu) {
 		this(popupMenu, true);
 	}
 	
-	public DropDownMenuButton(final JPopupMenu popupMenu, final boolean showMenuArrowIcon) {
+	public DropDownMenuButton(JPopupMenu popupMenu, boolean showMenuArrowIcon) {
 		super("");
 		this.showMenuArrowIcon = showMenuArrowIcon;
 		
 		defaultActionListener = (evt) -> {
-			final JPopupMenu pm = getPopupMenu();
+			var pm = getPopupMenu();
 			
 			if (pm != null) {
 //				if (showPopup) {
@@ -89,7 +87,7 @@ public class DropDownMenuButton extends JButton {
 		// See http://stackoverflow.com/questions/2421914/showing-hiding-a-jpopupmenu-from-a-jbutton-focuslistener-not-working
 //		addMouseListener(new MouseAdapter() {
 //	        @Override
-//	        public void mousePressed(final MouseEvent e) {
+//	        public void mousePressed(MouseEvent e) {
 //	            // This is called before the menu loses focus.
 //	            // If the menu is already visible, just set showPopup to false, so that the actionPerformed
 //	            // does not show the menu again.
@@ -98,7 +96,7 @@ public class DropDownMenuButton extends JButton {
 //	        }
 //
 //	        @Override
-//	        public void mouseReleased(final MouseEvent e) {
+//	        public void mouseReleased(MouseEvent e) {
 //	            showPopup = true;
 //	        }
 //	    });
@@ -108,18 +106,18 @@ public class DropDownMenuButton extends JButton {
 	
 	// ==[ PUBLIC METHODS ]=============================================================================================
 	
-	public void setPopupMenu(final JPopupMenu popupMenu) {
+	public void setPopupMenu(JPopupMenu popupMenu) {
 		this.popupMenu = popupMenu;
 		
 		if (popupMenu != null) {// FIXME
 //			popupMenu.addFocusListener(new FocusListener() {
 //		        @Override
-//		        public void focusLost(final FocusEvent e) {
+//		        public void focusLost(FocusEvent e) {
 //		            System.out.println("LOST FOCUS");
 //		            isShowingPopup = false;
 //		        }
 //		        @Override
-//		        public void focusGained(final FocusEvent e) {
+//		        public void focusGained(FocusEvent e) {
 //		            System.out.println("GAINED FOCUS");
 //		            isShowingPopup = true;
 //		        }
@@ -139,7 +137,7 @@ public class DropDownMenuButton extends JButton {
 
 	@Override
 	public Dimension getPreferredSize() {
-		final Dimension d = super.getPreferredSize();
+		var d = super.getPreferredSize();
 		
 		if (showMenuArrowIcon) // So the arrow icon doesn't overlap the text
 			d.width += buttonIcon.getIconWidth() + TEXT_ICON_GAP;
@@ -148,24 +146,24 @@ public class DropDownMenuButton extends JButton {
 	}
 	
 	@Override
-	public void addActionListener(final ActionListener l) {
+	public void addActionListener(ActionListener l) {
 		super.addActionListener(l);
 		updateEnabled();
 	}
 
 	@Override
-	public void removeActionListener(final ActionListener l) {
+	public void removeActionListener(ActionListener l) {
 		super.removeActionListener(l);
 		updateEnabled();
 	}
 
 	@Override
-	public void paintComponent(final Graphics g) {
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
 		if (showMenuArrowIcon) {
-			final Dimension size = getSize();
-			final Insets ins = getInsets();
+			var size = getSize();
+			var ins = getInsets();
 			int x = size.width - buttonIcon.getIconWidth() - ins.right;
 			int y = ins.top + ((size.height - ins.top - ins.bottom - buttonIcon.getIconHeight()) / 2);
 			buttonIcon.paintIcon(this, g, x, y);
@@ -175,7 +173,7 @@ public class DropDownMenuButton extends JButton {
 	// ==[ PRIVATE METHODS ]============================================================================================
 	
 	private synchronized void updateEnabled() {
-		final ActionListener[] actionListeners = getActionListeners();
+		var actionListeners = getActionListeners();
 		setEnabled((actionListeners != null && actionListeners.length > 0) || 
 				   (popupMenu != null && popupMenu.getSubElements().length > 0));
 	}
