4c9a818a43ad761c01846626ff789aaa4eab8151
Yihang Xin <yihang.xin@gladstone.ucsf.edu>
2023-01-10 22:57:49 -0800

    Fix CYTOSCAPE-13022

--- swing-application-impl/src/main/java/org/cytoscape/internal/view/help/HelpVideosTask.java
+++ swing-application-impl/src/main/java/org/cytoscape/internal/view/help/HelpVideosTask.java
@@ -15,25 +15,25 @@ import org.cytoscape.work.TaskMonitor;
  * Copyright (C) 2006 - 2021 The Cytoscape Consortium
  * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 2.1 of the 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
+ *
+ * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
  */
 
 public class HelpVideosTask extends AbstractTask {
-	
-	private static final String VIDEOS_URL = "https://www.youtube.com/channel/UCv6auk9FK4NgXiXiqrDLccw"; 
-	
+
+	private static final String VIDEOS_URL = "https://www.youtube.com/channel/UCv6auk9FK4NgXiXiqrDLccw";
+
 	private final CyServiceRegistrar serviceRegistrar;
 
 	public HelpVideosTask(final CyServiceRegistrar serviceRegistrar) {
@@ -44,7 +44,7 @@ public class HelpVideosTask extends AbstractTask {
 	public void run(TaskMonitor tm) {
 		final OpenBrowser openBrowser = serviceRegistrar.getService(OpenBrowser.class);
 		final CyVersion cyVersion = serviceRegistrar.getService(CyVersion.class);
-		
-		openBrowser.openURL(VIDEOS_URL, false);
+
+		openBrowser.openURL(VIDEOS_URL);
 	}
 }
