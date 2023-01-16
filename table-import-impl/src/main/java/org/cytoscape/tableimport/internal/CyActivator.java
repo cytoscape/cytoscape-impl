e128d839716e9306e6f314d79857a828ad49c0b7
Mike Kucera <mikekucera@gmail.com>
2023-01-16 09:54:30 -0500

    CYTOSCAPE-13031 Use new mime types for excel files.

--- table-import-impl/src/main/java/org/cytoscape/tableimport/internal/CyActivator.java
+++ table-import-impl/src/main/java/org/cytoscape/tableimport/internal/CyActivator.java
@@ -101,7 +101,7 @@ public class CyActivator extends AbstractCyActivator {
 			// ".xls"
 			var filter = new WildCardCyFileFilter(
 					new String[] { "xls", "xlsx" },
-					new String[] { "application/excel" },
+					new String[] { "application/excel", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" },
 					"Excel",
 					TABLE,
 					streamUtil
