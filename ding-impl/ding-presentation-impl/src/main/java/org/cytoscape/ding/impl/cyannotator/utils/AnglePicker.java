fcf131356be5c7fda37396a9b3a6c04f894598ad
Christian Lopes <chrtannus@gmail.com>
2023-02-03 14:23:01 -0300

    - Fixes issue where the color of the Edit (color) button is not updated if the color is changed by double-clicking a gradient marker. - Press SHIFT while dragging a gradient marker to snap to the closest main position/fraction (0.1, 0.2, 0.3, ...0.9). - Minor UI changes and code refactoring.  -- CYTOSCAPE-12596

--- ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/impl/cyannotator/utils/AnglePicker.java
+++ ding-impl/ding-presentation-impl/src/main/java/org/cytoscape/ding/impl/cyannotator/utils/AnglePicker.java
@@ -2,15 +2,18 @@ package org.cytoscape.ding.impl.cyannotator.utils;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.LinearGradientPaint;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.Stroke;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 
+import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
@@ -27,12 +30,20 @@ public class AnglePicker extends JPanel {
 	/** We need the 360 value for the findClosestNumber function, or it will be hard to snap to 0 degrees. */
 	private static final  int[] ALL_ANGLES = { 0, 45, 90, 135, 180, 225, 270, 315, 360 };
 	
+	private Color borderColor = UIManager.getColor("Separator.foreground");
+	private Color color1 = UIManager.getColor("CyComponent.borderColor");
+	private Color color2 = UIManager.getColor("Table.background");
+	private Color selColor = UIManager.getColor("Focus.color");
+	
+	private Stroke defStroke = new BasicStroke(1);
+	private Stroke thickStroke = new BasicStroke(2);
+	private Stroke dashedStroke = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 1 }, 0);
+	
 	private int angle = -1;
 	
 	private float[] fractions = { 0.0f, 1.0f };
 	private Color[] colors = { Color.BLACK, Color.WHITE };
 	
-	private boolean mousePressed;
 	private boolean mouseDragging;
 	
 	public AnglePicker() {
@@ -41,12 +52,10 @@ public class AnglePicker extends JPanel {
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent evt) {
-				mousePressed = true;
 				selectAngle(evt);
 			}
 			@Override
 			public void mouseReleased(MouseEvent evt) {
-				mousePressed = false;
 				mouseDragging = false;
 				repaint();
 			}
@@ -54,10 +63,8 @@ public class AnglePicker extends JPanel {
 		addMouseMotionListener(new MouseMotionAdapter() {
 			@Override
 			public void mouseDragged(MouseEvent evt) {
-				if (mousePressed) {
-					mouseDragging = true;
-					selectAngle(evt);
-				}
+				mouseDragging = true;
+				selectAngle(evt);
 			}
 		});
 	}
@@ -110,13 +117,6 @@ public class AnglePicker extends JPanel {
 		int pad = Math.max(insets.top + insets.bottom, insets.left + insets.right) / 2;
 		int r = Math.min(w, h) / 2 - pr - pb / 2 - pad;
 		
-		var color1 = UIManager.getColor("Separator.foreground");
-		var color2 = UIManager.getColor("Table.background");
-		var selColor = UIManager.getColor("Focus.color");
-		
-		var stroke1 = new BasicStroke(1);
-		var stroke2 = new BasicStroke(2);
-		
 		g2.translate(w / 2, h / 2);
 		
 		if (fractions != null && fractions.length > 0 && colors != null && colors.length > 0) {
@@ -128,26 +128,25 @@ public class AnglePicker extends JPanel {
 			g2.setPaint(paint);
 			g2.fill(bounds);
 			
-			g2.setColor(color1);
+			g2.setColor(borderColor);
 			g2.draw(bounds);
 		}
+		
+		g2.setStroke(dashedStroke);
+		g2.setColor(Color.LIGHT_GRAY);
+		g2.drawLine(-r, 0, r, 0);
+		g2.drawLine(0, -r, 0, r);
 		
 		g2.setColor(color1);
 		g2.drawOval(-r, -r, r * 2, r * 2); // external line
 		g2.setColor(color2);
 		g2.drawOval(-r + 1, -r + 1, (r - 1) * 2, (r - 1) * 2); // internal line
 		
-		var dashed = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 5 }, 0);
-		g2.setStroke(dashed);
-		
-		g2.drawLine(-r, 0, r, 0);
-		g2.drawLine(0, -r, 0, r);
-		
 		for (int angle : MAIN_ANGLES) {
 			int x = (int) (r * Math.cos(Math.toRadians(angle)));
 			int y = (int) (r * Math.sin(Math.toRadians(angle)));
 			
-			g2.setStroke(stroke2);
+			g2.setStroke(thickStroke);
 			g2.setColor(color1);
 			g2.drawOval(x - pr, y - pr, 2 * pr, 2 * pr);
 			
@@ -160,16 +159,16 @@ public class AnglePicker extends JPanel {
 			int y = (int) (r * Math.sin(Math.toRadians(angle)));
 			
 			if (mouseDragging) {
-				g2.setStroke(stroke1);
+				g2.setStroke(defStroke);
 				g2.setColor(selColor);
 				g2.drawLine(x, -y, 0, 0);
 			}
 			
-			g2.setStroke(stroke2);
+			g2.setStroke(thickStroke);
 			g2.setColor(color2);
 			g2.drawOval(x - pr, -y - pr, 2 * pr, 2 * pr);
 			
-			g2.setStroke(stroke1);
+			g2.setStroke(defStroke);
 			g2.setColor(selColor);
 			g2.fillOval(x - pr, -y - pr, 2 * pr, 2 * pr);
 		}
@@ -177,18 +176,18 @@ public class AnglePicker extends JPanel {
 		g2.dispose();
 	}
 	
-//	public static void main(String[] args) {
-//		var dialog = new JDialog();
-//		dialog.setTitle("Angle Picker");
-//		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-//		dialog.setModal(true);
-//		
-//		var picker = new AnglePicker();
-//		picker.setPreferredSize(new Dimension(200, 200));
-//		dialog.add(picker);
-//		
-//		dialog.pack();
-//		dialog.setLocationRelativeTo(null);
-//		dialog.setVisible(true);
-//	}
+	public static void main(String[] args) {
+		var dialog = new JDialog();
+		dialog.setTitle("Angle Picker");
+		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		dialog.setModal(true);
+		
+		var picker = new AnglePicker();
+		picker.setPreferredSize(new Dimension(200, 200));
+		dialog.add(picker);
+		
+		dialog.pack();
+		dialog.setLocationRelativeTo(null);
+		dialog.setVisible(true);
+	}
 }
