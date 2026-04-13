8f0add92537d04dfcc883c9e350bf511d026b3ed
Christian Lopes <chrtannus@gmail.com>
2026-04-13 17:16:42 -0300

    Don't parallelize Prefuse Layout if the 'deterministic' option is set -- CYTOSCAPE-13166

--- layout-prefuse-impl/src/main/java/prefuse/util/force/ForceSimulator.java
+++ layout-prefuse-impl/src/main/java/prefuse/util/force/ForceSimulator.java
@@ -44,6 +44,7 @@ public class ForceSimulator {
     private Force[] sforces;
     private int iflen, sflen;
     private Integrator integrator;
+    private final boolean isDeterministic;
     private float speedLimit = 1.0f;
     
     private final StateMonitor monitor;
@@ -52,17 +53,18 @@ public class ForceSimulator {
      * Create a new, empty ForceSimulator. A RungeKuttaIntegrator is used
      * by default.
      */
-    public ForceSimulator(StateMonitor monitor) {
-        this(new RungeKuttaIntegrator(monitor), monitor);
+    public ForceSimulator(StateMonitor monitor, boolean isDeterministic) {
+        this(new RungeKuttaIntegrator(monitor), monitor, isDeterministic);
     }
 
     /**
      * Create a new, empty ForceSimulator.
      * @param integrator the Integrator to use
      */
-    public ForceSimulator(Integrator integrator, StateMonitor monitor) {
+    public ForceSimulator(Integrator integrator, StateMonitor monitor, boolean isDeterministic) {
         this.integrator = integrator;
         this.monitor = monitor;
+        this.isDeterministic = isDeterministic;
         iforces = new Force[5];
         sforces = new Force[5];
         iflen = 0;
@@ -247,18 +249,22 @@ public class ForceSimulator {
      */
     protected void accumulate() {
     	// Init
-    	Arrays.asList(iforces).parallelStream().forEach(f -> {
-			if (!monitor.isCancelled() && f != null)
-				f.init(this);
-		});
-    	Arrays.asList(sforces).parallelStream().forEach(f -> {
-			if (!monitor.isCancelled() && f != null)
-				f.init(this);
-		});
-//		for (int i = 0; i < iflen && !monitor.isCancelled(); i++)
-//			iforces[i].init(this);
-//		for (int i = 0; i < sflen && !monitor.isCancelled(); i++)
-//			sforces[i].init(this);
+    	if (isDeterministic) {
+			for (int i = 0; i < iflen && !monitor.isCancelled(); i++)
+				iforces[i].init(this);
+			for (int i = 0; i < sflen && !monitor.isCancelled(); i++)
+				sforces[i].init(this);
+    	} else {
+    		// Can Be Parallelized
+    		Arrays.asList(iforces).parallelStream().forEach(f -> {
+    			if (!monitor.isCancelled() && f != null)
+    				f.init(this);
+    		});
+        	Arrays.asList(sforces).parallelStream().forEach(f -> {
+    			if (!monitor.isCancelled() && f != null)
+    				f.init(this);
+    		});
+    	}
 		
 		// Update forces
 		updateForceItems(items);
@@ -266,40 +272,45 @@ public class ForceSimulator {
     }
 
 	private void updateForceItems(Collection<ForceItem> list) {
-		list.parallelStream().forEach(item -> {
-			if (!monitor.isCancelled()) {
-				item.force[0] = 0.0f;
-				item.force[1] = 0.0f;
-				
-				for (int i = 0; i < iflen && !monitor.isCancelled(); i++)
-					iforces[i].getForce(item);
+		if (isDeterministic) {
+			for (ForceItem item : list) {
+				if (!monitor.isCancelled())
+					updateForceItem(item);
 			}
-		});
-//		for (ForceItem item : list) {
-//			if (monitor.isCancelled())
-//				return;
-//			
-//			item.force[0] = 0.0f;
-//			item.force[1] = 0.0f;
-//			
-//			for (int i = 0; i < iflen && !monitor.isCancelled(); i++)
-//				iforces[i].getForce(item);
-//		}
+		} else {
+			// Can Be Parallelized
+			list.parallelStream().forEach(item -> {
+				if (!monitor.isCancelled())
+					updateForceItem(item);
+			});
+		}
+	}
+	
+	private void updateForceItem(ForceItem item) {
+		item.force[0] = 0.0f;
+		item.force[1] = 0.0f;
+		
+		for (int i = 0; i < iflen && !monitor.isCancelled(); i++)
+			iforces[i].getForce(item);
 	}
 	
 	private void updateSprings(Collection<Spring> list) {
-		list.parallelStream().forEach(item -> {
-			if (!monitor.isCancelled()) {
-				for (int i = 0; i < sflen && !monitor.isCancelled(); i++)
-					sforces[i].getForce(item);
+		if (isDeterministic) {
+			for (Spring s : list) {
+				if (!monitor.isCancelled())
+					updateSpring(s);
 			}
-		});
-//		for (Spring s : list) {
-//			if (monitor.isCancelled())
-//				return;
-//			
-//			for (int i = 0; i < sflen && !monitor.isCancelled(); i++)
-//				sforces[i].getForce(s);
-//		}
+		} else {
+			// Can Be Parallelized
+			list.parallelStream().forEach(s -> {
+				if (!monitor.isCancelled())
+					updateSpring(s);
+			});
+		}
+	}
+	
+	private void updateSpring(Spring s) {
+		for (int i = 0; i < sflen && !monitor.isCancelled(); i++)
+			sforces[i].getForce(s);
 	}
 }
